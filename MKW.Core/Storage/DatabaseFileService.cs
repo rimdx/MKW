9f3ca8ac0bab9cf85b70d9c5728b7d173f2cd058
Timofei Zhakov <tima@chemodax.net>
2025-08-17 21:14:12 +0000

    Add simple Storage and Database interfaces which can now save and load data from JSON file, stored on disk. The command line can now use this framework to open and save the database.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@3980 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ MKW.Core/Storage/DatabaseFileService.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json;
+
+namespace MKW.Core.Storage
+{
+    public interface IDatabaseService
+    {
+        Database Open(string path);
+        void Save(Database database, string path);
+    }
+
+    public class DatabaseFileService : IDatabaseService
+    {
+        public Database Open(string path)
+        {
+            using FileStream fstream = File.OpenRead(path);
+
+            return JsonSerializer.Deserialize<Database>(fstream)!;
+        }
+
+        public void Save(Database database, string path)
+        {
+            using FileStream fstream = File.OpenWrite(path);
+
+            JsonSerializer.Serialize(fstream, database);
+        }
+    }
+}
--- MKW/Program.cs
+++ MKW/Program.cs
@@ -1,10 +1,47 @@
-﻿namespace MKW
+﻿using MKW.Core.Storage;
+
+namespace MKW
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            if (args.Length == 0)
+            {
+                Console.Error.WriteLine("Please provide the path to the database file.");
+                return;
+            }
+
+            string path = args[0];
+
+            Database database = EnsureDatabase(path);
+
+            Console.WriteLine($"[Verbose] Successfully opened database file.");
+        }
+
+        private static Database EnsureDatabase(string path)
+        {
+            var service = new DatabaseFileService();
+
+            if (File.Exists(path))
+            {
+                Console.WriteLine($"[Verbose] Opening database file: {path}.");
+
+                return service.Open(path);
+            }
+            else
+            {
+                Console.WriteLine($"[Verbose] Initializing database file at {path}.");
+
+                Database database = new Database
+                {
+                    Users = new List<User>()
+                };
+
+                service.Save(database, path);
+
+                return database;
+            }
         }
     }
 }
