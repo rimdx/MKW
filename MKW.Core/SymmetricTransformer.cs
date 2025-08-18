f457aed4f77d640f6e16bb82db1f3d99a73e6b98
Timofei Zhakov <tima@chemodax.net>
2025-08-18 14:47:38 +0000

    Factor out SymmetricTransformer
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@3991 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ MKW.Core/SymmetricTransformer.cs
@@ -0,0 +1,65 @@
+﻿using System.Security.Cryptography;
+
+namespace MKW.Core
+{
+    public class SymmetricTransformer : IDisposable
+    {
+        private readonly Aes aes;
+
+        protected SymmetricTransformer(byte[] key, byte[]? iv)
+        {
+            aes = Aes.Create();
+
+            aes.Key = key;
+
+            if (iv == null)
+                aes.GenerateIV();
+            else
+                aes.IV = iv;
+        }
+
+        public static SymmetricTransformer Create(byte[] key)
+        {
+            return new SymmetricTransformer(key, null);
+        }
+
+        public static SymmetricTransformer Open(byte[] key, byte[] iv)
+        {
+            return new SymmetricTransformer(key, iv);
+        }
+
+        public byte[] Encrypt(byte[] data)
+        {
+            using MemoryStream output = new MemoryStream();
+            using ICryptoTransform encryptor = aes.CreateEncryptor();
+            using CryptoStream encryptorStream = new CryptoStream(output, encryptor, CryptoStreamMode.Write);
+
+            encryptorStream.Write(data);
+            encryptorStream.FlushFinalBlock();
+
+            return output.ToArray();
+        }
+
+        public byte[] Decrypt(byte[] data)
+        {
+            using MemoryStream input = new MemoryStream(data);
+            using ICryptoTransform decryptor = aes.CreateDecryptor();
+            using CryptoStream decryptorStream = new CryptoStream(input, decryptor, CryptoStreamMode.Read);
+            using MemoryStream output = new MemoryStream(input.Capacity);
+
+            decryptorStream.CopyTo(output);
+
+            return output.ToArray();
+        }
+
+        public byte[] ExportIV()
+        {
+            return aes.IV;
+        }
+
+        public void Dispose()
+        {
+            aes.Dispose();
+        }
+    }
+}
