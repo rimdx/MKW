7da1e473f2c0ce344a080c76e7710b39400c6e41
Timofei Zhakov <tima@chemodax.net>
2025-08-18 16:08:09 +0000

    Separate database and client abstraction; make database managed in a better way, many other improvements.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@3996 0083e6b4-6090-234a-836e-88fcc467669c

--- MKW.Core/UserSession.cs
+++ MKW.Core/UserSession.cs
@@ -4,11 +4,11 @@ namespace MKW.Core
 {
     public class UserSession
     {
-        private readonly Database db;
+        private readonly IDatabaseSession db;
         private readonly User user;
         private readonly byte[] decryptedPrivateKey;
 
-        public UserSession(Database db, User user, byte[] decryptedPrivateKey)
+        public UserSession(IDatabaseSession db, User user, byte[] decryptedPrivateKey)
         {
             this.db = db;
             this.user = user;
