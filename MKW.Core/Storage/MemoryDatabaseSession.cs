fffec6db5982c8816bb65b29f13efd64368bea36
Timofei Zhakov <tima@chemodax.net>
2025-08-18 16:11:24 +0000

    Move MemoryDatabaseSession into a separate file
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@3998 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ MKW.Core/Storage/MemoryDatabaseSession.cs
@@ -0,0 +1,30 @@
+﻿namespace MKW.Core.Storage
+{
+    public class MemoryDatabaseSession : IDatabaseSession, IDisposable
+    {
+        public readonly Database Database;
+
+        public MemoryDatabaseSession()
+        {
+            Database = new Database();
+        }
+
+        public void AddUser(Guid id, User user)
+        {
+            Database.Users.Add(user);
+        }
+
+        public User GetUser(Guid id)
+        {
+            return Database.Users.First(u => u.Id == id);
+        }
+
+        public void Save()
+        {
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
