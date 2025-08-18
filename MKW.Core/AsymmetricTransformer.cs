3a123a9d50797a1a09d0687e68d7e0a36e1d52fd
Timofei Zhakov <tima@chemodax.net>
2025-08-18 14:59:49 +0000

    Factor out SymmetricTransformer
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@3992 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ MKW.Core/AsymmetricTransformer.cs
@@ -0,0 +1,64 @@
+﻿using System.Security.Cryptography;
+
+namespace MKW.Core
+{
+    public class AsymmetricTransformer : IDisposable
+    {
+        private readonly RSA rsa;
+
+        protected AsymmetricTransformer(RSA rsa)
+        {
+            this.rsa = rsa;
+        }
+
+        public static AsymmetricTransformer Create()
+        {
+            RSA rsa = RSA.Create();
+            return new AsymmetricTransformer(rsa);
+        }
+
+        public static AsymmetricTransformer Open(byte[] publicKey)
+        {
+            RSA rsa = RSA.Create();
+
+            rsa.ImportRSAPublicKey(publicKey, out _);
+
+            return new AsymmetricTransformer(rsa);
+        }
+
+        public static AsymmetricTransformer Open(byte[] publicKey, byte[] privateKey)
+        {
+            RSA rsa = RSA.Create();
+
+            rsa.ImportRSAPublicKey(publicKey, out _);
+            rsa.ImportRSAPrivateKey(privateKey, out _);
+
+            return new AsymmetricTransformer(rsa);
+        }
+
+        public byte[] Encrypt(byte[] data)
+        {
+            return rsa.Encrypt(data, RSAEncryptionPadding.Pkcs1);
+        }
+
+        public byte[] Decrypt(byte[] data)
+        {
+            return rsa.Decrypt(data, RSAEncryptionPadding.Pkcs1);
+        }
+
+        public byte[] ExportPublicKey()
+        {
+            return rsa.ExportRSAPublicKey();
+        }
+
+        public byte[] ExportPrivateKey()
+        {
+            return rsa.ExportRSAPrivateKey();
+        }
+
+        public void Dispose()
+        {
+            rsa.Dispose();
+        }
+    }
+}
