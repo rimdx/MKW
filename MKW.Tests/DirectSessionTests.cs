fe6fbed8930e0b4efb421453646c58196fcbfe00
Timofei Zhakov <tima@chemodax.net>
2025-08-18 15:12:29 +0000

    Add tests with opening a direct session (through the API) to the database and asserting stuff like that.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@3993 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ MKW.Tests/DirectSessionTests.cs
@@ -0,0 +1,24 @@
+﻿using MKW.Core;
+using MKW.Core.Storage;
+using NUnit.Framework.Legacy;
+
+namespace MKW.Tests
+{
+    public class DirectSessionTests
+    {
+        [Test]
+        public void SimpleAddUserTest()
+        {
+            Database db = new Database()
+            {
+                Users = new List<User>()
+            };
+
+            DatabaseSession session = new DatabaseSession(db);
+
+            session.AddUser("whattheheckamidoing");
+
+            ClassicAssert.AreEqual(1, db.Users.Count);
+        }
+    }
+}
