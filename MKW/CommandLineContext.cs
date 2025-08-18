d0d4e9732d0b3b2e43d31932d8882543c9052646
Timofei Zhakov <tima@chemodax.net>
2025-08-18 21:42:48 +0000

    Add --non-interactive and --force-interactive options.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@4038 0083e6b4-6090-234a-836e-88fcc467669c

--- MKW/CommandLineContext.cs
+++ MKW/CommandLineContext.cs
@@ -30,6 +30,16 @@ namespace MKW
             Description = "password to perform operation with",
         };
 
+        private readonly Option<bool> optNonInteractive = new("--non-interactive")
+        {
+            Description = "do no interactive prompting (default is to prompt only if standard input is a terminal device)"
+        };
+
+        private readonly Option<bool> optForceInteractive = new("--force-interactive")
+        {
+            Description = "do interactive prompting even if standard input is not a terminal device"
+        };
+
         public CommandLineContext()
         {
             rootCommand = new RootCommand("Multi-Key Wallet");
@@ -37,20 +47,28 @@ namespace MKW
             cmdAddUser = new Command("add-user", "adds a user to the database");
             cmdAddUser.Arguments.Add(argFile);
             cmdAddUser.Options.Add(optPassword);
+            cmdAddUser.Options.Add(optNonInteractive);
+            cmdAddUser.Options.Add(optForceInteractive);
             cmdAddUser.SetAction(AddUserAction);
 
             cmdAddEntry = new Command("add-entry", "adds an encrypted entry to the database");
             cmdAddEntry.Arguments.Add(argFile);
             cmdAddEntry.Arguments.Add(argPayload);
+            cmdAddEntry.Options.Add(optNonInteractive);
+            cmdAddEntry.Options.Add(optForceInteractive);
             cmdAddEntry.SetAction(AddEntryAction);
 
             cmdTouch = new Command("touch", "initializes empty database");
             cmdTouch.Arguments.Add(argFile);
+            cmdTouch.Options.Add(optNonInteractive);
+            cmdTouch.Options.Add(optForceInteractive);
             cmdTouch.SetAction(TouchAction);
 
             cmdEntries = new Command("entries", "list all entries in the database");
             cmdEntries.Arguments.Add(argFile);
             cmdEntries.Options.Add(optPassword);
+            cmdEntries.Options.Add(optNonInteractive);
+            cmdEntries.Options.Add(optForceInteractive);
             cmdEntries.SetAction(ListEntriesAction);
 
             rootCommand.Subcommands.Add(cmdAddUser);
@@ -122,12 +140,41 @@ namespace MKW
             }
         }
 
+        private void EnsureInteractive(ParseResult argv, string errorMessage)
+        {
+            bool non_interactive = argv.GetValue(optNonInteractive);
+            bool force_interactive = argv.GetValue(optForceInteractive);
+
+            /* The --non-interactive and --force-interactive options are mutually
+             * exclusive. */
+            if (non_interactive && force_interactive)
+            {
+                throw new Exception(errorMessage, new Exception("--non-interactive and --force-interactive are mutually exclusive"));
+            }
+
+            /* If neither --non-interactive nor --force-interactive was passed,
+             * be interactive if stdin is a terminal.
+             * If --force-interactive was passed, always be interactive. */
+            if (!force_interactive && !non_interactive)
+            {
+                if (Console.IsInputRedirected)
+                {
+                    throw new Exception(errorMessage, new Exception("Standard input has been redirected."));
+                }
+            }
+
+            if (non_interactive)
+                throw new Exception(errorMessage, new Exception("Interactive prompts are disabled by --non-interactive option."));
+        }
+
         private string GetPassword(ParseResult argv)
         {
             string? password = argv.GetValue(optPassword);
 
             if (password == null)
             {
+                EnsureInteractive(argv, "Please provide password using --password option.");
+
                 Console.Write("Enter password: ");
 
                 password = Console.ReadLine();
