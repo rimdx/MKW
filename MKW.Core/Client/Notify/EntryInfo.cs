d9f2e902defbfe728e312c96e61f9c0d41489e08
Timofei Zhakov <tima@chemodax.net>
2025-08-18 19:39:05 +0000

    Return some feedback from client actions.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@4021 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ MKW.Core/Client/Notify/EntryInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace MKW.Core.Client.Notify
+{
+    public class EntryInfo
+    {
+        public required ActionInfo Action { get; init; }
+
+        public required Guid Id { get; init; }
+
+        public required IReadOnlyList<UserInfo> EncodedForUsers { get; init; }
+    }
+}
