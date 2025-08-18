9a811df33e096f1c35ae3d2285c2ad90431c2260
Timofei Zhakov <tima@chemodax.net>
2025-08-18 14:13:27 +0000

    Add DatabaseSession abstraction and implement subcommands in the CLI; Run all this in the test-suite; New "AddUser()" method.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@3988 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ MKW.Core/DatabaseSession.cs
@@ -0,0 +1,18 @@
+﻿using MKW.Core.Storage;
+
+namespace MKW.Core
+{
+    public class DatabaseSession
+    {
+        private readonly Database db;
+
+        public DatabaseSession(Database db)
+        {
+            this.db = db;
+        }
+
+        public void AddUser(string password)
+        {
+        }
+    }
+}
