5f70716bf177ca41e115daeb343e7d72de6bc214
Timofei Zhakov <tima@chemodax.net>
2025-08-18 15:29:24 +0000

    Add simple user session and test it.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@3994 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ MKW.Core/UserSession.cs
@@ -0,0 +1,18 @@
+﻿using MKW.Core.Storage;
+
+namespace MKW.Core
+{
+    public class UserSession
+    {
+        private readonly Database db;
+        private readonly User user;
+        private readonly byte[] decryptedPrivateKey;
+
+        public UserSession(Database db, User user, byte[] decryptedPrivateKey)
+        {
+            this.db = db;
+            this.user = user;
+            this.decryptedPrivateKey = decryptedPrivateKey;
+        }
+    }
+}
