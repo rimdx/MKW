e1919dca667b1c97212dd6b745fa3139ce438cf1
Timofei Zhakov <tima@chemodax.net>
2025-08-18 18:17:35 +0000

    Introduce ClientSession.UpdateEntry() to allow editing, adding, and removing secret entries. Implement all encryption required and test the code.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@4010 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ MKW.Core/EntryPayload.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+namespace MKW.Core
+{
+    public record class EntryPayload
+    {
+        public byte[] Data { get; }
+
+        public EntryPayload(byte[] data)
+        {
+            Data = data;
+        }
+
+        public EntryPayload(string data)
+        {
+            Data = Encoding.Unicode.GetBytes(data);
+        }
+
+        public override string ToString()
+        {
+            return Encoding.Unicode.GetString(Data);
+        }
+    }
+}
