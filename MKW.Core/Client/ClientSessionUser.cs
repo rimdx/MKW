ea293878742df5c468b227fd769bb73863af3cdb
Timofei Zhakov <tima@chemodax.net>
2025-08-19 11:45:50 +0000

    Split ClientSession onto different files all implementing one /partial/ class.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@4060 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ MKW.Core/Client/ClientSessionUser.cs
@@ -0,0 +1,73 @@
+﻿using MKW.Core.Client.Notify;
+using MKW.Core.Cryptography;
+using MKW.Core.Storage;
+using System.Security.Cryptography;
+
+namespace MKW.Core.Client
+{
+    public partial class ClientSession : IDisposable
+    {
+        public UserInfo PromoteUser(string password)
+        {
+            SystemCredentialsManager credManager = new SystemCredentialsManager();
+
+            UserCredentials userCreds = UserCredentials.Create(password);
+            SystemCredentials systemCreds = credManager.GenerateCredentials(userCreds);
+
+            DatabaseUser user = new DatabaseUser
+            {
+                Id = Guid.NewGuid(),
+                PublicKey = systemCreds.PublicKey,
+                PrivateKey = systemCreds.PrivateKey,
+                Salt = systemCreds.Salt,
+            };
+
+            db.AddUser(user.Id, user);
+
+            return UserInfo.FromDatabaseUser(user);
+        }
+
+        public UserSession OpenUser(Guid id, string password)
+        {
+            DatabaseUser user = db.GetUser(id);
+
+            // Credentials can be opened within the entered password and the public salt
+            UserCredentials creds = UserCredentials.Open(password, user.Salt);
+
+            return OpenUser(user, creds);
+        }
+
+        public UserSession OpenUser(string password)
+        {
+            foreach (DatabaseUser user in db.EnumerateUsers())
+            {
+                try
+                {
+                    UserCredentials creds = UserCredentials.Open(password, user.Salt);
+
+                    return OpenUser(user, creds);
+                }
+                catch (CryptographicException)
+                {
+                    // Ignore this user if the credentials are invalid
+                }
+            }
+
+            throw new Exception("No valid user found with the provided password.");
+        }
+
+        public UserSession OpenUser(DatabaseUser user, UserCredentials creds)
+        {
+            // Private data of the user is encrypted symmetrically using our creds (decoder
+            // also needs some data stored in the public section of the object).
+            using SymmetricTransformer decoder = SymmetricTransformer.Open(creds.GetEncodingHash(),
+                                                                           creds.ExportSalt());
+
+            // Let's try'N decode the private key. We could potentially fail here. So
+            // some validation may be required.
+            byte[] privateKeyBytes = decoder.Decrypt(user.PrivateKey);
+
+            return new UserSession(db, user, privateKeyBytes);
+        }
+    }
+}
