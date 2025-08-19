08117ada915dce3d38034e802dd2de7d842c20a5
Timofei Zhakov <tima@chemodax.net>
2025-08-19 11:45:50 +0000

    Split ClientSession onto different files all implementing one /partial/ class.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@4060 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ MKW.Core/Client/ClientSessionAdmin.cs
@@ -0,0 +1,44 @@
+﻿using MKW.Core.Client.Notify;
+using MKW.Core.Cryptography;
+using MKW.Core.Storage;
+using System.Security.Cryptography;
+
+namespace MKW.Core.Client
+{
+    public partial class ClientSession : IDisposable
+    {
+        public UserInfo PromoteAdmin(string password)
+        {
+            SystemCredentialsManager credManager = new SystemCredentialsManager();
+
+            UserCredentials userCreds = UserCredentials.Create(password);
+            SystemCredentials systemCreds = credManager.GenerateCredentials(userCreds);
+
+            AdminUser user = new AdminUser
+            {
+                Id = Guid.NewGuid(),
+                PublicKey = systemCreds.PublicKey,
+                PrivateKey = systemCreds.PrivateKey,
+                Salt = systemCreds.Salt,
+            };
+
+            db.UpdateAdmin(user);
+
+            return UserInfo.FromDatabaseUser(user);
+        }
+
+        public AdminSession OpenAdmin(string password)
+        {
+            AdminUser admin = db.GetAdmin();
+
+            UserCredentials creds = UserCredentials.Open(password, admin.Salt);
+
+            using SymmetricTransformer decoder = SymmetricTransformer.Open(creds.GetEncodingHash(),
+                                                                           creds.ExportSalt());
+
+            byte[] privateKeyBytes = decoder.Decrypt(admin.PrivateKey);
+
+            return new AdminSession(db, admin, privateKeyBytes);
+        }
+    }
+}
---
+++ MKW.Core/Client/ClientSessionEntry.cs
@@ -0,0 +1,73 @@
+﻿using MKW.Core.Client.Notify;
+using MKW.Core.Cryptography;
+using MKW.Core.Storage;
+
+namespace MKW.Core.Client
+{
+    public partial class ClientSession : IDisposable
+    {
+        public EntryInfo UpdateEntry(Guid id, EntryPayload? entry)
+        {
+            if (entry == null)
+            {
+                db.UpdateEntry(id, null);
+
+                return new EntryInfo
+                {
+                    Id = id,
+                    Action = ActionInfo.Deleted,
+                    EncodedForUsers = []
+                };
+            }
+            else
+            {
+                DatabaseUser[] users = db.EnumerateUsers().ToArray();
+
+                DatabaseSecretEntry encodedEntry = EncodeEntry(entry, users);
+
+                List<UserInfo> encodedForUsers = [];
+                foreach (DatabaseUser user in users)
+                {
+                    encodedForUsers.Add(UserInfo.FromDatabaseUser(user));
+                }
+
+                DatabaseSecretEntry? oldEntry = db.QueryEntry(id);
+                db.UpdateEntry(id, encodedEntry);
+
+                return new EntryInfo
+                {
+                    Id = id,
+                    Action = oldEntry == null ? ActionInfo.Added : ActionInfo.Updated,
+                    EncodedForUsers = encodedForUsers
+                };
+            }
+        }
+
+        public DatabaseSecretEntry EncodeEntry(EntryPayload payload, IEnumerable<DatabaseUser> users)
+        {
+            using SymmetricTransformer payloadEncoder = SymmetricTransformer.Create();
+
+            byte[] data = payloadEncoder.Encrypt(payload.Data);
+
+            var keys = new Dictionary<Guid, byte[]>();
+
+            foreach (DatabaseUser user in db.EnumerateUsers())
+            {
+                using AsymmetricTransformer keyEncoder = AsymmetricTransformer.Open(user.PublicKey);
+
+                byte[] encyptedKey = keyEncoder.Encrypt(payloadEncoder.ExportKey());
+
+                keys.Add(user.Id, encyptedKey);
+            }
+
+            DatabaseSecretEntry entry = new DatabaseSecretEntry
+            {
+                Keys = keys,
+                Data = data,
+                Salt = payloadEncoder.ExportIV(),
+            };
+
+            return entry;
+        }
+    }
+}
