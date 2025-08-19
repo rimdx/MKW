38af93ff83844da54f3a895f26590372c6270061
Timofei Zhakov <tima@chemodax.net>
2025-08-19 09:34:03 +0000

    Refactoring.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@4056 0083e6b4-6090-234a-836e-88fcc467669c

--- MKW.Core/Client/AdminSession.cs
+++ MKW.Core/Client/AdminSession.cs
@@ -1,18 +1,26 @@
-﻿using MKW.Core.Storage;
+﻿using MKW.Core.Cryptography;
+using MKW.Core.Storage;
 
 namespace MKW.Core.Client
 {
-    public class AdminSession
+    public class AdminSession : IDisposable
     {
         private readonly IDatabase db;
         private readonly AdminUser admin;
-        private readonly byte[] privateKey;
+
+        private readonly AsymmetricTransformer transformer;
 
         public AdminSession(IDatabase db, AdminUser admin, byte[] privateKey)
         {
             this.db = db;
             this.admin = admin;
-            this.privateKey = privateKey;
+
+            transformer = AsymmetricTransformer.Open(admin.PublicKey, privateKey);
+        }
+
+        public void Dispose()
+        {
+            transformer.Dispose();
         }
     }
 }
