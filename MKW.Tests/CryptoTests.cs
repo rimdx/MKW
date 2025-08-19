1ec329c3a861997c118dd117b95383663a7fca35
Timofei Zhakov <tima@chemodax.net>
2025-08-19 09:49:49 +0000

    Experiment with signature.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@4057 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ MKW.Tests/CryptoTests.cs
@@ -0,0 +1,26 @@
+﻿using MKW.Core.Cryptography;
+using NUnit.Framework.Legacy;
+using System.Security.Cryptography;
+
+namespace MKW.Tests
+{
+    public class CryptoTests
+    {
+        [Test]
+        public void DummySignTest()
+        {
+            using AsymmetricTransformer transformer = AsymmetricTransformer.Create();
+
+            byte[] data = EncodingConverter.GetBytes("killmepls");
+
+            byte[] sign1 = transformer.Sign(data);
+            byte[] sign2 = transformer.Sign(data);
+
+            CollectionAssert.AreEqual(sign1, sign2);
+
+            ClassicAssert.IsTrue(transformer.Verify(data, sign1));
+            ClassicAssert.IsFalse(transformer.Verify(data, RandomNumberGenerator.GetBytes(sign1.Length)));
+            ClassicAssert.IsFalse(transformer.Verify(data, EncodingConverter.GetBytes("random123")));
+        }
+    }
+}
