8f4259e4d4a8024df91ad60c05cd8e14ac43628d
Timofei Zhakov <tima@chemodax.net>
2025-08-19 09:18:19 +0000

    Factor-out key generation into SystemCredentialsManager.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@4051 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ MKW.Core/Client/SystemCredentials.cs
@@ -0,0 +1,12 @@
+﻿namespace MKW.Core.Client
+{
+    public record class SystemCredentials
+    {
+        public required byte[] Salt { get; set; }
+
+        public required byte[] PublicKey { get; set; }
+
+        // Encrypted using user's password
+        public required byte[] PrivateKey { get; set; }
+    }
+}
---
+++ MKW.Core/Client/SystemCredentialsManager.cs
@@ -0,0 +1,33 @@
+﻿using MKW.Core.Cryptography;
+
+namespace MKW.Core.Client
+{
+    public class SystemCredentialsManager
+    {
+        public SystemCredentialsManager()
+        {
+        }
+
+        public SystemCredentials GenerateCredentials(UserCredentials userCredentials)
+        {
+            // Generate asymmetric pair of public and private keys
+            using AsymmetricTransformer userKey = AsymmetricTransformer.Create();
+
+            // Symmetric encoder for secret section.
+            using SymmetricTransformer encoder = SymmetricTransformer.Open(userCredentials.GetEncodingHash(),
+                                                                           userCredentials.ExportSalt());
+
+            byte[] privateKeyBytes = userKey.ExportPrivateKey();
+            byte[] privateKeyEncrypted = encoder.Encrypt(privateKeyBytes);
+
+            byte[] publicKeyBytes = userKey.ExportPublicKey();
+
+            return new SystemCredentials
+            {
+                PublicKey = publicKeyBytes,
+                PrivateKey = privateKeyEncrypted,
+                Salt = userCredentials.ExportSalt(),
+            };
+        }
+    }
+}
