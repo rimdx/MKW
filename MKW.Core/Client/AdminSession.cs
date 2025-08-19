8d32f808837d243d839088e5b085133b27d4eef5
Timofei Zhakov <tima@chemodax.net>
2025-08-19 09:32:23 +0000

    Implement creating and opening admins.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@4055 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ MKW.Core/Client/AdminSession.cs
@@ -0,0 +1,18 @@
+﻿using MKW.Core.Storage;
+
+namespace MKW.Core.Client
+{
+    public class AdminSession
+    {
+        private readonly IDatabase db;
+        private readonly AdminUser admin;
+        private readonly byte[] privateKey;
+
+        public AdminSession(IDatabase db, AdminUser admin, byte[] privateKey)
+        {
+            this.db = db;
+            this.admin = admin;
+            this.privateKey = privateKey;
+        }
+    }
+}
