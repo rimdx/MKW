a98884b3f6eb465b6d801d8382b3b23d906e9468
Timofei Zhakov <tima@chemodax.net>
2025-08-20 20:51:44 +0000

    Split CommandLineContextActions up.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@4094 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ MKW/CommandLineContextActions.cs
@@ -0,0 +1,54 @@
+﻿using MKW.Core.Client;
+using MKW.Core.Client.Notify;
+using System.CommandLine;
+
+namespace MKW
+{
+    public partial class CommandLineContext
+    {
+        private void AddUserAction(ParseResult argv)
+        {
+            using ClientSession session = OpenSession(argv);
+
+            UserInfo user = session.PromoteUser(GetPassword(argv));
+
+            Console.WriteLine($"User added with ID: {user.Id}");
+        }
+
+        private void AddEntryAction(ParseResult argv)
+        {
+            string payload = argv.GetRequiredValue(argPayload);
+
+            using ClientSession session = OpenSession(argv);
+
+            EntryInfo entry = session.UpdateEntry(Guid.NewGuid(), new EntryPayload(payload));
+
+            Console.WriteLine($"{entry.Action}: {entry.Id} for {entry.EncodedForUsers.Count} users");
+        }
+
+        private void TouchAction(ParseResult argv)
+        {
+            using ClientSession session = OpenSession(argv);
+        }
+
+        private void ListEntriesAction(ParseResult argv)
+        {
+            using ClientSession session = OpenSession(argv);
+            using UserSession userSession = session.OpenUser(GetPassword(argv));
+
+            foreach (KeyedEntry entry in userSession.EnumerateEntries())
+            {
+                Console.WriteLine($"-- {entry.Id}:");
+
+                if (entry.Payload == null)
+                {
+                    Console.WriteLine($"[hidden]");
+                }
+                else
+                {
+                    Console.WriteLine($"{entry.Payload}");
+                }
+            }
+        }
+    }
+}
