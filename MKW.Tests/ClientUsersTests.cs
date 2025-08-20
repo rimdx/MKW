409916abe94f920de4d31bb750f47ec9ff6bf046
Timofei Zhakov <tima@chemodax.net>
2025-08-20 20:23:35 +0000

    Organise tests.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@4090 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ MKW.Tests/ClientUsersTests.cs
@@ -0,0 +1,66 @@
+﻿using MKW.Core.Client;
+using MKW.Core.Client.Notify;
+using MKW.Core.Storage;
+using MKW.Core.Storage.JSON;
+using NUnit.Framework.Legacy;
+using System.Security.Cryptography;
+
+namespace MKW.Tests
+{
+    public class ClientUsersTests
+    {
+        [Test]
+        public void SimpleAddUserTest()
+        {
+            using MemoryDatabaseSession db = new MemoryDatabaseSession();
+            using ClientSession session = ClientSession.Open(db);
+
+            UserInfo user = session.PromoteUser("whattheheckamidoing");
+
+            IDatabaseUser[] users = db.EnumerateUsers().ToArray();
+            ClassicAssert.AreEqual(1, users.Length);
+
+            ClassicAssert.AreEqual(user.Id, users[0].Id);
+            ClassicAssert.AreEqual(user.PublicKey, users[0].PublicKey);
+        }
+
+        [Test]
+        public void OpenUserTest()
+        {
+            using SandBox sbox = new SandBox();
+            using ClientSession client = sbox.OpenSession();
+
+            UserInfo user = client.PromoteUser("awesomesecretno1willeverguess");
+
+            using UserSession userSession = client.OpenUser(user.Id, "awesomesecretno1willeverguess");
+
+            Assert.Throws<Exception>(
+                () => client.OpenUser(new Guid("{DEADCCCE-69CF-3242-810A-C54B3D490797}"),
+                                       "awesomesecretno1willeverguess")
+            );
+
+            Assert.Throws<CryptographicException>(
+                () => client.OpenUser(user.Id, "randomheckerpwdhaha")
+            );
+        }
+
+        [Test]
+        public void OpenUserTestNoId()
+        {
+            using SandBox sbox = new SandBox();
+            using ClientSession client = sbox.OpenSession();
+
+            UserInfo user1 = client.PromoteUser("cred1");
+            UserInfo user2 = client.PromoteUser("cred2");
+            UserInfo user3 = client.PromoteUser("cred3");
+
+            using UserSession userSession1 = client.OpenUser("cred1");
+            using UserSession userSession2 = client.OpenUser("cred2");
+            using UserSession userSession3 = client.OpenUser("cred3");
+
+            Assert.Throws<Exception>(
+                () => client.OpenUser("nonexistingpassword")
+            );
+        }
+    }
+}
