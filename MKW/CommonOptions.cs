6171ebcb5be0ee1f4fdd5e949c51fb2bfea1ec4a
Timofei Zhakov <tima@chemodax.net>
2025-08-21 12:57:03 +0000

    Group options and arguments in CommonOptions.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@4107 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ MKW/CommonOptions.cs
@@ -0,0 +1,32 @@
+﻿using System.CommandLine;
+
+namespace MKW
+{
+    public static class CommonOptions
+    {
+        public readonly static Argument<string> File = new("file")
+        {
+            Description = "path to the database file",
+        };
+
+        public readonly static Argument<string> Payload = new("payload")
+        {
+            Description = "secret payload of the entry",
+        };
+
+        public readonly static Option<string> Password = new("--password")
+        {
+            Description = "password to perform operation with",
+        };
+
+        public readonly static Option<bool> NonInteractive = new("--non-interactive")
+        {
+            Description = "do no interactive prompting (default is to prompt only if standard input is a terminal device)"
+        };
+
+        public readonly static Option<bool> ForceInteractive = new("--force-interactive")
+        {
+            Description = "do interactive prompting even if standard input is not a terminal device"
+        };
+    }
+}
