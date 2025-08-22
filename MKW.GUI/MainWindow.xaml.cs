8e06c55ba4cdc522938504d4cbe64631f245fa9b
Timofei Zhakov <tima@chemodax.net>
2025-08-22 17:38:22 +0000

    GUI: begin designing main window.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@4169 0083e6b4-6090-234a-836e-88fcc467669c

--- MKW.GUI/MainWindow.xaml.cs
+++ MKW.GUI/MainWindow.xaml.cs
@@ -1,24 +1,29 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
-namespace WMK.GUI
+namespace MKW.GUI
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly MainWindowModel model;
+
         public MainWindow()
         {
+            model = new MainWindowModel();
+            DataContext = model;
             InitializeComponent();
+        }
+
+        private void NewDatabase_Click(object sender, RoutedEventArgs e)
+        {
+            model.NewDatabase();
+        }
+
+        private void OpenDatabase_Click(object sender, RoutedEventArgs e)
+        {
+            model.OpenDatabase();
         }
     }
 }
