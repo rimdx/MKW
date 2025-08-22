b107542668ecd716b3e3c739a5ac44883a1bc2e2
Timofei Zhakov <tima@chemodax.net>
2025-08-22 16:28:51 +0000

    Add and setup blank WPF application for the GUI.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@4165 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ MKW.GUI/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WMK.GUI
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
