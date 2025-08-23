6015d82213b02930b9ee24780ab3ba7f361e6811
Timofei Zhakov <tima@chemodax.net>
2025-08-23 22:32:18 +0000

    GUI: draft separate 'Create Database' dialog, which will initialise database and promote first admin.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@4191 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ MKW.GUI/CreateDatabaseWindowViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+
+namespace MKW.GUI
+{
+    public class CreateDatabaseWindowViewModel : INotifyPropertyChanged
+    {
+        private readonly string path;
+
+        public DatabaseModel? Database { get; private set; }
+
+        public CreateDatabaseWindowViewModel(string path)
+        {
+            this.path = path;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public string Password { get; set; } = "";
+        public string DatabasePath => path;
+
+        public bool DoCreateDatabase()
+        {
+            Database = DatabaseModel.Create(path);
+            Database.CreateAdmin(Password);
+            return true;
+        }
+    }
+}
