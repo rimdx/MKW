f9a721772f56dd352819001bd5d7e9d732c8d223
Timofei Zhakov <tima@chemodax.net>
2025-08-23 17:11:41 +0000

    GUI: Add simple login form.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@4170 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ MKW.GUI/LoginWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using MKW.Core.Storage;
+using System.Windows;
+using System.Windows.Input;
+
+namespace MKW.GUI
+{
+    public partial class LoginWindow : Window
+    {
+        private readonly LoginWindowModel model;
+
+        public LoginWindow(IDatabase database, string databasePath)
+        {
+            model = new LoginWindowModel(database, databasePath);
+            DataContext = model;
+            InitializeComponent();
+        }
+
+        private void OK_Click(object sender, RoutedEventArgs e)
+        {
+            model.DoLogin();
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Keyboard.Focus(UserPasswordInput);
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            model.Dispose();
+        }
+    }
+}
