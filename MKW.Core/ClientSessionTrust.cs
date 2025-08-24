718c7ebe0068d63eebf01e570d58c67bec9967e4
Timofei Zhakov <tima@chemodax.net>
2025-08-24 21:43:14 +0000

    Fix a very stupid bug, by converting IEnumerable from UserTrustProvider.EnumerateUsersTrust() to an array, because this enumerator accesses trustProvider, which is disposed outside of the function.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@4246 0083e6b4-6090-234a-836e-88fcc467669c

--- MKW.Core/ClientSessionTrust.cs
+++ MKW.Core/ClientSessionTrust.cs
@@ -8,9 +8,15 @@ namespace MKW.Core.Client
         public IEnumerable<UserInfo> EnumerateUsersTrust()
         {
             IDatabaseUser admin = Database.OpenAdmin(true);
-            using UserTrustProvider trust = new UserTrustProvider(this, admin);
+            using UserTrustProvider trustProvider = new UserTrustProvider(this, admin);
 
-            return trust.EnumerateUsersTrust();
+            IEnumerable<UserInfo> trust = trustProvider.EnumerateUsersTrust();
+
+            // Convert IEnumerable to an array, before returning from function,
+            // because outside the trustProvider will be disposed.
+            //
+            // Dear .NET, why??
+            return trust.ToArray();
         }
     }
 }
