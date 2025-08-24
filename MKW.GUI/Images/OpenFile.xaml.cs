91b1bfc10037c24b291f386563f7c24bf03a0f98
Timofei Zhakov <tima@chemodax.net>
2025-08-24 18:51:45 +0000

    GUI: add the start page
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@4233 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ MKW.GUI/Images/OpenFile.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace MKW.GUI.Images
+{
+    public partial class OpenFile : Viewbox
+    {
+        public OpenFile()
+        {
+            InitializeComponent();
+        }
+    }
+}
