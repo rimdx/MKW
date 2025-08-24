773a3bdac1adebd834653c8ce55b512066cca799
Timofei Zhakov <tima@chemodax.net>
2025-08-24 18:51:45 +0000

    GUI: add the start page
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@4233 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ MKW.GUI/Images/NewDocument.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace MKW.GUI.Images
+{
+    public partial class NewDocument : Viewbox
+    {
+        public NewDocument()
+        {
+            InitializeComponent();
+        }
+    }
+}
