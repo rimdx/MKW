994cc78fbf709858bfdf9c5227ccf1675dfe99a3
Timofei Zhakov <tima@chemodax.net>
2025-08-24 21:53:09 +0000

    Test UserTrustController
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@4248 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ MKW.Tests/TrustControllerTests.cs
@@ -0,0 +1,34 @@
+﻿using MKW.Core.Client;
+using MKW.Core.Client.Notify;
+using NUnit.Framework.Legacy;
+
+namespace MKW.Tests
+{
+    public class TrustControllerTests
+    {
+        [Test]
+        public void SimpleTest()
+        {
+            using SandBox sbox = new SandBox();
+            using ClientSession client = sbox.OpenSession();
+
+            using UserSession user = sbox.CreateUser(client, "123", out UserInfo userInfo, Trust.None);
+            using AdminSession admin = sbox.OpenAdmin(client);
+
+            using UserTrustController trustController = new UserTrustController(client, user);
+
+            UserInfo adminInfo = client.GetAdminInfo();
+
+            adminInfo.Trust = Trust.FullTrust;
+            userInfo.Trust = Trust.None;
+
+            CollectionAssert.AreEqual(
+                new UserInfo[]
+                {
+                    adminInfo,
+                    userInfo
+                },
+                trustController.EnumerateUsersTrust());
+        }
+    }
+}
