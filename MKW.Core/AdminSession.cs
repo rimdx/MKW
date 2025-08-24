b26f55b16c68372bab0517134fd4aadec65adfa4
Timofei Zhakov <tima@chemodax.net>
2025-08-24 12:15:00 +0000

    Make admins inherit users, since they have the same functionality as the users by design, except they are stored separately, and have some extra permission.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@4198 0083e6b4-6090-234a-836e-88fcc467669c

--- MKW.Core/AdminSession.cs
+++ MKW.Core/AdminSession.cs
@@ -1,31 +1,25 @@
 ﻿using MKW.Core.Client.Notify;
-using MKW.Core.Cryptography;
 using MKW.Core.Storage;
 
 namespace MKW.Core.Client
 {
-    public class AdminSession : IDisposable
+    public class AdminSession : UserSession, IDisposable
     {
-        private readonly ClientSession client;
         private readonly IDatabaseAdmin admin;
-
-        private readonly AsymmetricTransformer transformer;
 
         public AdminSession(ClientSession client /* reference */,
                             IDatabaseAdmin admin /* reference */,
                             ReadOnlySpan<byte> privateKey)
+            : base(client, admin, privateKey)
         {
-            this.client = client;
             this.admin = admin;
-
-            transformer = AsymmetricTransformer.Open(admin.PublicKey.Span, privateKey);
         }
 
         public void UpdateTrust(Guid userId, Trust trust)
         {
             IDatabaseUser user = client.Database.OpenUser(userId, true);
 
-            ReadOnlyMemory<byte> signature = transformer.Sign(user.PublicKey.Span);
+            ReadOnlyMemory<byte> signature = Transformer.Sign(user.PublicKey.Span);
 
             if (trust == Trust.FullTrust)
             {
@@ -41,11 +35,6 @@ namespace MKW.Core.Client
             }
 
             admin.Save();
-        }
-
-        public void Dispose()
-        {
-            transformer.Dispose();
         }
     }
 }
