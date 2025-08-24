37f96c424d93d87242cfd45837b564a09618cc06
Timofei Zhakov <tima@chemodax.net>
2025-08-24 21:20:27 +0000

    Factor out UserTrustProvider, which will compute trust of a user (me) and report to the client.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@4243 0083e6b4-6090-234a-836e-88fcc467669c

--- MKW.Core/ClientSessionTrust.cs
+++ MKW.Core/ClientSessionTrust.cs
@@ -1,5 +1,4 @@
 ﻿using MKW.Core.Client.Notify;
-using MKW.Core.Cryptography;
 using MKW.Core.Storage;
 
 namespace MKW.Core.Client
@@ -9,36 +8,9 @@ namespace MKW.Core.Client
         public IEnumerable<UserInfo> EnumerateUsersTrust()
         {
             IDatabaseUser admin = Database.OpenAdmin(true);
+            UserTrustProvider trust = new UserTrustProvider(this, admin);
 
-            using AsymmetricTransformer adminKey = AsymmetricTransformer.Open(admin.PublicKey.Span);
-
-            foreach (IDatabaseUser user in EnumerateDatabaseUsers())
-            {
-                UserInfo notify = UserInfo.FromDatabaseUser(user);
-                notify.Trust = VerifyTrust(user, admin, adminKey);
-                yield return notify;
-            }
-        }
-
-        private Trust VerifyTrust(IDatabaseUser user, IDatabaseUser admin, AsymmetricTransformer adminKey)
-        {
-            // TODO: this is insecure!
-            if (user.Id.IsAdmin)
-            {
-                // We always trust admins.
-                // TODO: sign admins to handle potential fake admins
-                return Trust.FullTrust;
-            }
-
-            foreach (ReadOnlyMemory<byte> trust in admin.EnumerateTrust())
-            {
-                if (adminKey.Verify(user.PublicKey.Span, trust.Span))
-                {
-                    return Trust.FullTrust;
-                }
-            }
-
-            return Trust.None;
+            return trust.EnumerateUsersTrust();
         }
     }
 }
---
+++ MKW.Core/UserTrustProvider.cs
@@ -0,0 +1,58 @@
+﻿using MKW.Core.Client.Notify;
+using MKW.Core.Cryptography;
+using MKW.Core.Storage;
+
+namespace MKW.Core.Client
+{
+    public class UserTrustProvider : IDisposable
+    {
+        private readonly ClientSession client;
+        private readonly IDatabaseUser me;
+        private readonly AsymmetricTransformer key;
+
+        public UserTrustProvider(ClientSession client, IDatabaseUser user)
+        {
+            this.client = client;
+            me = user;
+            key = AsymmetricTransformer.Open(user.PublicKey.Span);
+        }
+
+        public IEnumerable<UserInfo> EnumerateUsersTrust()
+        {
+            IDatabaseUser admin = client.Database.OpenAdmin(true);
+
+            foreach (IDatabaseUser user in client.EnumerateDatabaseUsers())
+            {
+                UserInfo notify = UserInfo.FromDatabaseUser(user);
+                notify.Trust = VerifyTrust(user);
+                yield return notify;
+            }
+        }
+
+        private Trust VerifyTrust(IDatabaseUser user)
+        {
+            // TODO: this is insecure!
+            if (user.Id.IsAdmin)
+            {
+                // We always trust admins.
+                // TODO: sign admins to handle potential fake admins
+                return Trust.FullTrust;
+            }
+
+            foreach (ReadOnlyMemory<byte> trust in me.EnumerateTrust())
+            {
+                if (key.Verify(user.PublicKey.Span, trust.Span))
+                {
+                    return Trust.FullTrust;
+                }
+            }
+
+            return Trust.None;
+        }
+
+        public void Dispose()
+        {
+            key.Dispose();
+        }
+    }
+}
