a92d7a0c846b84be111190027662a3374e4963e3
Timofei Zhakov <tima@chemodax.net>
2025-08-25 23:57:05 +0000

    GUI: Move DatabaseUserViewModel into its own file.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@4298 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ MKW.GUI/DatabaseUserViewModel.cs
@@ -0,0 +1,14 @@
+﻿using MKW.Core.Storage;
+using MKW.GUI.Images;
+
+namespace MKW.GUI
+{
+    public class DatabaseUserViewModel
+    {
+        public required bool IsAdmin { get; init; }
+        public required UserId Id { get; init; }
+        public required string Name { get; init; }
+
+        public object Icon => IsAdmin ? new Admin() : new User();
+    }
+}
