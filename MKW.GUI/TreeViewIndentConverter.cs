cb0924ff2db020358c263412d6684fc1e8c35fef
Timofei Zhakov <tima@chemodax.net>
2025-08-25 20:34:58 +0000

    GUI: configure treeview
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@4280 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ MKW.GUI/TreeViewIndentConverter.cs
@@ -0,0 +1,37 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace MKW.GUI
+{
+    public class TreeViewIndentConverter : IValueConverter
+    {
+        public double IndentSize { get; set; } = 16.0;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is TreeViewItem item)
+            {
+                int level = 0;
+                DependencyObject parent = VisualTreeHelper.GetParent(item);
+
+                while (parent != null)
+                {
+                    if (parent is TreeViewItem) level++;
+                    parent = VisualTreeHelper.GetParent(parent);
+                }
+
+                return level * IndentSize;
+            }
+
+            return 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
