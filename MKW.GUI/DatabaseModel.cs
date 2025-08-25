ee080d8337263ebb22ebabbbd52318f3649d1d7f
Timofei Zhakov <tima@chemodax.net>
2025-08-25 23:56:41 +0000

    Rename LoginUser to DatabaseUserViewModel.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@4297 0083e6b4-6090-234a-836e-88fcc467669c

--- MKW.GUI/DatabaseModel.cs
+++ MKW.GUI/DatabaseModel.cs
@@ -62,9 +62,9 @@ namespace MKW.GUI
             OnEntriesChanged?.Invoke(this, new EventArgs());
         }
 
-        public IEnumerable<LoginUser> EnumerateUsers()
+        public IEnumerable<DatabaseUserViewModel> EnumerateUsers()
         {
-            yield return new LoginUser
+            yield return new DatabaseUserViewModel
             {
                 Id = UserId.Admin(),
                 IsAdmin = true,
@@ -73,7 +73,7 @@ namespace MKW.GUI
 
             foreach (UserInfo user in Client.EnumerateUsers())
             {
-                yield return new LoginUser
+                yield return new DatabaseUserViewModel
                 {
                     Id = user.Id,
                     IsAdmin = false,
