b29bf24f1b33b06a46307a7b877a371418457ea6
Timofei Zhakov <tima@chemodax.net>
2025-08-26 14:27:29 +0000

    GUI: draft UI of the 'Promote User' dialog and bind it to the main window.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@4310 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ MKW.GUI/PromoteUserWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using MKW.GUI.Model;
+using System.Windows;
+
+namespace MKW.GUI
+{
+    public partial class PromoteUserWindow : Window
+    {
+        private readonly DatabaseModel database;
+
+        public PromoteUserWindow(DatabaseModel database)
+        {
+            this.database = database;
+            DataContext = this;
+            InitializeComponent();
+        }
+
+        private void PasswordInput_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void PasswordRepeatInput_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void OK_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
