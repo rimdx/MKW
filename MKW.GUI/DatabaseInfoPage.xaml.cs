5f364695fea1e7c5578cb8b88ec8550dd799ab17
Timofei Zhakov <tima@chemodax.net>
2025-08-26 16:12:36 +0000

    GUI: add DatabaseInfoPage.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@4318 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ MKW.GUI/DatabaseInfoPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+
+namespace MKW.GUI
+{
+    public partial class DatabaseInfoPage : UserControl
+    {
+        private readonly DatabaseViewModel model;
+
+        public DatabaseInfoPage(DatabaseViewModel model)
+        {
+            this.model = model;
+            DataContext = this;
+            InitializeComponent();
+        }
+    }
+}
