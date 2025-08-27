8760608b9f941d25866fd2de37914f06edc6ffc9
Timofei Zhakov <tima@chemodax.net>
2025-08-27 20:07:58 +0000

    Describe ITrustProvider
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@4354 0083e6b4-6090-234a-836e-88fcc467669c

--- MKW.Core/ITrustProvider.cs
+++ MKW.Core/ITrustProvider.cs
@@ -2,6 +2,23 @@
 
 namespace MKW.Core.Client
 {
+    /// <summary>
+    /// Used to evaluate trust network.
+    ///
+    /// Each user in a trust network may have references to other users which they
+    /// explicitly trust (use <c>EnumerateExplicitlyTrustedUsers()</c> function to
+    /// retrieve such or <c>VerifyTrust()</c> to check whether one's public key is
+    /// trust or not).
+    ///
+    /// Based on explicit trusts of each user, a complete trust network can be
+    /// built.  Trust providers should implement a recursive algorithm that walks
+    /// through explicitly trusted users, and then their children, etc.  Use the
+    /// <c>EnumerateImplicitlyTrustedUsers()</c> method for such.
+    ///
+    /// In most of the cases, if no custom behaviour is needed, user should rely
+    /// on the <c>EnumerateImplicitlyTrustedUsers()</c> method in order to retrieve
+    /// all the users they trust, i.e. when encoding entries.
+    /// </summary>
     public interface ITrustProvider : IDisposable
     {
         IEnumerable<UserInfo> EnumerateImplicitlyTrustedUsers();
