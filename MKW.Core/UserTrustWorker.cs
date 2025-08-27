e72303e24107aacba2563668b8b947c849c9c336
Timofei Zhakov <tima@chemodax.net>
2025-08-27 11:53:20 +0000

    Drop old trust implementation.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@4344 0083e6b4-6090-234a-836e-88fcc467669c

--- MKW.Core/UserTrustWorker.cs
+++ MKW.Core/UserTrustWorker.cs
@@ -81,11 +81,11 @@ namespace MKW.Core.Client
             if (me.TrustProvider == null)
             {
                 using UserTrustProvider trustProvider = new UserTrustProvider(client, me.DatabaseUser);
-                return trustProvider.VerifyTrust2(node.DatabaseUser);
+                return trustProvider.VerifyTrust(node.DatabaseUser);
             }
             else
             {
-                return me.TrustProvider.VerifyTrust2(node.DatabaseUser);
+                return me.TrustProvider.VerifyTrust(node.DatabaseUser);
             }
         }
 
