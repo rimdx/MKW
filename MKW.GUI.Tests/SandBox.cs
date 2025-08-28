73243581e14b452ff09f59f1c0cfad5a2b246d0b
Timofei Zhakov <tima@chemodax.net>
2025-08-28 15:18:51 +0000

    GUI:Tests: test RecentFilesService.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@4373 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ MKW.GUI.Tests/SandBox.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Win32;
+
+namespace MKW.GUI.Tests
+{
+    public class SandBox : IDisposable
+    {
+        public static readonly string TestRootKey =
+            Path.Combine(RegistryKeys.RootKeyPath, "UnitTesting");
+
+        public RegistryService RegistryService;
+
+        public SandBox()
+        {
+            Registry.CurrentUser.DeleteSubKey(TestRootKey);
+            RegistryService = new RegistryService(TestRootKey);
+        }
+
+        public void Dispose()
+        {
+            RegistryService.Dispose();
+        }
+    }
+}
