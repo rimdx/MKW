f9676f3d74c82e41198a2690c2a35d7fb08326c0
Timofei Zhakov <tima@chemodax.net>
2025-08-28 13:08:40 +0000

    GUI: factor out RecentFilesCollectionViewModel.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@4360 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ MKW.GUI/RecentFilesCollectionViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.ObjectModel;
+
+namespace MKW.GUI
+{
+    public class RecentFilesCollectionViewModel : ObservableCollection<RecentFileItemViewModel>, IDisposable
+    {
+        private readonly RecentFilesService recentFilesService;
+
+        public RecentFilesCollectionViewModel(RecentFilesService recentFilesService)
+        {
+            this.recentFilesService = recentFilesService;
+            this.recentFilesService.RecentFilesChanged += RecentFilesChanged;
+            RefreshRecentFiles();
+        }
+
+        private void RecentFilesChanged(object? sender, EventArgs e)
+        {
+            RefreshRecentFiles();
+        }
+
+        private void RefreshRecentFiles()
+        {
+            Clear();
+
+            foreach (string file in recentFilesService.EnumerateRecentFiles())
+            {
+                Add(new RecentFileItemViewModel(file));
+            }
+        }
+
+        public void Dispose()
+        {
+            recentFilesService.RecentFilesChanged -= RecentFilesChanged;
+        }
+    }
+}
