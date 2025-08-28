da1b3a11c6fdfeb04008fa65b420be8c1b433681
Timofei Zhakov <tima@chemodax.net>
2025-08-28 12:42:06 +0000

    GUI: add RecentFileItemViewModel
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@4357 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ MKW.GUI/RecentFileItemViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace MKW.GUI
+{
+    public class RecentFileItemViewModel
+    {
+        public string FullPath { get; }
+
+        public RecentFileItemViewModel(string fullPath)
+        {
+            FullPath = fullPath;
+        }
+    }
+}
