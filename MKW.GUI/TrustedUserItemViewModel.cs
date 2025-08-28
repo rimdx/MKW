d8f6f5b16dfc244638f01cb297d820a00a84285f
Timofei Zhakov <tima@chemodax.net>
2025-08-28 21:54:07 +0000

    GUI: display trusted users on the 'user properties' dialog.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@4412 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ MKW.GUI/TrustedUserItemViewModel.cs
@@ -0,0 +1,29 @@
+﻿using MKW.Core.Client.Notify;
+using MKW.Core.Storage;
+using MKW.GUI.Images;
+
+namespace MKW.GUI
+{
+    public class TrustedUserItemViewModel
+    {
+        private readonly UserInfo user;
+
+        public UserId UserId => user.Id;
+
+        public object Icon
+        {
+            get
+            {
+                if (user.Id.IsAdmin)
+                    return new Admin();
+                else
+                    return new User();
+            }
+        }
+
+        public TrustedUserItemViewModel(UserInfo user)
+        {
+            this.user = user;
+        }
+    }
+}
