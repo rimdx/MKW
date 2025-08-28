4a49465a70bc6578ca6fbed54bb543e711ccb331
Timofei Zhakov <tima@chemodax.net>
2025-08-28 13:04:11 +0000

    GUI: recent files: display fancier text (filename+path).
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@4359 0083e6b4-6090-234a-836e-88fcc467669c

--- MKW.GUI/RecentFileItemViewModel.cs
+++ MKW.GUI/RecentFileItemViewModel.cs
@@ -1,8 +1,13 @@
-﻿namespace MKW.GUI
+﻿using System.IO;
+
+namespace MKW.GUI
 {
     public class RecentFileItemViewModel
     {
         public string FullPath { get; }
+        public string FileName => Path.GetFileName(FullPath);
+
+        public string FileNameWithFullPath => $"{FileName} ({FullPath})";
 
         public RecentFileItemViewModel(string fullPath)
         {
