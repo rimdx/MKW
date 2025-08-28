6bdec94e2f15e2c6cec0665a960699789a72c868
Timofei Zhakov <tima@chemodax.net>
2025-08-28 00:12:08 +0000

    GUI: minimal recent files support, showed in menu items.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@4355 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ MKW.GUI/Services/RegistryKeys.cs
@@ -0,0 +1,9 @@
+﻿namespace MKW.GUI
+{
+    public static class RegistryKeys
+    {
+        public const string RootKeyPath = @"Software\Multi-Key Wallet";
+        public const string RecentFilesKeyName = "";
+
+    }
+}
---
+++ MKW.GUI/Services/RegistryService.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Win32;
+
+namespace MKW.GUI
+{
+    public class RegistryService : IDisposable
+    {
+        private RegistryKey rootKey;
+
+        public RegistryService()
+        {
+            rootKey = Registry.CurrentUser.CreateSubKey(RegistryKeys.RootKeyPath);
+        }
+
+        public string[] GetRecentFiles()
+        {
+            return GetValue<string[]>(RegistryKeys.RecentFilesKeyName, []);
+        }
+
+        public void SetRecentFiles(string[] recentFiles)
+        {
+            SetValue(RegistryKeys.RecentFilesKeyName, recentFiles);
+        }
+
+        private T GetValue<T>(string key, T defaultValue) where T : class
+        {
+            object? value = rootKey.GetValue(key);
+
+            if (value == null)
+            {
+                return defaultValue;
+            }
+            else
+            {
+                if (value is T typedValue)
+                {
+                    return typedValue;
+                }
+                else
+                {
+                    return defaultValue;
+                }
+            }
+        }
+
+        private void SetValue<T>(string key, T value) where T : class
+        {
+            rootKey.SetValue(key, value);
+        }
+
+        public void Dispose()
+        {
+            rootKey.Dispose();
+        }
+    }
+}
