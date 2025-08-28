5a252f68addcd01cb9b01b9379528437c3881bc7
Timofei Zhakov <tima@chemodax.net>
2025-08-28 20:16:57 +0000

    GUI: factor out DatabaseUserCollectionViewModel class.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@4403 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ MKW.GUI/DatabaseUserCollectionViewModel.cs
@@ -0,0 +1,37 @@
+﻿using MKW.GUI.Model;
+using System.Collections.ObjectModel;
+
+namespace MKW.GUI
+{
+    public class DatabaseUserCollectionViewModel : ObservableCollection<DatabaseUserModel>, IDisposable
+    {
+        private readonly DatabaseModel database;
+
+        public DatabaseUserCollectionViewModel(DatabaseModel database)
+        {
+            database.OnUsersChanged += Database_OnUsersChanged;
+            RefreshUsers();
+            this.database = database;
+        }
+
+        private void Database_OnUsersChanged(object? sender, EventArgs e)
+        {
+            RefreshUsers();
+        }
+
+        private void RefreshUsers()
+        {
+            Clear();
+
+            foreach (DatabaseUserModel user in database.EnumerateUsers())
+            {
+                Add(user);
+            }
+        }
+
+        public void Dispose()
+        {
+            database.OnUsersChanged -= Database_OnUsersChanged;
+        }
+    }
+}
