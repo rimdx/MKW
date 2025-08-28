d3f329a4e78f2cf859db4faecc4cff2f912c464d
Timofei Zhakov <tima@chemodax.net>
2025-08-28 14:52:33 +0000

    GUI: factor out management of common file operation dialogs into a static class FileDialogUtils, which can create and setup dialogs for saving database and opening database.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@4365 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ MKW.GUI/FileDialogUtils.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Win32;
+
+namespace MKW.GUI
+{
+    public static class FileDialogUtils
+    {
+        public static FileDialog CreateOpenDatabaseDialog()
+        {
+            return new OpenFileDialog
+            {
+                DefaultExt = ".mkw",
+                Filter = "Multi-Key Wallet Database File|*.mkw"
+            };
+        }
+
+        public static FileDialog CreateSaveDatabaseDialog()
+        {
+            return new SaveFileDialog
+            {
+                FileName = "New Database",
+                DefaultExt = ".mkw",
+                Filter = "Multi-Key Wallet Database File|*.mkw"
+            };
+        }
+    }
+}
