e13d15adf581aa13b875a6ad3eb8607a8b02683e
Timofei Zhakov <tima@chemodax.net>
2025-08-28 20:13:04 +0000

    GUI: factor out DatabaseEntryCollectionViewModel class.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@4402 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ MKW.GUI/DatabaseEntryCollectionViewModel.cs
@@ -0,0 +1,43 @@
+﻿using MKW.Core.Client;
+using MKW.GUI.Model;
+using System.Collections.ObjectModel;
+
+namespace MKW.GUI
+{
+    public class DatabaseEntryCollectionViewModel : ObservableCollection<DatabaseEntryModel>, IDisposable
+    {
+        private readonly DatabaseModel database;
+
+        public DatabaseEntryCollectionViewModel(DatabaseModel database)
+        {
+            this.database = database;
+
+            database.OnEntriesChanged += Database_OnEntriesChanged;
+            RefreshEntries();
+        }
+
+        private void Database_OnEntriesChanged(object? sender, EventArgs e)
+        {
+            RefreshEntries();
+        }
+
+        public void RefreshEntries()
+        {
+            Clear();
+
+            foreach (UserEntry entry in database.User!.EnumerateEntries())
+            {
+                Add(new DatabaseEntryModel
+                {
+                    Id = entry.Id,
+                    Payload = entry.OpenPayload()?.ToString()
+                });
+            }
+        }
+
+        public void Dispose()
+        {
+            database.OnEntriesChanged -= Database_OnEntriesChanged;
+        }
+    }
+}
