1e783255a8edfb64a6b507dd74b35e872ecede6f
Timofei Zhakov <tima@chemodax.net>
2025-08-28 15:07:46 +0000

    GUI:Tests: add tests on the GUI project.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@4370 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ MKW.GUI.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace MKW.GUI.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
