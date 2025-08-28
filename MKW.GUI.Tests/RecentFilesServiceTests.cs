5c1ff557ce5c8e68f5ac011590241d5c0c74803a
Timofei Zhakov <tima@chemodax.net>
2025-08-28 15:18:51 +0000

    GUI:Tests: test RecentFilesService.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@4373 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ MKW.GUI.Tests/RecentFilesServiceTests.cs
@@ -0,0 +1,53 @@
+﻿namespace MKW.GUI.Tests
+{
+    [TestFixture]
+    public class RecentFilesServiceTests
+    {
+        [Test]
+        public void SimpleTest()
+        {
+            using SandBox sbox = new SandBox();
+            RecentFilesService recentFilesService = new RecentFilesService(sbox.RegistryService);
+
+            CollectionAssert.IsEmpty(recentFilesService.EnumerateRecentFiles());
+
+            recentFilesService.OnFileOpened(@"F:\path\to\file1.txt");
+            recentFilesService.OnFileOpened(@"F:\path\to\file2.txt");
+
+            CollectionAssert.AreEqual(
+                new[]
+                {
+                    @"F:\path\to\file2.txt",
+                    @"F:\path\to\file1.txt",
+                },
+                recentFilesService.EnumerateRecentFiles());
+        }
+
+        [Test]
+        public void OverloadTest()
+        {
+            using SandBox sbox = new SandBox();
+            RecentFilesService recentFilesService = new RecentFilesService(sbox.RegistryService);
+
+            recentFilesService.OnFileOpened(@"F:\path\to\file1.txt");
+            recentFilesService.OnFileOpened(@"F:\path\to\file2.txt");
+            recentFilesService.OnFileOpened(@"F:\path\to\file3.txt");
+            recentFilesService.OnFileOpened(@"F:\path\to\file4.txt");
+            recentFilesService.OnFileOpened(@"F:\path\to\file5.txt");
+            recentFilesService.OnFileOpened(@"F:\path\to\file6.txt");
+            recentFilesService.OnFileOpened(@"F:\path\to\file7.txt");
+            recentFilesService.OnFileOpened(@"F:\path\to\file8.txt");
+
+            CollectionAssert.AreEqual(
+                new[]
+                {
+                    @"F:\path\to\file8.txt",
+                    @"F:\path\to\file7.txt",
+                    @"F:\path\to\file6.txt",
+                    @"F:\path\to\file5.txt",
+                    @"F:\path\to\file4.txt",
+                },
+                recentFilesService.EnumerateRecentFiles());
+        }
+    }
+}
