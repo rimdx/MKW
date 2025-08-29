4956ebe4fed1d8cab4f342d82e87a5a3c943c223
Timofei Zhakov <tima@chemodax.net>
2025-08-29 13:42:53 +0000

    Implement simple base32 encoder and test it.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@4417 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ MKW.Core.Common/Base32EncodeTransformer.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Security.Cryptography;
+
+namespace MKW.Core.Common
+{
+    public class Base32EncodeTransformer : ICryptoTransform
+    {
+        private readonly BitArray bits;
+
+        public bool CanReuseTransform => true;
+        public bool CanTransformMultipleBlocks => false;
+
+        public int InputBlockSize => Base32BitConvert.InputChunkSize;
+        public int OutputBlockSize => Base32BitConvert.OutputChunkSize;
+
+        public Base32EncodeTransformer()
+        {
+            bits = new BitArray(InputBlockSize * OutputBlockSize);
+        }
+
+        public int TransformBlock(byte[] inputBuffer, int inputOffset, int inputCount,
+                                  byte[] outputBuffer, int outputOffset)
+        {
+            // convert raw args to spans
+            ReadOnlySpan<byte> inputSpan = new ReadOnlySpan<byte>(inputBuffer, inputOffset, inputCount);
+            Span<byte> outputSpan = new Span<byte>(outputBuffer, outputOffset, OutputBlockSize);
+
+            return EncodeChunk(inputSpan, outputSpan);
+        }
+
+        public byte[] TransformFinalBlock(byte[] inputBuffer, int inputOffset, int inputCount)
+        {
+            ReadOnlySpan<byte> inputSpan = new ReadOnlySpan<byte>(inputBuffer, inputOffset, inputCount);
+
+            if (inputCount == 0)
+            {
+                // base32 doesn't need final flush for empty buffer
+                return [];
+            }
+            else
+            {
+                byte[] outputBuffer = new byte[OutputBlockSize];
+                Span<byte> outputSpan = new Span<byte>(outputBuffer);
+
+                int count = EncodeChunk(inputSpan, outputSpan);
+                // fill the rest with padding
+                Base32BitConvert.WritePadding(outputSpan.Slice(count));
+
+                return outputBuffer;
+            }
+        }
+
+        private int EncodeChunk(ReadOnlySpan<byte> inputSpan, Span<byte> outputSpan)
+        {
+            Base32BitConvert.BufferToBits(inputSpan, bits);
+
+            int count = Base32BitConvert.EncodeChunk(bits, inputSpan.Length * 8, outputSpan);
+
+            return count;
+        }
+
+        public void Dispose()
+        {
+            /* no-op */
+        }
+    }
+}
