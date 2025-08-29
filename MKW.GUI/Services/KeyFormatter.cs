fa362aa3a4d55ceea4887f586cf5295923fdb466
Timofei Zhakov <tima@chemodax.net>
2025-08-29 18:13:46 +0000

    GUI: display public key on the 'user properties' dialog.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@4422 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ MKW.GUI/Services/KeyFormatter.cs
@@ -0,0 +1,38 @@
+﻿using MKW.Core.Common;
+using System.Text;
+
+namespace MKW.GUI.Services
+{
+    public class KeyFormatter
+    {
+        private readonly int lineLength;
+
+        public KeyFormatter(int lineLength)
+        {
+            this.lineLength = lineLength;
+        }
+
+        public string GetString(ReadOnlySpan<byte> data)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            string base32string = Base32Convert.Encode(data);
+
+            int i = 0;
+            foreach (char c in base32string)
+            {
+                if (i >= lineLength)
+                {
+                    sb.AppendLine();
+                    i = 0;
+                }
+
+                sb.Append(c);
+
+                i++;
+            }
+
+            return sb.ToString();
+        }
+    }
+}
