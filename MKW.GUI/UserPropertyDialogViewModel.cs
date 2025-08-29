b35dfcb579ce74ed4d7615a4f8db5b71fda2e106
Timofei Zhakov <tima@chemodax.net>
2025-08-29 18:13:46 +0000

    GUI: display public key on the 'user properties' dialog.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@4422 0083e6b4-6090-234a-836e-88fcc467669c

--- MKW.GUI/UserPropertyDialogViewModel.cs
+++ MKW.GUI/UserPropertyDialogViewModel.cs
@@ -1,15 +1,18 @@
 ﻿using MKW.Core.Client.Notify;
 using MKW.GUI.Model;
+using MKW.GUI.Services;
 
 namespace MKW.GUI
 {
     public class UserPropertyDialogViewModel : ViewModelBase
     {
         private readonly UserInfo user;
+        private readonly KeyFormatter keyFormatter;
 
         public UserPropertyDialogViewModel(DatabaseModel database, UserInfo user /* reference */)
         {
             this.user = user;
+            keyFormatter = new KeyFormatter(50);
             TrustedUsers = new TrustedUsersCollectionViewModel(database, user.Id);
         }
 
@@ -21,5 +24,7 @@ namespace MKW.GUI
         {
             return true;
         }
+
+        public string PublicKey => keyFormatter.GetString(user.PublicKey.Span);
     }
 }
