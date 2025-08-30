f3d756233a305d9860c0901c8bacf322a687565e
Timofei Zhakov <tima@chemodax.net>
2025-08-30 15:49:23 +0000

    Factor out IAdminController interface, containing several admin actions, and AdminController class implementing each of these.  ClientSession now simply wraps one into another.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@4445 0083e6b4-6090-234a-836e-88fcc467669c

--- MKW.Core.Client/ClientSessionAdmin.cs
+++ MKW.Core.Client/ClientSessionAdmin.cs
@@ -1,6 +1,4 @@
 ﻿using MKW.Core.Client.Notify;
-using MKW.Core.Cryptography;
-using MKW.Core.Storage;
 
 namespace MKW.Core.Client
 {
@@ -8,40 +6,17 @@ namespace MKW.Core.Client
     {
         public UserInfo PromoteAdmin(string password)
         {
-            SystemCredentialsManager credManager = new SystemCredentialsManager();
-
-            UserCredentials userCreds = UserCredentials.Create(password);
-            SystemCredentials systemCreds = credManager.GenerateCredentials(userCreds);
-
-            IDatabaseUser admin = Database.CreateAdmin();
-
-            admin.PublicKey = systemCreds.PublicKey;
-            admin.PrivateKey = systemCreds.PrivateKey;
-            admin.Salt = systemCreds.Salt;
-
-            admin.Save();
-
-            return UserInfo.FromDatabaseUser(admin);
+            return adminController.PromoteAdmin(password);
         }
 
         public AdminSession OpenAdmin(string password)
         {
-            IDatabaseUser admin = Database.OpenAdmin(false);
-
-            UserCredentials creds = UserCredentials.Open(password, admin.Salt);
-
-            using SymmetricTransformer decoder = SymmetricTransformer.Open(creds.GetSecretKey().Span,
-                                                                           creds.ExportSalt().Span);
-
-            Memory<byte> privateKeyBytes = decoder.Decrypt(admin.PrivateKey.Span);
-
-            return new AdminSession(this, admin, privateKeyBytes.Span);
+            return adminController.OpenAdmin(password);
         }
 
         public UserInfo GetAdminInfo()
         {
-            IDatabaseUser admin = Database.OpenAdmin(true);
-            return UserInfo.FromDatabaseUser(admin);
+            return adminController.GetAdminInfo();
         }
     }
 }
