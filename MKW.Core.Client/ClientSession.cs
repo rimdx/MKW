a45046e153db46bf8a073e6b11406f3bfeae33a6
Timofei Zhakov <tima@chemodax.net>
2025-08-30 15:53:22 +0000

    Implement the ClientSession class in a singe file.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@4446 0083e6b4-6090-234a-836e-88fcc467669c

--- MKW.Core.Client/ClientSession.cs
+++ MKW.Core.Client/ClientSession.cs
@@ -1,8 +1,10 @@
-﻿using MKW.Core.Storage;
+﻿using MKW.Core.Client.Notify;
+using MKW.Core.Cryptography;
+using MKW.Core.Storage;
 
 namespace MKW.Core.Client
 {
-    public partial class ClientSession : IDisposable
+    public class ClientSession : IUserController, IAdminController, IEntryController, IDisposable
     {
         public IDatabase Database { get; }
 
@@ -44,6 +46,144 @@ namespace MKW.Core.Client
             ClientSession client = new ClientSession(db, ownsDb);
             client.PromoteAdmin(adminPassword);
             return client;
+        }
+
+        internal IEnumerable<IDatabaseUser> EnumerateDatabaseUsers()
+        {
+            yield return Database.OpenAdmin(true);
+
+            foreach (IDatabaseUser user in Database.EnumerateUsers())
+            {
+                yield return user;
+            }
+        }
+
+        internal IDatabaseUser OpenDatabaseUser(UserId id, bool readOnly)
+        {
+            if (id.IsAdmin)
+            {
+                return Database.OpenAdmin(readOnly);
+            }
+            else
+            {
+                return Database.OpenUser(id, readOnly);
+            }
+        }
+
+        // todo: ITrustProvider
+
+        public IEnumerable<UserInfo> EnumerateUsersTrust()
+        {
+            IDatabaseUser admin = Database.OpenAdmin(true);
+            using UserTrustProvider trustProvider = new UserTrustProvider(this, admin);
+
+            IEnumerable<UserInfo> trust = trustProvider.EnumerateImplicitlyTrustedUsers();
+
+            // Convert IEnumerable to an array, before returning from function,
+            // because outside the trustProvider will be disposed.
+            //
+            // Dear .NET, why??
+            return trust.ToArray();
+        }
+
+        public IEnumerable<UserInfo> EnumerateUsersTrust(UserId userId)
+        {
+            IDatabaseUser user = OpenDatabaseUser(userId, true);
+            using UserTrustProvider trustProvider = new UserTrustProvider(this, user);
+
+            foreach (UserInfo trust in trustProvider.EnumerateImplicitlyTrustedUsers())
+            {
+                yield return trust;
+            }
+        }
+
+        // IUserController
+
+        public UserInfo PromoteUser(string password)
+        {
+            return userController.PromoteUser(password);
+        }
+
+        public UserSession OpenUser(UserId id, string password)
+        {
+            return userController.OpenUser(id, password);
+        }
+
+        public IEnumerable<UserInfo> EnumerateUsers()
+        {
+            foreach (UserInfo user in userController.EnumerateUsers())
+            {
+                yield return user;
+            }
+        }
+
+        public UserSession OpenUser(string password)
+        {
+            return userController.OpenUser(password);
+        }
+
+        public UserSession OpenUser(IDatabaseUser user, UserCredentials creds)
+        {
+            return userController.OpenUser(user, creds);
+        }
+
+        public UserInfo GetUserInfo(UserId id)
+        {
+            return userController.GetUserInfo(id);
+        }
+
+        // IAdminController
+
+        public UserInfo PromoteAdmin(string password)
+        {
+            return adminController.PromoteAdmin(password);
+        }
+
+        public AdminSession OpenAdmin(string password)
+        {
+            return adminController.OpenAdmin(password);
+        }
+
+        public UserInfo GetAdminInfo()
+        {
+            return adminController.GetAdminInfo();
+        }
+
+        // IEntryController
+
+        private EntryController OpenEntryController()
+        {
+            return new EntryController(this, Database.OpenAdmin(true));
+        }
+
+        public IEntrySession OpenEntry(EntryId id)
+        {
+            using EntryController entryController = OpenEntryController();
+            return entryController.OpenEntry(id);
+        }
+
+        public IEntrySession CreateEntry(EntryId id)
+        {
+            using EntryController entryController = OpenEntryController();
+            return entryController.CreateEntry(id);
+        }
+
+        public IEntrySession CreateEntry()
+        {
+            using EntryController entryController = OpenEntryController();
+            return entryController.CreateEntry();
+        }
+
+        public EntryInfo DeleteEntry(EntryId id)
+        {
+            using EntryController entryController = OpenEntryController();
+            return entryController.DeleteEntry(id);
+        }
+
+        public EntryInfo UpdateEntry(EntryId id, EntryPayload? payload)
+        {
+            using EntryController entryController = OpenEntryController();
+            return entryController.UpdateEntry(id, payload);
         }
 
         public void Dispose()
