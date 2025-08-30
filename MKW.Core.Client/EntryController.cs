4bfd13cf3aec5bfd77fa3deca91d3dea065d72b6
Timofei Zhakov <tima@chemodax.net>
2025-08-30 14:07:58 +0000

    Factor out EntryController class to delegate entry management to it.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@4436 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ MKW.Core.Client/EntryController.cs
@@ -0,0 +1,89 @@
+﻿using MKW.Core.Client.Notify;
+using MKW.Core.Storage;
+
+namespace MKW.Core.Client
+{
+    public class EntryController : IDisposable
+    {
+        private readonly ClientSession client;
+        private readonly IDatabaseUser user;
+
+        public EntryController(ClientSession client /* reference */,
+                               IDatabaseUser user)
+        {
+            this.client = client;
+            this.user = user;
+        }
+
+        private ITrustProvider OpenTrustProvider()
+        {
+            return new UserTrustProvider(client, user);
+        }
+
+        public Entry OpenEntry(EntryId id)
+        {
+            IDatabaseEntry dbEntry = client.Database.OpenEntry(id, false);
+            return new Entry(client, OpenTrustProvider() /* move */, dbEntry);
+        }
+
+        public Entry CreateEntry(EntryId id)
+        {
+            IDatabaseEntry dbEntry = client.Database.CreateEntry(id);
+            dbEntry.Save();
+            return new Entry(client, OpenTrustProvider() /* move */, dbEntry);
+        }
+
+        public Entry CreateEntry() => CreateEntry(EntryId.Create());
+
+        public EntryInfo DeleteEntry(EntryId id)
+        {
+            client.Database.DeleteEntry(id);
+
+            return new EntryInfo
+            {
+                Id = id,
+                Action = ActionInfo.Deleted,
+                EncodedForUsers = []
+            };
+        }
+
+        public Entry EnsureEntry(EntryId id, out bool created)
+        {
+            created = !client.Database.HasEntry(id);
+
+            if (created)
+            {
+                return CreateEntry(id);
+            }
+            else
+            {
+                return OpenEntry(id);
+            }
+        }
+
+        public EntryInfo UpdateEntry(EntryId id, EntryPayload? payload)
+        {
+            if (payload == null)
+            {
+                return DeleteEntry(id);
+            }
+            else
+            {
+                using Entry entry = EnsureEntry(id, out bool created);
+
+                EntryInfo notify = entry.UpdatePayload(payload);
+
+                return new EntryInfo
+                {
+                    Id = notify.Id,
+                    EncodedForUsers = notify.EncodedForUsers,
+                    Action = created ? ActionInfo.Added : ActionInfo.Updated,
+                };
+            }
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
