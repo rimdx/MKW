46392f9e746d213e5dd818992a5c04c6562862c4
Timofei Zhakov <tima@chemodax.net>
2025-08-30 19:30:09 +0000

    Add simple EntryEditor.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@4458 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ MKW.Core.Editor.Tests/SandBox.cs
@@ -0,0 +1,66 @@
+﻿using MKW.Core.Client;
+using MKW.Core.Client.Notify;
+using MKW.Core.Storage;
+using MKW.Core.Storage.JSON;
+
+namespace MKW.Core.Editor.Tests
+{
+    public class SandBox : IDisposable
+    {
+        public string DatabasePath { get; }
+        public string AdminSecret => "adminsecret123";
+
+        public SandBox(bool init = true)
+        {
+            DatabasePath = Path.GetFullPath("./test.json");
+
+            // todo: safer way?
+            File.Delete("./test.json");
+
+            if (init)
+            {
+                using JSONDatabaseSession db = JSONDatabaseSession.Create(DatabasePath);
+                using ClientSession client = ClientSession.Create(db, AdminSecret);
+            }
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public IDatabase OpenDatabase()
+        {
+            return JSONDatabaseSession.Open(DatabasePath, false);
+        }
+
+        public ClientSession OpenSession()
+        {
+            return ClientSession.Open(OpenDatabase(), true);
+        }
+
+        public AdminSession OpenAdmin(ClientSession client)
+        {
+            return client.OpenAdmin(AdminSecret);
+        }
+
+        public UserSession CreateUser(ClientSession client,
+                                      string password,
+                                      out UserInfo user,
+                                      bool trusted = true)
+        {
+            using AdminSession admin = OpenAdmin(client);
+
+            user = client.PromoteUser(password);
+
+            UserSession userSession = client.OpenUser(user.Id, password);
+
+            if (trusted)
+            {
+                admin.UpdateTrust(user.Id, Trust.ExplicitTrust);
+                userSession.UpdateTrust(admin.Id, Trust.ExplicitTrust);
+            }
+
+            return userSession;
+        }
+    }
+}
---
+++ MKW.Core.Editor/EntryEditor.cs
@@ -0,0 +1,63 @@
+﻿using MKW.Core.Client;
+using MKW.Core.Client.Notify;
+using MKW.Core.Storage;
+
+namespace MKW.Core.Editor
+{
+    public class EntryEditor : IEntrySession, IDisposable
+    {
+        private readonly IEntrySession proxy;
+
+        public EntryId Id => proxy.Id;
+
+        private EntryPayload? payloadBefore;
+        private EntryPayload? payloadAfter;
+
+        public EntryEditor(IEntrySession proxy /* move */)
+        {
+            this.proxy = proxy;
+
+            payloadBefore = proxy.OpenPayload();
+            payloadAfter = payloadBefore;
+        }
+
+        public IEnumerable<UserInfo> EnumerateEncodedForUsers()
+        {
+            foreach (UserInfo user in EnumerateEncodedForUsers())
+            {
+                yield return user;
+            }
+        }
+
+        public EntryPayload? OpenPayload()
+        {
+            return payloadAfter;
+        }
+
+        public EntryInfo UpdatePayload(EntryPayload payload)
+        {
+            payloadAfter = payload;
+
+            return new EntryInfo
+            {
+                Id = proxy.Id,
+                Action = ActionInfo.Deleted, // todo:
+                EncodedForUsers = [] // todo:
+            };
+        }
+
+        public void Commit()
+        {
+            if (payloadBefore != payloadAfter && payloadAfter != null)
+            {
+                proxy.UpdatePayload(payloadAfter);
+                payloadBefore = payloadAfter;
+            }
+        }
+
+        public void Dispose()
+        {
+            proxy.Dispose();
+        }
+    }
+}
