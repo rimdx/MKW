a2b3087535e6b88cd2a4c0e6ad65c9361577661f
Timofei Zhakov <tima@chemodax.net>
2025-08-30 17:58:41 +0000

    Factor out UserEntryController.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@4449 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ MKW.Core.Client/UserEntryController.cs
@@ -0,0 +1,95 @@
+﻿using MKW.Core.Client.Notify;
+using MKW.Core.Storage;
+
+namespace MKW.Core.Client
+{
+    public class UserEntryController : IEntryController, IDisposable
+    {
+        private readonly ClientSession client;
+        private readonly IDatabase database;
+        private readonly UserSession user;
+
+        public UserEntryController(ClientSession client /* reference */,
+                                   IDatabase database /* reference */,
+                                   UserSession user /* reference */)
+        {
+            this.client = client;
+            this.database = database;
+            this.user = user;
+        }
+
+        public IEntrySession OpenEntry(EntryId id)
+        {
+            IDatabaseEntry dbEntry = database.OpenEntry(id, false);
+            return new UserEntry(client, user, dbEntry);
+        }
+
+        public IEntrySession CreateEntry(EntryId id)
+        {
+            IDatabaseEntry dbEntry = database.CreateEntry(id);
+            dbEntry.Save();
+            return new UserEntry(client, user, dbEntry);
+        }
+
+        public IEntrySession CreateEntry() => CreateEntry(EntryId.Create());
+
+        public EntryInfo DeleteEntry(EntryId id)
+        {
+            database.DeleteEntry(id);
+
+            return new EntryInfo
+            {
+                Id = id,
+                Action = ActionInfo.Deleted,
+                EncodedForUsers = []
+            };
+        }
+
+        public IEntrySession EnsureEntry(EntryId id, out bool created)
+        {
+            created = !database.HasEntry(id);
+
+            if (created)
+            {
+                return CreateEntry(id);
+            }
+            else
+            {
+                return OpenEntry(id);
+            }
+        }
+
+        public EntryInfo UpdateEntry(EntryId id, EntryPayload? payload)
+        {
+            if (payload == null)
+            {
+                return DeleteEntry(id);
+            }
+            else
+            {
+                using IEntrySession entry = EnsureEntry(id, out bool created);
+
+                EntryInfo notify = entry.UpdatePayload(payload);
+
+                return new EntryInfo
+                {
+                    Id = notify.Id,
+                    EncodedForUsers = notify.EncodedForUsers,
+                    Action = created ? ActionInfo.Added : ActionInfo.Updated,
+                };
+            }
+        }
+
+        public IEnumerable<IEntrySession> EnumerateEntries()
+        {
+            foreach (IDatabaseEntry entry in database.EnumerateEntries())
+            {
+                yield return new UserEntry(client, user, entry);
+            }
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
