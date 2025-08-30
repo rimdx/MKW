58804af010d3e7fa876572cee83ae4882c0f7ba3
Timofei Zhakov <tima@chemodax.net>
2025-08-30 19:30:09 +0000

    Add simple EntryEditor.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@4458 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ MKW.Core.Editor.Tests/EntryEditorTests.cs
@@ -0,0 +1,60 @@
+﻿using MKW.Core.Client;
+using MKW.Core.Storage;
+using NUnit.Framework.Legacy;
+
+namespace MKW.Core.Editor.Tests
+{
+    public class EntryEditorTests
+    {
+        [Test]
+        public void SimpleTest()
+        {
+            using SandBox sbox = new SandBox();
+            using ClientSession client = sbox.OpenSession();
+
+            using UserSession user = sbox.CreateUser(client, "123", out _);
+
+            EntryId id = EntryId.Create();
+            using EntryEditor editor = new EntryEditor(user.CreateEntry(id));
+
+            {
+                using IEntrySession entry = user.OpenEntry(id);
+                ClassicAssert.AreEqual(null,
+                                       entry.OpenPayload());
+                ClassicAssert.AreEqual(null,
+                                       editor.OpenPayload());
+            }
+
+            editor.UpdatePayload(new EntryPayload("data1"));
+
+            {
+                using IEntrySession entry = user.OpenEntry(id);
+                ClassicAssert.AreEqual(null,
+                                       entry.OpenPayload());
+                ClassicAssert.AreEqual(new EntryPayload("data1"),
+                                       editor.OpenPayload());
+            }
+
+            editor.Commit();
+
+            {
+                using IEntrySession entry = user.OpenEntry(id);
+                ClassicAssert.AreEqual(new EntryPayload("data1"),
+                                       entry.OpenPayload());
+                ClassicAssert.AreEqual(new EntryPayload("data1"),
+                                       editor.OpenPayload());
+            }
+
+            editor.Commit();
+            editor.UpdatePayload(new EntryPayload("data2"));
+
+            {
+                using IEntrySession entry = user.OpenEntry(id);
+                ClassicAssert.AreEqual(new EntryPayload("data1"),
+                                       entry.OpenPayload());
+                ClassicAssert.AreEqual(new EntryPayload("data2"),
+                                       editor.OpenPayload());
+            }
+        }
+    }
+}
