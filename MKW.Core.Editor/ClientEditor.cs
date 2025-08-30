1fdbdc9d9efa132b0a70a0a5a252d82de9e180ce
Timofei Zhakov <tima@chemodax.net>
2025-08-30 19:01:43 +0000

    MKW.Core.Editor: add ClientEditor.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@4455 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ MKW.Core.Editor/ClientEditor.cs
@@ -0,0 +1,18 @@
+﻿using MKW.Core.Client;
+
+namespace MKW.Core.Editor
+{
+    public class ClientEditor : IDisposable
+    {
+        private readonly ClientSession client;
+
+        public ClientEditor(ClientSession client)
+        {
+            this.client = client;
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
