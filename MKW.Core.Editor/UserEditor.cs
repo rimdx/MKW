18070ae4707f165cfe00fe58775530dbc1380bba
Timofei Zhakov <tima@chemodax.net>
2025-08-30 22:25:10 +0000

    First version of UserEditor.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@4468 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ MKW.Core.Editor/UserEditor.cs
@@ -0,0 +1,102 @@
+﻿using MKW.Core.Client;
+using MKW.Core.Client.Notify;
+using MKW.Core.Storage;
+
+namespace MKW.Core.Editor
+{
+    public class UserEditor
+        : IUserSession
+        , IEntryController
+        , ITrustProvider
+        , ITrustController
+        , IDisposable
+    {
+        private readonly IUserSession proxy;
+
+        public UserId Id => proxy.Id;
+
+        public UserEditor(IUserSession proxy)
+        {
+            this.proxy = proxy;
+        }
+
+        public void Commit()
+        {
+        }
+
+        public void Dispose()
+        {
+            proxy.Dispose();
+        }
+
+        // IEntryController
+
+        public IEntrySession CreateEntry()
+        {
+            return proxy.CreateEntry();
+        }
+
+        public IEntrySession CreateEntry(EntryId id)
+        {
+            return proxy.CreateEntry(id);
+        }
+
+        public EntryInfo DeleteEntry(EntryId id)
+        {
+            return proxy.DeleteEntry(id);
+        }
+
+        public IEnumerable<IEntrySession> EnumerateEntries()
+        {
+            foreach (IEntrySession entry in proxy.EnumerateEntries())
+            {
+                yield return entry;
+            }
+        }
+
+        public IEntrySession OpenEntry(EntryId id)
+        {
+            return proxy.OpenEntry(id);
+        }
+
+        public EntryInfo UpdateEntry(EntryId id, EntryPayload? payload)
+        {
+            return proxy.UpdateEntry(id, payload);
+        }
+
+        // ITrustProvider
+
+        public IEnumerable<UserInfo> EnumerateImplicitlyTrustedUsers()
+        {
+            foreach (UserInfo user in proxy.EnumerateImplicitlyTrustedUsers())
+            {
+                yield return user;
+            }
+        }
+
+        public IEnumerable<UserInfo> EnumerateExplicitlyTrustedUsers()
+        {
+            foreach (UserInfo user in proxy.EnumerateExplicitlyTrustedUsers())
+            {
+                yield return user;
+            }
+        }
+
+        public Trust GetExplicitTrust(ReadOnlySpan<byte> publicKey)
+        {
+            return proxy.GetExplicitTrust(publicKey);
+        }
+
+        public Trust GetImplicitTrust(UserId userId)
+        {
+            return proxy.GetImplicitTrust(userId);
+        }
+
+        // ITrustController
+
+        public void UpdateTrust(UserId userId, Trust trust)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
