8f029c75459427d80bf1af2ff0d293de4c4396fe
Timofei Zhakov <tima@chemodax.net>
2025-08-30 12:09:09 +0000

    GUI:User Properties: Add simple warning, telling that the user is not verified.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@4429 0083e6b4-6090-234a-836e-88fcc467669c

--- MKW.GUI/UserPropertyDialogViewModel.cs
+++ MKW.GUI/UserPropertyDialogViewModel.cs
@@ -9,12 +9,22 @@ namespace MKW.GUI
         private readonly DatabaseModel database;
         private readonly UserInfo user;
         private readonly KeyFormatter keyFormatter;
+        private readonly Trust trust;
 
         public UserPropertyDialogViewModel(DatabaseModel database, UserInfo user /* reference */)
         {
             this.database = database;
             this.user = user;
             keyFormatter = new KeyFormatter(50);
+
+            trust = Trust.None;
+            foreach (UserInfo userTrust in database.User!.EnumerateUsersTrust())
+            {
+                if (userTrust.Id == user.Id)
+                {
+                    trust = userTrust.Trust;
+                }
+            }
         }
 
         public string UserId => user.Id.ToString();
@@ -27,5 +37,9 @@ namespace MKW.GUI
         }
 
         public string PublicKey => keyFormatter.GetString(user.PublicKey.Span);
+
+        public bool IsUntrusted => trust == Trust.None;
+        public bool IsUser => !database.User!.Id.IsAdmin;
+        public bool IsAdmin => database.User!.Id.IsAdmin;
     }
 }
