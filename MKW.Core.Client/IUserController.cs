948433d91d402ddd2b84756ab2cae609f4cdf484
Timofei Zhakov <tima@chemodax.net>
2025-08-30 14:34:41 +0000

    Add IUserController interface.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@4442 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ MKW.Core.Client/IUserController.cs
@@ -0,0 +1,15 @@
+﻿using MKW.Core.Client.Notify;
+using MKW.Core.Cryptography;
+using MKW.Core.Storage;
+
+namespace MKW.Core.Client
+{
+    public interface IUserController : IDisposable
+    {
+        UserInfo PromoteUser(string password);
+
+        UserSession OpenUser(string password);
+        UserSession OpenUser(UserId id, string password);
+        UserSession OpenUser(IDatabaseUser user, UserCredentials creds);
+    }
+}
