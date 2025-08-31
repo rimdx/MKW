271e16b3307732ca5d704de2b54be2363f35ed60
Timofei Zhakov <tima@chemodax.net>
2025-08-31 16:18:25 +0000

    Draft resource lifetime management object.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@4478 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ MKW.Core.Common.Tests/ResourceTests.cs
@@ -0,0 +1,61 @@
+﻿using NUnit.Framework.Legacy;
+
+namespace MKW.Core.Common.Tests
+{
+    public class ResourceTests
+    {
+        private interface ITestResource : IResource<ITestResource>
+        {
+            public int Disposed { get; }
+        }
+
+        private class TestResource : Resource<ITestResource>, ITestResource
+        {
+            public int Disposed { get; private set; }
+
+            public override void Dispose(bool disposing)
+            {
+                Disposed++;
+            }
+        }
+
+        [Test]
+        public void SimpleTest()
+        {
+            TestResource resource = new TestResource();
+
+            ClassicAssert.AreEqual(0, resource.Disposed);
+
+            resource.Dispose();
+
+            ClassicAssert.AreEqual(1, resource.Disposed);
+        }
+
+        [Test]
+        public void SimpleMoveTest()
+        {
+            TestResource resource = new TestResource();
+            ClassicAssert.AreEqual(0, resource.Disposed);
+
+            IResource<ITestResource> moved = resource.Move();
+
+            ClassicAssert.AreEqual(0, resource.Disposed);
+            ClassicAssert.AreEqual(0, moved.Value.Disposed);
+
+            moved.Dispose();
+
+            ClassicAssert.AreEqual(1, resource.Disposed);
+            ClassicAssert.AreEqual(1, moved.Value.Disposed);
+
+            resource.Dispose();
+            ClassicAssert.AreEqual(1, resource.Disposed);
+            ClassicAssert.AreEqual(1, moved.Value.Disposed);
+
+            moved.Dispose();
+            resource.Dispose();
+
+            ClassicAssert.AreEqual(1, resource.Disposed);
+            ClassicAssert.AreEqual(1, moved.Value.Disposed);
+        }
+    }
+}
---
+++ MKW.Core.Common/Resource.cs
@@ -0,0 +1,69 @@
+﻿namespace MKW.Core.Common
+{
+    public class Resource<T> : IResource<T>, IDisposable where T : class, IResource<T>
+    {
+        private ResourceState state;
+
+        protected Resource()
+        {
+            state = ResourceState.Original;
+        }
+
+        private Resource(T value, ResourceState state)
+        {
+            this.value = value;
+            this.state = state;
+        }
+
+        private readonly T? value;
+        public T Value
+        {
+            get
+            {
+                if (value == null)
+                {
+                    return (T)(object)this;
+                }
+                else
+                {
+                    return value;
+                }
+            }
+        }
+
+        public static implicit operator T(Resource<T> resource)
+        {
+            return resource.Value;
+        }
+
+        public IResource<T> Reference()
+        {
+            return new Resource<T>(Value, ResourceState.Reference);
+        }
+
+        public IResource<T> Move()
+        {
+            state = ResourceState.OriginalMovedOut;
+            return new Resource<T>(Value, ResourceState.ReferenceOwned);
+        }
+
+        public virtual void Dispose(bool disposing)
+        {
+        }
+
+        public void Dispose()
+        {
+            if (state == ResourceState.Original || state == ResourceState.ReferenceOwned)
+            {
+                if (value != null)
+                {
+                    value.Dispose(true);
+                }
+                else
+                {
+                    Dispose(true);
+                }
+            }
+        }
+    }
+}
