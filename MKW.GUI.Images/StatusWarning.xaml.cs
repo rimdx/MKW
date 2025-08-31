bd279d2b0ff08ecc127d890c626adb461ffa2165
Timofei Zhakov <tima@chemodax.net>
2025-08-31 19:56:29 +0000

    Images: add StatusWarning icon
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@4487 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ MKW.GUI.Images/StatusWarning.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace MKW.GUI.Images
+{
+    public partial class StatusWarning : Viewbox
+    {
+        public StatusWarning()
+        {
+            InitializeComponent();
+        }
+    }
+}
