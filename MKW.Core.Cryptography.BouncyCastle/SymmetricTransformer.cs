a389aeb7fd3c520b898c14c839bbfd16fe7aaecb
Timofei Zhakov <tima@chemodax.net>
2025-08-31 23:48:16 +0000

    BouncyCastleCryptographyProvider: NotImplement ISymmetricTransformer.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@4512 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ MKW.Core.Cryptography.BouncyCastle/SymmetricTransformer.cs
@@ -0,0 +1,45 @@
+﻿
+namespace MKW.Core.Cryptography.BouncyCastle
+{
+    internal class SymmetricTransformer : ISymmetricTransformer, IDisposable
+    {
+        private SymmetricTransformer()
+        {
+        }
+
+        public static SymmetricTransformer Create()
+        {
+            return new SymmetricTransformer();
+        }
+
+        public static SymmetricTransformer Open(ReadOnlySpan<byte> key, ReadOnlySpan<byte> iv)
+        {
+            return new SymmetricTransformer();
+        }
+
+        public Memory<byte> Decrypt(ReadOnlySpan<byte> data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Memory<byte> Encrypt(ReadOnlySpan<byte> data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Memory<byte> ExportIV()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Memory<byte> ExportKey()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
