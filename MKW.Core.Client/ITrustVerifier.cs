e449521f2b1351c6033ffc05d9b1f4f323c329b4
Timofei Zhakov <tima@chemodax.net>
2025-08-31 13:27:20 +0000

    Add ITrustVerifier type that does low-level calculations of trust of each specific public key.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@4474 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ MKW.Core.Client/ITrustVerifier.cs
@@ -0,0 +1,9 @@
+﻿using MKW.Core.Client.Notify;
+
+namespace MKW.Core.Client
+{
+    public interface ITrustVerifier : IDisposable
+    {
+        Trust GetTrust(ReadOnlySpan<byte> otherPublicKey);
+    }
+}
---
+++ MKW.Core.Client/UserTrustVerifier.cs
@@ -0,0 +1,36 @@
+﻿using MKW.Core.Client.Notify;
+using MKW.Core.Cryptography;
+using MKW.Core.Storage;
+
+namespace MKW.Core.Client
+{
+    public class UserTrustVerifier : ITrustVerifier, IDisposable
+    {
+        private readonly IAsymmetricPublicTransformer publicKey;
+        private readonly IDatabaseUser me;
+
+        public UserTrustVerifier(IDatabaseUser me)
+        {
+            this.me = me;
+            publicKey = AsymmetricTransformer.Open(me.PublicKey.Span);
+        }
+
+        public Trust GetTrust(ReadOnlySpan<byte> otherPublicKey)
+        {
+            foreach (ReadOnlyMemory<byte> trust in me.EnumerateTrust())
+            {
+                if (publicKey.Verify(otherPublicKey, trust.Span))
+                {
+                    return Trust.ExplicitTrust;
+                }
+            }
+
+            return Trust.None;
+        }
+
+        public void Dispose()
+        {
+            publicKey.Dispose();
+        }
+    }
+}
