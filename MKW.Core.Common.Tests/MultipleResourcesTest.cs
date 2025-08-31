2334fb91edf56a797d1a15d865602a2c277a5238
Timofei Zhakov <tima@chemodax.net>
2025-08-31 17:14:49 +0000

    Test resources with more complex objects.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@4484 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ MKW.Core.Common.Tests/MultipleResourcesTest.cs
@@ -0,0 +1,74 @@
+﻿using NUnit.Framework.Legacy;
+
+namespace MKW.Core.Common.Tests
+{
+    public class MultipleResourcesTest
+    {
+        private interface IA : IResource
+        {
+            int PropA { get; }
+        }
+
+        private interface IB : IResource
+        {
+            int PropB { get; }
+        }
+
+        private class A : Resource<A>, IA, IResource
+        {
+            public int PropA => 11;
+            public int Disposed { get; private set; }
+
+            public override void Dispose(bool disposing)
+            {
+                Disposed++;
+            }
+        }
+
+        private class B : Resource<B>, IB, IResource
+        {
+            public int PropB => 22;
+            public int Disposed { get; private set; }
+
+            public override void Dispose(bool disposing)
+            {
+                Disposed++;
+            }
+        }
+
+        private class Complex : Resource<Complex>, IA, IB, IResource
+        {
+            private readonly Resource<IA> a;
+            private readonly Resource<IB> b;
+
+            public Complex(Resource<IA> a, Resource<IB> b)
+            {
+                this.a = a;
+                this.b = b;
+            }
+
+            public int PropA => a.Value.PropA;
+            public int PropB => b.Value.PropB;
+
+            public override void Dispose(bool disposing)
+            {
+                a.Dispose();
+                b.Dispose();
+            }
+        }
+
+        [Test]
+        public void Test()
+        {
+            A a = new A();
+            B b = new B();
+
+            Complex complex = new Complex(a.Move<IA>(), b.Reference<IB>());
+
+            complex.Dispose();
+
+            ClassicAssert.AreEqual(1, a.Disposed);
+            ClassicAssert.AreEqual(0, b.Disposed);
+        }
+    }
+}
