689358d1f67ef07a6d29ff513f31c51de222ccaf
Timofei Zhakov <tima@chemodax.net>
2025-08-31 20:41:46 +0000

    Add ICryptographyProvider interface and its default implementation; CryptographyProvider.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@4499 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ MKW.Core.Cryptography/CryptographyProvider.cs
@@ -0,0 +1,48 @@
+﻿namespace MKW.Core.Cryptography
+{
+    public class CryptographyProvider : IDisposable, ICryptographyProvider
+    {
+        public CryptographyProvider()
+        {
+        }
+
+        public ISymmetricTransformer CreateSymmetricTransformer()
+        {
+            return SymmetricTransformer.Create();
+        }
+
+        public ISymmetricTransformer OpenSymmetricTransformer(ReadOnlySpan<byte> key, ReadOnlySpan<byte> iv)
+        {
+            return SymmetricTransformer.Open(key, iv);
+        }
+
+        public IAsymmetricPrivateTransformer CreateAsymmetricTransformer()
+        {
+            return AsymmetricTransformer.Create();
+        }
+
+        public IAsymmetricPublicTransformer OpenAsymmetricTransformer(ReadOnlySpan<byte> publicKey)
+        {
+            return AsymmetricTransformer.Open(publicKey);
+        }
+
+        public IAsymmetricPublicTransformer OpenAsymmetricTransformer(ReadOnlySpan<byte> publicKey, ReadOnlySpan<byte> privateKey)
+        {
+            return AsymmetricTransformer.Open(publicKey, privateKey);
+        }
+
+        public UserCredentials CreateUserCredentials(string password)
+        {
+            return UserCredentials.Create(password);
+        }
+
+        public UserCredentials OpenUserCredentials(string password, ReadOnlyMemory<byte> salt)
+        {
+            return UserCredentials.Open(password, salt);
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
