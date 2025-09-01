fac66c574b281b13d4228fdba39c51361ecf1c63
Timofei Zhakov <tima@chemodax.net>
2025-09-01 10:52:12 +0000

    BouncyCastle: NotImplement UserCredentials.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@4518 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ MKW.Core.Cryptography.BouncyCastle/UserCredentials.cs
@@ -0,0 +1,44 @@
+﻿namespace MKW.Core.Cryptography.BouncyCastle
+{
+    public class UserCredentials : IUserCredentials, IDisposable
+    {
+        private readonly string password;
+        private readonly ReadOnlyMemory<byte> salt;
+
+        protected UserCredentials(string password, ReadOnlyMemory<byte> salt)
+        {
+            this.password = password;
+            this.salt = salt;
+        }
+
+        public static UserCredentials Create(string password)
+        {
+            return new UserCredentials(password, GenerateSalt());
+        }
+
+        public static UserCredentials Open(string password, ReadOnlyMemory<byte> salt)
+        {
+            return new UserCredentials(password, salt);
+        }
+
+        public ReadOnlyMemory<byte> ExportSalt()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Memory<byte> GetSecretKey()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static ReadOnlyMemory<byte> GenerateSalt()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
