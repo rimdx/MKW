61052af139559963daf6d2bcf3daf6f7990fdbf0
Timofei Zhakov <tima@chemodax.net>
2025-09-01 10:26:19 +0000

    Add StreamDisown.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@4513 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ MKW.Core.Common/StreamDisown.cs
@@ -0,0 +1,56 @@
+﻿namespace MKW.Core.Common
+{
+    /// <summary>
+    /// Similar to Subversion's svn_stream_disown().
+    /// </summary>
+    public class StreamDisown : Stream
+    {
+        private readonly Stream proxy;
+
+        public StreamDisown(Stream proxy)
+        {
+            this.proxy = proxy;
+        }
+
+        public override bool CanRead => proxy.CanRead;
+        public override bool CanSeek => proxy.CanSeek;
+        public override bool CanWrite => proxy.CanWrite;
+        public override long Length => proxy.Length;
+
+        public override long Position
+        { 
+            get => proxy.Position;
+            set => proxy.Position = value;
+        }
+
+        public override void Flush()
+        {
+            proxy.Flush();
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            return proxy.Read(buffer, offset, count);
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            return proxy.Seek(offset, origin);
+        }
+
+        public override void SetLength(long value)
+        {
+            proxy.SetLength(value);
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            proxy.Write(buffer, offset, count);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            /* no-op */
+        }
+    }
+}
