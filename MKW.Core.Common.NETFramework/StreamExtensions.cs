50703c80dcfb508d1da76b0f76a0cfa09a23faf4
Timofei Zhakov <tima@chemodax.net>
2025-09-01 22:20:04 +0000

    Add a shared project to patch .NET framework and implement several stream extension in here.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@4540 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ MKW.Core.Common.NETFramework/StreamExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System.Buffers;
+
+namespace MKW.Core.Common
+{
+    public static class StreamExtensions
+    {
+        public static int Read(this Stream stream, Span<byte> buffer)
+        {
+            byte[] sharedBuffer = ArrayPool<byte>.Shared.Rent(buffer.Length);
+
+            try
+            {
+                int numRead = stream.Read(sharedBuffer, 0, buffer.Length);
+
+                Span<byte> span = new Span<byte>(sharedBuffer);
+                span.CopyTo(buffer);
+
+                return span.Length;
+            }
+            finally
+            {
+                ArrayPool<byte>.Shared.Return(sharedBuffer);
+            }
+        }
+
+        public static void Write(this Stream stream, ReadOnlySpan<byte> buffer)
+        {
+            byte[] sharedBuffer = ArrayPool<byte>.Shared.Rent(buffer.Length);
+
+            try
+            {
+                buffer.CopyTo(sharedBuffer);
+                stream.Write(sharedBuffer, 0, buffer.Length);
+            }
+            finally
+            {
+                ArrayPool<byte>.Shared.Return(sharedBuffer);
+            }
+        }
+    }
+}
