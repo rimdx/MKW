d3675b1506ef88638ff529bdd6c091b607028796
Timofei Zhakov <tima@chemodax.net>
2025-09-01 13:29:59 +0000

    BouncyCastle: implement UserCredentials.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@4524 0083e6b4-6090-234a-836e-88fcc467669c

--- MKW.Core.Cryptography.BouncyCastle/UserCredentials.cs
+++ MKW.Core.Cryptography.BouncyCastle/UserCredentials.cs
@@ -1,44 +1,58 @@
-﻿namespace MKW.Core.Cryptography.BouncyCastle
+﻿using Org.BouncyCastle.Asn1.Nist;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Generators;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Security;
+
+namespace MKW.Core.Cryptography.BouncyCastle
 {
     public class UserCredentials : IUserCredentials, IDisposable
     {
-        private readonly string password;
+        private readonly ReadOnlyMemory<byte> password;
         private readonly ReadOnlyMemory<byte> salt;
+        private Pkcs5S2ParametersGenerator generator;
 
-        protected UserCredentials(string password, ReadOnlyMemory<byte> salt)
+        protected UserCredentials(ReadOnlyMemory<byte> password, ReadOnlyMemory<byte> salt)
         {
             this.password = password;
             this.salt = salt;
+
+            IDigest digest = DigestUtilities.GetDigest(NistObjectIdentifiers.IdSha256);
+            generator = new Pkcs5S2ParametersGenerator(digest);
         }
 
         public static UserCredentials Create(string password)
         {
-            return new UserCredentials(password, GenerateSalt());
+            return new UserCredentials(EncodingConverter.GetBytes(password), GenerateSalt());
         }
 
         public static UserCredentials Open(string password, ReadOnlyMemory<byte> salt)
         {
-            return new UserCredentials(password, salt);
+            return new UserCredentials(EncodingConverter.GetBytes(password), salt);
         }
 
         public ReadOnlyMemory<byte> ExportSalt()
         {
-            throw new NotImplementedException();
+            return salt.ToArray();
         }
 
         public Memory<byte> GetSecretKey()
         {
-            throw new NotImplementedException();
+            generator.Init(password.Span, salt.Span, 100_000);
+
+            KeyParameter key = (KeyParameter)generator.GenerateDerivedMacParameters(128);
+
+            return key.GetKey();
         }
 
         private static ReadOnlyMemory<byte> GenerateSalt()
         {
-            throw new NotImplementedException();
+            SecureRandom random = new SecureRandom();
+            return SecureRandom.GetNextBytes(random, 16);
         }
 
         public void Dispose()
         {
-            throw new NotImplementedException();
         }
     }
 }
