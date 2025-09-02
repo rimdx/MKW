2c8bc963870e49b5c697e00e02aac64e44ca8b22
Timofei Zhakov <tima@chemodax.net>
2025-09-02 17:55:54 +0000

    Crypt.System: implement ICryptographyProvider.CreateRandomGenerator().
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@4575 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ MKW.Core.Cryptography.System/SystemRandomGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Cryptography;
+
+namespace MKW.Core.Cryptography.System
+{
+    internal class SystemRandomGenerator : IRandomGenerator
+    {
+        public SystemRandomGenerator()
+        {
+        }
+
+        public byte[] NextBytes(int length)
+        {
+            return RandomNumberGenerator.GetBytes(length);
+        }
+    }
+}
