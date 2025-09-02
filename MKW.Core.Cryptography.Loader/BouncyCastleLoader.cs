ba0ff8c0ffb404c3234f96ff1b101438a2b269bf
Timofei Zhakov <tima@chemodax.net>
2025-09-02 18:14:35 +0000

    Add abstract cryptography interface loader.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@4579 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ MKW.Core.Cryptography.Loader/BouncyCastleLoader.cs
@@ -0,0 +1,14 @@
+﻿using MKW.Core.Cryptography.BouncyCastle;
+
+namespace MKW.Core.Cryptography.Loader
+{
+    public static class BouncyCastleLoader
+    {
+        public static bool Supported => true;
+
+        public static ICryptographyProvider Create()
+        {
+            return new BouncyCastleCryptographyProvider();
+        }
+    }
+}
