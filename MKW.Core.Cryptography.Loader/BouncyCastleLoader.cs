9ce4892c80f2d040bdc7dd77a70ae0434be596d0
Timofei Zhakov <tima@chemodax.net>
2025-09-02 18:19:14 +0000

    MKW.Core.Cryptography.Loader: implement creating cryptography providers by provider name.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@4580 0083e6b4-6090-234a-836e-88fcc467669c

--- MKW.Core.Cryptography.Loader/BouncyCastleLoader.cs
+++ MKW.Core.Cryptography.Loader/BouncyCastleLoader.cs
@@ -4,6 +4,8 @@ namespace MKW.Core.Cryptography.Loader
 {
     public static class BouncyCastleLoader
     {
+        public const string Name = "BouncyCastle.Cryptography";
+
         public static bool Supported => true;
 
         public static ICryptographyProvider Create()
