c1c1d6249b8cb6c1015a5f3e96819f89d879281c
Timofei Zhakov <tima@chemodax.net>
2025-09-02 18:14:35 +0000

    Add abstract cryptography interface loader.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@4579 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ MKW.Core.Cryptography.Loader/CryptographyProvider.cs
@@ -0,0 +1,21 @@
+﻿namespace MKW.Core.Cryptography.Loader
+{
+    public static class CryptographyProvider
+    {
+        public static ICryptographyProvider Create()
+        {
+            if (SystemCryptographyLoader.Supported)
+            {
+                return SystemCryptographyLoader.Create();
+            }
+            else if (BouncyCastleLoader.Supported)
+            {
+                return BouncyCastleLoader.Create();
+            }
+            else
+            {
+                throw new NotSupportedException();
+            }
+        }
+    }
+}
---
+++ MKW.Core.Cryptography.Loader/SystemCryptographyLoader.cs
@@ -0,0 +1,21 @@
+﻿namespace MKW.Core.Cryptography.Loader
+{
+    public class SystemCryptographyLoader
+    {
+#if NETFRAMEWORK
+        public static bool Supported => false;
+
+        public static ICryptographyProvider Create()
+        {
+            throw new NotSupportedException();
+        }
+#else
+        public static bool Supported => true;
+
+        public static ICryptographyProvider Create()
+        {
+            return new System.CryptographyProvider();
+        }
+#endif
+    }
+}
