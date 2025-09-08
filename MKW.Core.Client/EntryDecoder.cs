9e345831936ad7870d974bd7a5c7533323ff4e84
Timofei Zhakov <tima@chemodax.net>
2025-09-08 13:43:26 +0000

    Factor out EntryDecoder.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@4670 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ MKW.Core.Client/EntryDecoder.cs
@@ -0,0 +1,40 @@
+﻿using MKW.Core.Cryptography;
+using MKW.Core.Storage;
+
+namespace MKW.Core.Client
+{
+    public class EntryDecoder : IDisposable
+    {
+        private readonly ICryptographyProvider crypto;
+        private readonly UserSession user;
+
+        public EntryDecoder(ICryptographyProvider crypto,
+                            UserSession user)
+        {
+            this.crypto = crypto;
+            this.user = user;
+        }
+
+        public EntryPayload? DecodeEntry(IDatabaseEntry entry)
+        {
+            if (entry.Keys.TryGetValue(user.Id, out ReadOnlyMemory<byte> encodedKey) == false)
+            {
+                // No key for this user, cannot decode the entry
+                return null;
+            }
+
+            Memory<byte> decryptedKey = user.Transformer.Decrypt(encodedKey.Span);
+
+            using ISymmetricTransformer dataDecoder = crypto.OpenSymmetricTransformer(
+                decryptedKey.Span, entry.Salt.Span);
+
+            Memory<byte> decryptedData = dataDecoder.Decrypt(entry.Data.Span);
+
+            return new EntryPayload(decryptedData);
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
