a4d84efc92f23fa7f4d92392d526f8b7dbf8dd04
Timofei Zhakov <tima@chemodax.net>
2025-09-09 21:11:31 +0000

    UserEntry: add separate creation and opening, and hide its constructor.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@4680 0083e6b4-6090-234a-836e-88fcc467669c

--- MKW.Core.Client/UserEntry.cs
+++ MKW.Core.Client/UserEntry.cs
@@ -13,15 +13,37 @@ namespace MKW.Core.Client
         protected readonly EntryDecoder decoder;
         protected readonly EntrySharer sharer;
 
-        public UserEntry(ClientSession client,
-                         ICryptographyProvider crypto,
-                         UserSession user,
-                         IDatabaseEntry entry)
+        protected UserEntry(ClientSession client,
+                            ICryptographyProvider crypto,
+                            UserSession user,
+                            IDatabaseEntry entry)
             : base(client, crypto, user.TrustController, entry)
         {
             this.user = user;
             decoder = new EntryDecoder(crypto, user);
             sharer = new EntrySharer(accessController, decoder, encoder);
+        }
+
+        public static UserEntry Create(ClientSession client,
+                                       ICryptographyProvider crypto,
+                                       UserSession user,
+                                       IDatabaseEntry entry)
+        {
+            return new UserEntry(client,
+                                 crypto,
+                                 user,
+                                 entry);
+        }
+
+        public static UserEntry Open(ClientSession client,
+                                     ICryptographyProvider crypto,
+                                     UserSession user,
+                                     IDatabaseEntry entry)
+        {
+            return new UserEntry(client,
+                                 crypto,
+                                 user,
+                                 entry);
         }
 
         public override EntryPayload? OpenPayload()
