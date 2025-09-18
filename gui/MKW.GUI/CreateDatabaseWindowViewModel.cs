5d3b698df8147f83c95c42e0dc68659b2ea6a005
Ivan.Z <53048645+chemodax@users.noreply.github.com>
2025-09-18 14:00:10 +0000

    GUI: Rename field.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@4803 0083e6b4-6090-234a-836e-88fcc467669c

--- gui/MKW.GUI/CreateDatabaseWindowViewModel.cs
+++ gui/MKW.GUI/CreateDatabaseWindowViewModel.cs
@@ -12,17 +12,17 @@ namespace MKW.GUI
             // TODO: factor out
             // TODO: save latest directory to registry storage
             string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-            m_databasePath = Path.Combine(documentsPath, "New Database.mkw");
+            databasePath = Path.Combine(documentsPath, "New Database.mkw");
         }
 
         public string Password { get; set; }
         public bool IsPasswordMatch { get; set; }
 
-        private string m_databasePath;
+        private string databasePath;
         public string DatabasePath
         {
-            get => m_databasePath;
-            set => SetProperty(ref m_databasePath, value);
+            get => databasePath;
+            set => SetProperty(ref databasePath, value);
         }
 
         public bool DoCreateDatabase()
