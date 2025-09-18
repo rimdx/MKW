7321c92f329d51acdb0bb189d54ddeedd86ff58c
Timofei Zhakov <tima@chemodax.net>
2025-09-18 17:18:54 +0000

    Add wizard framework and implement database creation using it.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@4822 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ gui/MKW.GUI/Wizard/WizardWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System.ComponentModel;
+using System.Windows;
+
+namespace MKW.GUI.Wizard
+{
+    public partial class WizardWindow : Window
+    {
+        private readonly WizardViewModel viewModel;
+
+        public WizardWindow(WizardViewModel viewModel)
+        {
+            this.viewModel = viewModel;
+            DataContext = viewModel;
+            InitializeComponent();
+
+            viewModel.PropertyChanged += ViewModel_PropertyChanged;
+            UpdatePageContent();
+        }
+
+        private void UpdatePageContent()
+        {
+            PageContent.Content = viewModel.CurrentPage;
+        }
+
+        private void ViewModel_PropertyChanged(object? sender, PropertyChangedEventArgs e)
+        {
+            if (e.MatchProperty(nameof(viewModel.CurrentPage)))
+            {
+                UpdatePageContent();
+            }
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                viewModel.Back();
+            }
+            catch (Exception ex)
+            {
+                ErrorReporter.HandleException(ex);
+            }
+        }
+
+        private void Next_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                viewModel.Next();
+            }
+            catch (Exception ex)
+            {
+                ErrorReporter.HandleException(ex);
+            }
+        }
+
+        private void Finish_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (viewModel.Finish())
+                {
+                    Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorReporter.HandleException(ex);
+            }
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (viewModel.Cancel())
+                {
+                    Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorReporter.HandleException(ex);
+            }
+        }
+    }
+}
