d8e1b2800fbb1786c6f40c6cf505b38b43518158
Ivan.Z <53048645+chemodax@users.noreply.github.com>
2025-09-18 21:49:45 +0000

    GUI: Use WPF commanding to implement File | Exit.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@4858 0083e6b4-6090-234a-836e-88fcc467669c

--- gui/MKW.GUI/AppCommands.cs
+++ gui/MKW.GUI/AppCommands.cs
@@ -5,5 +5,6 @@ namespace MKW.GUI
     internal static class AppCommands
     {
         public static RoutedUICommand OpenRecent { get; } = new RoutedUICommand();
+        public static RoutedUICommand Exit { get; } = new RoutedUICommand();
     }
 }
