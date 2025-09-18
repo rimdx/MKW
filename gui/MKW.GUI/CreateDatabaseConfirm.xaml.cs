3face4e463fb82a4d4521270e9b3c612e4b141c7
Timofei Zhakov <tima@chemodax.net>
2025-09-18 20:31:57 +0000

    GUI: draft confirmation page
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@4846 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ gui/MKW.GUI/CreateDatabaseConfirm.xaml.cs
@@ -0,0 +1,17 @@
+﻿using MKW.GUI.Wizard;
+
+namespace MKW.GUI
+{
+    public partial class CreateDatabaseConfirm : WizardPage
+    {
+        private CreateDatabaseWindowViewModel viewModel;
+
+        public CreateDatabaseConfirm(CreateDatabaseWindowViewModel viewModel)
+            : base("Confirm Database Creation")
+        {
+            this.viewModel = viewModel;
+            DataContext = viewModel;
+            InitializeComponent();
+        }
+    }
+}
