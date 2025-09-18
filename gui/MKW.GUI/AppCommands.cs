aa90e34199e51e3df9a9286c9fe1dea91df423b4
Ivan.Z <53048645+chemodax@users.noreply.github.com>
2025-09-18 21:46:26 +0000

    GUI: Use dedicated AppCommands.OpenRecent.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@4857 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ gui/MKW.GUI/AppCommands.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Input;
+
+namespace MKW.GUI
+{
+    internal static class AppCommands
+    {
+        public static RoutedUICommand OpenRecent { get; } = new RoutedUICommand();
+    }
+}
