385892e1b63843c2e820e200c2f7fe78980b8180
Ivan.Z <53048645+chemodax@users.noreply.github.com>
2025-09-19 20:55:47 +0000

    Add PasswordUtils.MeasurePasswordEntropy().
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@4933 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ common/MKW.Common.Tests/PasswordUtilsTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework.Legacy;
+using System.Globalization;
+
+namespace MKW.Common.Tests
+{
+    [TestFixture]
+    [Parallelizable]
+    public class PasswordUtilsTests
+    {
+        [Test]
+        [TestCase("", "0.00")]
+        [TestCase("1", "3.32")]
+        [TestCase("12", "6.64")]
+        [TestCase("test", "18.80")]
+        [TestCase("TEST", "18.80")]
+        [TestCase("Test", "22.80")]
+        public void MeasurePasswordEntropySimpleTest(string password, string expected)
+        {
+            double actual = PasswordUtils.MeasurePasswordEntropy(password);
+
+            ClassicAssert.AreEqual(
+                expected.ToString(),
+                string.Format(CultureInfo.InvariantCulture, "{0:0.00}", actual));
+        }
+    }
+}
