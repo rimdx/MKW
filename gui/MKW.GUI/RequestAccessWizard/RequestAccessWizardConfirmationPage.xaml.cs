10ff0e58a082f2b2a7d909ddfbc3bcbf474bc9f6
Timofei Zhakov <tima@chemodax.net>
2025-09-19 17:43:22 +0000

    GUI: RequestAccessWizard: add confirmation page which also creates the key.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@4913 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ gui/MKW.GUI/RequestAccessWizard/RequestAccessWizardConfirmationPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using MKW.GUI.Wizard;
+using System.Windows;
+
+namespace MKW.GUI.RequestAccessWizard
+{
+    public partial class RequestAccessWizardConfirmationPage : WizardPage
+    {
+        private readonly RequestAccessWizardViewModel viewModel;
+
+        public RequestAccessWizardConfirmationPage(RequestAccessWizardViewModel viewModel)
+            : base("Confirm Access Request Creation")
+        {
+            this.viewModel = viewModel;
+            DataContext = viewModel;
+            InitializeComponent();
+        }
+
+        public override bool Next()
+        {
+            try
+            {
+                viewModel.GenerateRequest();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                ErrorReporter.HandleException(Window.GetWindow(this), ex);
+                return false;
+            }
+        }
+    }
+}
