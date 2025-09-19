c4775722513bdba99f6674a20db77338563ab65b
Timofei Zhakov <tima@chemodax.net>
2025-09-19 18:03:24 +0000

    GUI: AddUserWizard: blank new wizard with empty pages
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@4917 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ gui/MKW.GUI/AddUserWizard/AddUserWizardImportRequestPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using MKW.GUI.Wizard;
+
+namespace MKW.GUI.AddUserWizard
+{
+    public partial class AddUserWizardImportRequestPage : WizardPage
+    {
+        private readonly AddUserWizardViewModel viewModel;
+
+        public AddUserWizardImportRequestPage(AddUserWizardViewModel viewModel)
+            : base("Import User Request")
+        {
+            this.viewModel = viewModel;
+            DataContext = viewModel;
+            InitializeComponent();
+        }
+    }
+}
---
+++ gui/MKW.GUI/AddUserWizard/AddUserWizardWelcomePage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using MKW.GUI.Wizard;
+
+namespace MKW.GUI.AddUserWizard
+{
+    public partial class AddUserWizardWelcomePage : WizardPage
+    {
+        private readonly AddUserWizardViewModel viewModel;
+
+        public AddUserWizardWelcomePage(AddUserWizardViewModel viewModel)
+            : base("Welcome")
+        {
+            this.viewModel = viewModel;
+            DataContext = viewModel;
+            InitializeComponent();
+        }
+    }
+}
