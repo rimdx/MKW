df0aa7703965d7b4a396d3a02734be56bb9c5170
Ivan.Z <53048645+chemodax@users.noreply.github.com>
2025-09-19 17:50:15 +0000

    GUI: RequestAccessWizard: Validate password on password page.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@4915 0083e6b4-6090-234a-836e-88fcc467669c

--- gui/MKW.GUI/RequestAccessWizard/RequestAccessWizardPasswordPage.xaml.cs
+++ gui/MKW.GUI/RequestAccessWizard/RequestAccessWizardPasswordPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using MKW.GUI.Wizard;
+using System.Windows;
 
 namespace MKW.GUI.RequestAccessWizard
 {
@@ -12,6 +13,23 @@ namespace MKW.GUI.RequestAccessWizard
             this.viewModel = viewModel;
             DataContext = viewModel;
             InitializeComponent();
+        }
+
+        public override bool Next()
+        {
+            if (viewModel.Password.Length == 0)
+            {
+                MessageBox.Show(Window.GetWindow(this), "Password cannot be empty.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+
+            if (!viewModel.IsPasswordMatch)
+            {
+                MessageBox.Show(Window.GetWindow(this), "Password doesn't match.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+
+            return true;
         }
     }
 }
