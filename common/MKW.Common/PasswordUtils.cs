6b0d9498c48d4cfe0ec691ac72dc3e37aad47715
Ivan.Z <53048645+chemodax@users.noreply.github.com>
2025-09-19 20:55:47 +0000

    Add PasswordUtils.MeasurePasswordEntropy().
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@4933 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ common/MKW.Common/PasswordUtils.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+
+namespace MKW.Common
+{
+    public class PasswordUtils
+    {
+        private static readonly string[] categories = 
+            [
+                "0123456789",
+                "abcdefghijklmnopqrstuvwxyz",
+                "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+                " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+            ];
+
+        public static double MeasurePasswordEntropy(string password)
+        {
+            BitArray mask = new BitArray(categories.Length + 1);
+            int charRange = 0;
+
+            foreach (char ch in password)
+            {
+                int category = GetCharCategory(ch);
+
+                if (!mask.Get(category + 1))
+                {
+                    if (category >= 0)
+                    {
+                        charRange += categories[category].Length;
+                    }
+                    else
+                    {
+                        charRange += 20;
+                    }
+
+                    mask.Set(category + 1, true);
+                }
+            }
+
+            if (charRange == 0)
+            {
+                return 0;
+            }
+
+            double entropy = password.Length * Math.Log(charRange) / Math.Log(2);
+
+            return entropy;
+
+            static int GetCharCategory(char ch)
+            {
+                for (int i = 0; i < categories.Length; i++)
+                {
+                    if (categories[i].IndexOf(ch) >= 0)
+                    {
+                        return i;
+                    }
+                }
+
+                return -1;
+            }
+        }
+    }
+}
