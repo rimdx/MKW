3ef432227a25edfb713e00215a75bacd6c02269c
Timofei Zhakov <tima@chemodax.net>
2025-09-19 18:04:38 +0000

    GUI: AddUserWizardViewModel: consume DatabaseModel instance
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@4919 0083e6b4-6090-234a-836e-88fcc467669c

--- gui/MKW.GUI/AddUserWizard/AddUserWizardViewModel.cs
+++ gui/MKW.GUI/AddUserWizard/AddUserWizardViewModel.cs
@@ -1,12 +1,17 @@
-﻿using MKW.GUI.Wizard;
+﻿using MKW.GUI.Model;
+using MKW.GUI.Wizard;
 
 namespace MKW.GUI.AddUserWizard
 {
     public class AddUserWizardViewModel : WizardViewModel
     {
-        public AddUserWizardViewModel()
+        private readonly DatabaseModel model;
+
+        public AddUserWizardViewModel(DatabaseModel model)
             : base("Add User")
         {
+            this.model = model;
+
             AddPage(new AddUserWizardWelcomePage(this));
             AddPage(new AddUserWizardImportRequestPage(this));
             AddPage(new AddUserWizardConfirmationPage(this));
