9d0bc93cb28d77b477d252c8bfcfb96258718c4a
Timofei Zhakov <tima@chemodax.net>
2025-09-20 17:42:04 +0000

    Add UserMetadata and serialize it
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@4960 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ core/MKW.Core/UserMetadata.cs
@@ -0,0 +1,8 @@
+﻿namespace MKW.Core
+{
+    public record class UserMetadata
+    {
+        public required string UserId { get; init; }
+        public required string DisplayName { get; init; }
+    }
+}
---
+++ core/MKW.Core/UserMetadataSerializer.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json;
+
+namespace MKW.Core
+{
+    public static class UserMetadataSerializer
+    {
+        public static UserMetadata Deserialize(ReadOnlySpan<byte> data)
+        {
+            UserMetadata? metadata = JsonSerializer.Deserialize<UserMetadata>(data);
+
+            if (metadata == null)
+            {
+                throw new NullReferenceException();
+            }
+
+            return metadata;
+        }
+
+        public static ReadOnlyMemory<byte> Serialize(UserMetadata metadata)
+        {
+            return JsonSerializer.SerializeToUtf8Bytes(metadata);
+        }
+    }
+}
