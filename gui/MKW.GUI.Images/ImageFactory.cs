c18ae8ceb50cf0ba847959b99e5cbc1ba11cbcf4
Timofei Zhakov <tima@chemodax.net>
2025-09-20 14:40:19 +0000

    Images: factor out ImageFactory.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@4942 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ gui/MKW.GUI.Images/ImageFactory.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows.Controls;
+
+namespace MKW.GUI.Images
+{
+    public static class ImageFactory
+    {
+        public static Viewbox? MakeImage(ImageMoniker moniker) => moniker switch
+        {
+            ImageMoniker.None => null,
+            ImageMoniker.AddDatabase => new AddDatabase(),
+            ImageMoniker.AddUser => new AddUser(),
+            ImageMoniker.Admin => new Admin(),
+            ImageMoniker.AsymmetricKey => new AsymmetricKey(),
+            ImageMoniker.Close => new Close(),
+            ImageMoniker.Database => new Database(),
+            ImageMoniker.DatabaseFile => new DatabaseFile(),
+            ImageMoniker.DataList => new DataList(),
+            ImageMoniker.Delete => new Delete(),
+            ImageMoniker.DeleteDocument => new DeleteDocument(),
+            ImageMoniker.DeleteFolder => new DeleteFolder(),
+            ImageMoniker.Edit => new Edit(),
+            ImageMoniker.EditDatabase => new EditDatabase(),
+            ImageMoniker.EditDocument => new EditDocument(),
+            ImageMoniker.EditKey => new EditKey(),
+            ImageMoniker.FolderClosed => new FolderClosed(),
+            ImageMoniker.FolderOpened => new FolderOpened(),
+            ImageMoniker.Key => new Key(),
+            ImageMoniker.LoginUser => new LoginUser(),
+            ImageMoniker.NewDocument => new NewDocument(),
+            ImageMoniker.NewFolder => new NewFolder(),
+            ImageMoniker.NewUser => new NewUser(),
+            ImageMoniker.OpenFile => new OpenFile(),
+            ImageMoniker.OpenFolder => new OpenFolder(),
+            ImageMoniker.PasswordBox => new PasswordBox(),
+            ImageMoniker.PasswordStrength => new PasswordStrength(),
+            ImageMoniker.Save => new Save(),
+            ImageMoniker.SaveAs => new SaveAs(),
+            ImageMoniker.StatusHelp => new StatusHelp(),
+            ImageMoniker.StatusOK => new StatusOK(),
+            ImageMoniker.StatusWarning => new StatusWarning(),
+            ImageMoniker.Team => new Team(),
+            ImageMoniker.User => new User(),
+        };
+    }
+}
