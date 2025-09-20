0c135e5852bc54994e41a7e1ef6f16fdb4010d3e
Timofei Zhakov <tima@chemodax.net>
2025-09-20 14:59:13 +0000

    generator.ps1: generate ImageMoniker.cs
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@4945 0083e6b4-6090-234a-836e-88fcc467669c

--- gui/MKW.GUI.Images/generator.ps1
+++ gui/MKW.GUI.Images/generator.ps1
@@ -1,3 +1,11 @@
+$ImageMonikerCS = "$PSScriptRoot/ImageMoniker.cs"
+
+"namespace MKW.GUI.Images
+{
+    public enum ImageMoniker
+    {
+        None," | Out-File $ImageMonikerCS -Encoding utf8BOM
+
 Get-ChildItem $PSScriptRoot *.xaml | ForEach-Object {
     $name = $_.BaseName
 
@@ -16,8 +24,13 @@ namespace MKW.GUI.Images
 
     (Get-Content -Path $_).Replace("<Viewbox Width=", "<Viewbox x:Class=`"MKW.GUI.Images.$name`" Width=") | Out-File -FilePath $_ -Encoding ansi
 
+    "        $name," | Add-Content $ImageMonikerCS -Encoding utf8BOM
+
     svn add "$_.cs" --force
 }
+
+"    }
+}" | Add-Content $ImageMonikerCS -Encoding utf8BOM
 
 Write-Host "-----"
 
