dabbf65ba9a65581af7cdc327e6e254ea3e367b0
Ivan.Z <53048645+chemodax@users.noreply.github.com>
2025-09-20 16:45:56 +0000

    GUI: Extract PasswordViewModel.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@4954 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ gui/MKW.GUI/CreateDatabaseWizard/PasswordViewModel.cs
@@ -0,0 +1,108 @@
+﻿using MKW.Common;
+using MKW.GUI.Images;
+
+namespace MKW.GUI
+{
+    public class PasswordViewModel : ViewModelBase
+    {
+        private string password;
+        private string passwordRepeat;
+        private double passwordEntropy;
+        private int passwordLength;
+        private bool passwordMismatch;
+        private ImageMoniker passwordQualityIcon;
+
+        public PasswordViewModel()
+        {
+            password = "";
+            passwordRepeat = "";
+            passwordEntropy = GetPasswordEntropy(password);
+            passwordQualityIcon = GetPasswordQualityIcon(passwordEntropy);
+            passwordLength = GetPasswordLength(password);
+            passwordMismatch = GetPasswordMismatch(password, passwordRepeat);
+        }
+
+        public string Password
+        {
+            get => password;
+            set
+            {
+                if (SetProperty(ref password, value))
+                {
+                    PasswordEntropy = GetPasswordEntropy(password);
+                    PasswordLength = GetPasswordLength(password);
+                    PasswordMismatch = GetPasswordMismatch(password, passwordRepeat);
+                }
+            }
+        }
+
+        public string PasswordRepeat
+        {
+            get => passwordRepeat;
+            set
+            {
+                if (SetProperty(ref passwordRepeat, value))
+                {
+                    PasswordMismatch = GetPasswordMismatch(password, passwordRepeat);
+                }
+            }
+        }
+
+        public bool PasswordMismatch
+        {
+            get => passwordMismatch;
+            private set => SetProperty(ref passwordMismatch, value);
+        }
+
+        public double PasswordEntropy
+        {
+            get => passwordEntropy;
+            private set
+            {
+                if (SetProperty(ref passwordEntropy, value))
+                {
+                    PasswordQualityIcon = GetPasswordQualityIcon(value);
+                }
+            }
+        }
+
+        public int PasswordLength
+        {
+            get => passwordLength;
+            private set => SetProperty(ref passwordLength, value);
+        }
+
+        private static int GetPasswordLength(string password)
+        {
+            return password.Length;
+        }
+
+        private static double GetPasswordEntropy(string password)
+        {
+            return PasswordUtils.MeasurePasswordEntropy(password);
+        }
+
+        private static bool GetPasswordMismatch(string password, string passwordRepeat)
+        {
+            return password != passwordRepeat;
+        }
+
+        private static ImageMoniker GetPasswordQualityIcon(double passwordEntropy)
+        {
+            if (passwordEntropy >= 75)
+            {
+                return ImageMoniker.StatusOK;
+            }
+            else
+            {
+                return ImageMoniker.StatusWarning;
+            }
+        }
+
+        public ImageMoniker PasswordQualityIcon
+        {
+            get => passwordQualityIcon;
+            private set => SetProperty(ref passwordQualityIcon, value);
+        }
+    }
+}
