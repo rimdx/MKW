4fad7506c924f9cf477f4fe9202f989e91f08446
Timofei Zhakov <tima@chemodax.net>
2025-09-20 15:03:08 +0000

    generator.ps1: generate ImageFactory.cs
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@4948 0083e6b4-6090-234a-836e-88fcc467669c

--- gui/MKW.GUI.Images/generator.ps1
+++ gui/MKW.GUI.Images/generator.ps1
@@ -1,10 +1,21 @@
 $ImageMonikerCS = "$PSScriptRoot/ImageMoniker.cs"
+$ImageFactoryCS = "$PSScriptRoot/ImageFactory.cs"
 
 "namespace MKW.GUI.Images
 {
     public enum ImageMoniker
     {
         None," | Out-File $ImageMonikerCS -Encoding utf8
+
+"using System.Windows.Controls;
+
+namespace MKW.GUI.Images
+{
+    public static class ImageFactory
+    {
+        public static Viewbox? MakeImage(ImageMoniker moniker) => moniker switch
+        {
+            ImageMoniker.None => null," | Out-File $ImageFactoryCS -Encoding utf8
 
 Get-ChildItem $PSScriptRoot *.xaml | ForEach-Object {
     $name = $_.BaseName
@@ -25,12 +36,17 @@ namespace MKW.GUI.Images
     (Get-Content -Path $_).Replace("<Viewbox Width=", "<Viewbox x:Class=`"MKW.GUI.Images.$name`" Width=") | Out-File -FilePath $_ -Encoding ascii
 
     "        $name," | Add-Content $ImageMonikerCS -Encoding utf8
+    "            ImageMoniker.$name => new $name()," | Add-Content $ImageFactoryCS -Encoding utf8
 
     svn add "$_.cs" --force
 }
 
 "    }
 }" | Add-Content $ImageMonikerCS -Encoding utf8
+
+"        };
+    }
+}" | Add-Content $ImageFactoryCS -Encoding utf8
 
 Write-Host "-----"
 
