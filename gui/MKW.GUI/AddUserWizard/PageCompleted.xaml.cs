6012d7298420ea0d20e4586c3716340a79b13cdc
Timofei Zhakov <tima@chemodax.net>
2025-09-20 22:30:54 +0000

    GUI:AddUserWizard: add 'completed' page and create the user when next is hit on the confirmation page.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@4998 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ gui/MKW.GUI/AddUserWizard/PageCompleted.xaml.cs
@@ -0,0 +1,18 @@
+﻿using MKW.GUI.Wizard;
+using System.Windows.Controls;
+
+namespace MKW.GUI.AddUserWizard
+{
+    public partial class PageCompleted : WizardPage
+    {
+        private readonly AddUserWizardViewModel viewModel;
+
+        public PageCompleted(AddUserWizardViewModel viewModel)
+            : base("Completed")
+        {
+            this.viewModel = viewModel;
+            DataContext = viewModel;
+            InitializeComponent();
+        }
+    }
+}
--- gui/MKW.GUI/AddUserWizard/PageConfirmation.xaml.cs
+++ gui/MKW.GUI/AddUserWizard/PageConfirmation.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using MKW.GUI.Wizard;
+using System.Windows;
 
 namespace MKW.GUI.AddUserWizard
 {
@@ -12,6 +13,20 @@ namespace MKW.GUI.AddUserWizard
             this.viewModel = viewModel;
             DataContext = viewModel;
             InitializeComponent();
+        }
+
+        public override bool Next()
+        {
+            try
+            {
+                viewModel.DoAddUser();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                ErrorReporter.HandleException(Window.GetWindow(this), ex);
+                return false;
+            }
         }
     }
 }
