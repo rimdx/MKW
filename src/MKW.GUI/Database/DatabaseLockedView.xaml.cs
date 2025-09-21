01ec891ce53157e751e023841dc9ee404aa82951
Ivan.Z <53048645+chemodax@users.noreply.github.com>
2025-09-21 19:30:11 +0000

    GUI: Add missing files.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@5048 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.GUI/Database/DatabaseLockedView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace MKW.GUI.Database
+{
+    public partial class DatabaseLockedView : UserControl
+    {
+        private readonly DatabaseLockedViewModel model;
+
+        public DatabaseLockedView(DatabaseLockedViewModel model)
+        {
+            this.model = model;
+            DataContext = model;
+
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/MKW.GUI/Database/DatabaseUnlockedView.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace MKW.GUI.Database
+{
+    public partial class DatabaseUnlockedView : UserControl
+    {
+        private readonly DatabaseUnlockedViewModel model;
+
+        public DatabaseUnlockedView(DatabaseUnlockedViewModel model)
+        {
+            this.model = model;
+            DataContext = model;
+
+            InitializeComponent();
+
+            InfoPage.Content = new PageInfo(model);
+            EntriesPage.Content = new PageEntries(model);
+            UsersPage.Content = new PageUsers(model);
+        }
+
+        private void AddEntry_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                NewEntryWindowViewModel viewModel = model.CreateNewEntryWindowViewModel();
+                NewEntryWindow window = new NewEntryWindow(viewModel, Window.GetWindow(this));
+                window.ShowDialog();
+            }
+            catch (Exception ex)
+            {
+                ErrorReporter.HandleException(Window.GetWindow(this), ex);
+            }
+        }
+
+        private void EditEntry_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (model.SelectedEntry != null)
+                {
+                    using EditEntryWindowViewModel viewModel = model.CreateEditEntryWindowViewModel(model.SelectedEntry.Id);
+                    EditEntryWindow window = new EditEntryWindow(viewModel, Window.GetWindow(this));
+                    window.ShowDialog();
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorReporter.HandleException(Window.GetWindow(this), ex);
+            }
+        }
+    }
+}
