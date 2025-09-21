6a7157bbe427f8c03c32c2ed1cb716d4713bb2b4
Timofei Zhakov <tima@chemodax.net>
2025-09-21 17:35:27 +0000

    UserMetadataEncoder: sign metadata.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@5032 0083e6b4-6090-234a-836e-88fcc467669c

--- src/MKW.Core.Client/UserMetadataEncoder.cs
+++ src/MKW.Core.Client/UserMetadataEncoder.cs
@@ -5,15 +5,20 @@ namespace MKW.Core.Client
 {
     public class UserMetadataEncoder
     {
-        public UserMetadataEncoder()
+        private readonly IAsymmetricPrivateTransformer adminKey;
+
+        public UserMetadataEncoder(IAsymmetricPrivateTransformer adminKey)
         {
+            this.adminKey = adminKey;
         }
 
         public void UpdateMetadata(IDatabaseUser user, UserMetadata metadata)
         {
             ReadOnlyMemory<byte> encoded = UserMetadataSerializer.Serialize(metadata);
+            ReadOnlyMemory<byte> signature = adminKey.Sign(encoded.Span);
 
             user.Metadata = encoded;
+            user.MetadataAdminSignature = signature;
         }
     }
 }
