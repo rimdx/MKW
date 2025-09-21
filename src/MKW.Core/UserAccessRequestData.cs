c42b5a7e3e8a73942bda0e32f5c079a656f8260b
Timofei Zhakov <tima@chemodax.net>
2025-09-21 21:18:52 +0000

    Move UserAccessRequestSerializer to the MKW.Core library.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@5062 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.Core/UserAccessRequestData.cs
@@ -0,0 +1,13 @@
+﻿namespace MKW.Core
+{
+    internal class UserAccessRequestData
+    {
+        public required ReadOnlyMemory<byte> Salt { get; set; }
+
+        public required ReadOnlyMemory<byte> PublicKey { get; set; }
+
+        public required ReadOnlyMemory<byte> PrivateKey { get; set; }
+
+        public required ReadOnlyMemory<byte> AdminSignature { get; set; }
+    }
+}
---
+++ src/MKW.Core/UserAccessRequestSerializer.cs
@@ -0,0 +1,49 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace MKW.Core
+{
+    public partial class UserAccessRequestSerializer : IAccessRequestSerializer
+    {
+        [JsonSourceGenerationOptions()]
+        [JsonSerializable(typeof(UserAccessRequestData))]
+        private partial class SerializerContext : JsonSerializerContext
+        {
+        }
+
+        public UserAccessRequestSerializer()
+        {
+        }
+
+        public ReadOnlyMemory<byte> Serialize(UserAccessRequest data)
+        {
+            UserAccessRequestData obj = new UserAccessRequestData
+            {
+                Salt = data.Salt,
+                PublicKey = data.PublicKey,
+                PrivateKey = data.EncryptedPrivateKey,
+                AdminSignature = data.AdminSignature,
+            };
+
+            return JsonSerializer.SerializeToUtf8Bytes(obj, SerializerContext.Default.UserAccessRequestData);
+        }
+
+        public UserAccessRequest Deserialize(ReadOnlySpan<byte> data)
+        {
+            UserAccessRequestData? parsed = JsonSerializer.Deserialize(data, SerializerContext.Default.UserAccessRequestData);
+
+            if (parsed == null)
+            {
+                throw new NullReferenceException();
+            }
+
+            return new UserAccessRequest
+            {
+                Salt = parsed.Salt,
+                PublicKey = parsed.PublicKey,
+                EncryptedPrivateKey = parsed.PrivateKey,
+                AdminSignature = parsed.AdminSignature,
+            };
+        }
+    }
+}
