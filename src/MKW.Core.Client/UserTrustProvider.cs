7a042aea225e1fa641feff4861e0313eeee6e316
Timofei Zhakov <tima@chemodax.net>
2025-09-21 19:15:15 +0000

    Inline UserTrustVerifier into UserTrustProvider.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@5044 0083e6b4-6090-234a-836e-88fcc467669c

--- src/MKW.Core.Client/UserTrustProvider.cs
+++ src/MKW.Core.Client/UserTrustProvider.cs
@@ -11,7 +11,6 @@ namespace MKW.Core.Client
         protected readonly IDatabase database;
         protected readonly ICryptographyProvider crypto;
         protected readonly IDatabaseUser admin;
-        protected readonly ITrustVerifier trustVerifier;
 
         public UserTrustProvider(IDatabase database,
                                  ICryptographyProvider crypto,
@@ -20,14 +19,33 @@ namespace MKW.Core.Client
             this.database = database;
             this.crypto = crypto;
             this.admin = admin;
-            trustVerifier = new UserTrustVerifier(crypto, admin);
+        }
+
+        public Trust GetTrust(ReadOnlySpan<byte> otherPublicKey)
+        {
+            if (otherPublicKey.SequenceEqual(admin.PublicKey.Span))
+            {
+                return Trust.SelfTrust;
+            }
+
+            using IAsymmetricPublicTransformer publicKey = crypto.OpenAsymmetricTransformer(admin.PublicKey.Span);
+
+            foreach (ReadOnlyMemory<byte> trust in admin.EnumerateTrust())
+            {
+                if (publicKey.Verify(otherPublicKey, trust.Span))
+                {
+                    return Trust.ExplicitTrust;
+                }
+            }
+
+            return Trust.None;
         }
 
         public IEnumerable<UserInfo> EnumerateImplicitlyTrustedUsers()
         {
             foreach (IDatabaseUser user in database.EnumerateUsers())
             {
-                Trust trust = trustVerifier.GetTrust(user.PublicKey.Span);
+                Trust trust = GetTrust(user.PublicKey.Span);
 
                 if (trust == Trust.ExplicitTrust || trust == Trust.SelfTrust)
                 {
@@ -39,12 +57,11 @@ namespace MKW.Core.Client
         public Trust GetImplicitTrust(UserId userId)
         {
             IDatabaseUser user = database.OpenUser(userId, true);
-            return trustVerifier.GetTrust(user.PublicKey.Span);
+            return GetTrust(user.PublicKey.Span);
         }
 
         public virtual void Dispose()
         {
-            trustVerifier.Dispose();
         }
     }
 }
