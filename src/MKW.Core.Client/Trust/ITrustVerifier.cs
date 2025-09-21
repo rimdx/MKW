5644545a6bfe817c8146d29fead904e9abb4e3a3
Timofei Zhakov <tima@chemodax.net>
2025-09-21 19:15:15 +0000

    Inline UserTrustVerifier into UserTrustProvider.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@5044 0083e6b4-6090-234a-836e-88fcc467669c

--- src/MKW.Core.Client/Trust/ITrustVerifier.cs
+++
@@ -1,9 +0,0 @@
-﻿using MKW.Core.Notify;
-
-namespace MKW.Core.Client
-{
-    public interface ITrustVerifier : IDisposable
-    {
-        Trust GetTrust(ReadOnlySpan<byte> otherPublicKey);
-    }
-}
