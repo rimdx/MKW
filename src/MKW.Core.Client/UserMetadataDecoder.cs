2e1bf6dd344a5cd4468044e2cfa45231306aacac
Timofei Zhakov <tima@chemodax.net>
2025-09-21 17:44:15 +0000

    UserMetadataDecoder: verify metadata.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@5036 0083e6b4-6090-234a-836e-88fcc467669c

--- src/MKW.Core.Client/UserMetadataDecoder.cs
+++ src/MKW.Core.Client/UserMetadataDecoder.cs
@@ -1,20 +1,27 @@
 ﻿using MKW.Core.Storage;
+using MKW.Cryptography;
 
 namespace MKW.Core.Client
 {
     public class UserMetadataDecoder : IDisposable
     {
-        public UserMetadataDecoder()
+        private readonly IAsymmetricPublicTransformer adminKey;
+
+        public UserMetadataDecoder(IAsymmetricPublicTransformer adminKey)
         {
+            this.adminKey = adminKey;
         }
 
         public UserMetadata OpenMetadata(IDatabaseUser user)
         {
-            UserMetadata metadata = UserMetadataSerializer.Deserialize(user.Metadata.Span);
-
-            // TODO: verify
-
-            return metadata;
+            if (adminKey.Verify(user.Metadata.Span, user.MetadataAdminSignature.Span))
+            {
+                return UserMetadataSerializer.Deserialize(user.Metadata.Span);
+            }
+            else
+            {
+                throw new Exceptions.InvalidUserMetadataSignature();
+            }
         }
 
         public void Dispose()
