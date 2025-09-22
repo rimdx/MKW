48a9cb3b9546f4587b9ee1049767594885106657
Timofei Zhakov <tima@chemodax.net>
2025-09-22 17:30:45 +0000

    Factor out TempFile abstraction.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@5110 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.Common/TempFile.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.Win32.SafeHandles;
+
+namespace MKW.Common
+{
+    public class TempFile : FileStream
+    {
+        private bool owns = true;
+        private string tempPath;
+        private string newPath;
+
+        private TempFile(SafeFileHandle handle, string tempPath, string newPath)
+            : base(handle, FileAccess.Write)
+        {
+            this.tempPath = tempPath;
+            this.newPath = newPath;
+        }
+
+        private static string GenerateTempFileName(string path)
+        {
+            if (File.Exists(path))
+            {
+                // TODO: properly generate file name
+                return path + ".tmp";
+            }
+            else
+            {
+                return path;
+            }
+        }
+
+        public static TempFile Create(string path)
+        {
+            string tempPath = GenerateTempFileName(path);
+
+            FileStream file = new FileStream(tempPath,
+                                             FileMode.CreateNew,
+                                             FileAccess.Write);
+
+            try
+            {
+                return new TempFile(file.SafeFileHandle, tempPath, path);
+            }
+            catch (Exception)
+            {
+                file.Dispose();
+                throw;
+            }
+        }
+
+        public void Accept()
+        {
+            if (owns)
+            {
+                Flush(true);
+                base.Dispose(true);
+
+                if (tempPath != newPath)
+                {
+                    File.Replace(tempPath, newPath, null);
+                }
+
+                owns = false;
+            }
+        }
+
+        public void Reject()
+        {
+            if (owns)
+            {
+                base.Dispose(true);
+                File.Delete(tempPath);
+                owns = false;
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            Reject();
+        }
+    }
+}
