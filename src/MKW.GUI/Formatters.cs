0ed32feb66d29a17e55a4a41401d56522784c7c3
Ivan.Z <53048645+chemodax@users.noreply.github.com>
2025-09-22 21:51:44 +0000

    GUI: Add DisplayName for username.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@5166 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.GUI/Formatters.cs
@@ -0,0 +1,17 @@
+﻿namespace MKW.GUI
+{
+    internal static class Formatters
+    {
+        public static string FormatUserName(string userId, string displayName)
+        {
+            if (displayName.Length > 0)
+            {
+                return $"{displayName} <{userId}>";
+            }
+            else
+            {
+                return userId;
+            }
+        }
+    }
+}
