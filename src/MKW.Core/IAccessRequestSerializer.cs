eaca300375ac61e1c80a1d0a5680b299567880f3
Timofei Zhakov <tima@chemodax.net>
2025-09-22 21:50:33 +0000

    Drop IAccessRequestSerializer.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@5165 0083e6b4-6090-234a-836e-88fcc467669c

--- src/MKW.Core/IAccessRequestSerializer.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MKW.Core
-{
-    public interface IAccessRequestSerializer
-    {
-        UserAccessRequest Deserialize(ReadOnlySpan<byte> data);
-        ReadOnlyMemory<byte> Serialize(UserAccessRequest data);
-    }
-}
