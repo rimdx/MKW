c94624ff01983b9a5ae3f1e3302b4f838f49b9b1
Timofei Zhakov <tima@chemodax.net>
2025-09-22 19:32:32 +0000

    Open AccessController by entry id.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@5128 0083e6b4-6090-234a-836e-88fcc467669c

--- src/MKW.Core.Client/Entry.cs
+++ src/MKW.Core.Client/Entry.cs
@@ -56,7 +56,7 @@ namespace MKW.Core.Client
                                  UserSession user,
                                  IDatabaseEntry entry)
         {
-            AccessController accessController = AccessController.Open(database, entry);
+            AccessController accessController = AccessController.Open(database, entry.Id);
 
             return new Entry(database,
                              crypto,
