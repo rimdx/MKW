f299a53d22d0c73c5e94d474fd7ea8f2bb7d7887
Timofei Zhakov <tima@chemodax.net>
2025-09-22 16:44:15 +0000

    UserAccessRequest: make all fields required.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@5103 0083e6b4-6090-234a-836e-88fcc467669c

--- src/MKW.Core/UserAccessRequest.cs
+++ src/MKW.Core/UserAccessRequest.cs
@@ -3,12 +3,12 @@
     public record class UserAccessRequest
     {
         /* plain */
-        public ReadOnlyMemory<byte> Salt { get; init; }
-        public ReadOnlyMemory<byte> PublicKey { get; init; }
+        public required ReadOnlyMemory<byte> Salt { get; init; }
+        public required ReadOnlyMemory<byte> PublicKey { get; init; }
 
         /* encrypted */
-        public ReadOnlyMemory<byte> EncryptedPrivateKey { get; init; }
+        public required ReadOnlyMemory<byte> EncryptedPrivateKey { get; init; }
 
-        public ReadOnlyMemory<byte> AdminSignature { get; init; }
+        public required ReadOnlyMemory<byte> AdminSignature { get; init; }
     }
 }
