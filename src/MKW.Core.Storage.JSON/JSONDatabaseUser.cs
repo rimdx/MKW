082b70b3164051de3a12f378d6f998a763b973ab
Timofei Zhakov <tima@chemodax.net>
2025-09-22 21:50:09 +0000

    Follow up to r5163: fix conflicts.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@5164 0083e6b4-6090-234a-836e-88fcc467669c

--- src/MKW.Core.Storage.JSON/JSONDatabaseUser.cs
+++ src/MKW.Core.Storage.JSON/JSONDatabaseUser.cs
@@ -1,19 +1,4 @@
-﻿
-<<<<<<< TODO: Unmerged change from project 'MKW.Core.Storage.JSON (net472)', Before:
-namespace MKW.Core.Storage.JSON.Types
-{
-=======
-using MKW;
-using MKW.Core;
-using MKW.Core.Storage;
-using MKW.Core.Storage.JSON;
-using MKW.Core.Storage.JSON;
-using MKW.Core.Storage.JSON.Types;
-
-namespace MKW.Core.Storage.JSON
-{
->>>>>>> After
-    namespace MKW.Core.Storage.JSON
+﻿namespace MKW.Core.Storage.JSON
 {
     internal record class JSONDatabaseUser
     {
