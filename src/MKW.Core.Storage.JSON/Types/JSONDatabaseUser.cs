7c4cade391574c1fe7ea735fedf45cc03d1ed4d1
Timofei Zhakov <tima@chemodax.net>
2025-09-22 16:20:05 +0000

    Database: rename AdminSignature to AdminTrustSignature in the implementation layer.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@5097 0083e6b4-6090-234a-836e-88fcc467669c

--- src/MKW.Core.Storage.JSON/Types/JSONDatabaseUser.cs
+++ src/MKW.Core.Storage.JSON/Types/JSONDatabaseUser.cs
@@ -11,7 +11,7 @@
         public required ReadOnlyMemory<byte> Metadata { get; init; }
         public required ReadOnlyMemory<byte> MetadataAdminSignature { get; init; }
 
-        public required ReadOnlyMemory<byte> AdminSignature { get; init; }
+        public required ReadOnlyMemory<byte> AdminTrustSignature { get; init; }
 
         public required List<ReadOnlyMemory<byte>> Trust { get; init; }
     }
