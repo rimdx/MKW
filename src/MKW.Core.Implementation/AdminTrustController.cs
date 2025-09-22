a35d90526fd350515c4c253d837d814469f69aa6
Timofei Zhakov <tima@chemodax.net>
2025-09-22 12:34:50 +0000

    Simplify AdminTrustController (formerly UserTrustController).
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@5090 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.Core.Implementation/AdminTrustController.cs
@@ -0,0 +1,29 @@
+﻿using MKW.Core.Storage;
+using MKW.Cryptography;
+
+namespace MKW.Core.Implementation
+{
+    public class AdminTrustController : IDisposable
+    {
+        protected readonly IAsymmetricPrivateTransformer adminKey;
+
+        public AdminTrustController(IAsymmetricPrivateTransformer adminKey)
+        {
+            this.adminKey = adminKey;
+        }
+
+        public void AddTrust(IDatabaseUser user)
+        {
+            user.AdminSignature = adminKey.Sign(user.PublicKey.Span);
+            user.Save();
+        }
+
+        public void Dispose()
+        {
+            // no-op
+
+            // In this case, the UserTrustProvider.key is actually managed by
+            // caller, since the user is given by a reference.
+        }
+    }
+}
