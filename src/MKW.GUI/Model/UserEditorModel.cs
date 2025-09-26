5a06cae3e54e9d1b7d2b0b50382e39c470bd7220
Timofei Zhakov <tima@chemodax.net>
2025-09-26 16:00:00 +0000

    GUI: Remove trust from UserEditorModel.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@5220 0083e6b4-6090-234a-836e-88fcc467669c

--- src/MKW.GUI/Model/UserEditorModel.cs
+++ src/MKW.GUI/Model/UserEditorModel.cs
@@ -7,22 +7,14 @@ namespace MKW.GUI.Model
         private readonly DatabaseUnlockedModel database;
         private readonly UserInfo user;
 
-        private readonly bool initialTrust;
-        private bool newTrust;
-
         public UserId Id => user.Id;
 
         public ReadOnlySpan<byte> PublicKey => user.PublicKey.Span;
-
-        public bool Trust => newTrust;
 
         public UserEditorModel(DatabaseUnlockedModel database, UserInfo user)
         {
             this.database = database;
             this.user = user;
-
-            initialTrust = database.User!.VerifyTrust(user.Id);
-            newTrust = initialTrust;
         }
 
         public void OnApply()
