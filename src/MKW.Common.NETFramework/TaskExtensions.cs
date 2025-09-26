d689b1d6130fd769dc291d0f531a4335008ca3b1
Timofei Zhakov <tima@chemodax.net>
2025-09-26 22:58:14 +0000

    Introduce Task.WaitAsync(CancellationToken) into .NET framework.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@5237 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.Common.NETFramework/TaskExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace MKW.Common
+{
+    public static class TaskExtensions
+    {
+        public static async Task<T> WaitAsync<T>(this Task<T> task, CancellationToken cancellationToken)
+        {
+            async Task<T> canceller()
+            {
+                await Task.Delay(-1, cancellationToken);
+                // never reached
+                throw new TaskCanceledException();
+            }
+
+            Task<T> result = await Task.WhenAny(
+                task,
+                canceller()
+            );
+
+            return await result;
+        }
+    }
+}
