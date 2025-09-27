665b1bd53d54213b1770dc3cc654605516f1c149
Timofei Zhakov <tima@chemodax.net>
2025-09-27 12:06:46 +0000

    GUI: Add fake SingleInstanceApplicationManager and other abstraction to convert application to single instance.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@5256 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.GUI/SingleInstance/ISingleInstanceApplication.cs
@@ -0,0 +1,8 @@
+﻿namespace MKW.GUI.SingleInstance
+{
+    public interface ISingleInstanceApplication
+    {
+        void InvokeMainInstance(string[] args);
+        void InvokeExternalInstance(string[] args);
+    }
+}
---
+++ src/MKW.GUI/SingleInstance/SingleInstanceApplicationManager.cs
@@ -0,0 +1,18 @@
+﻿namespace MKW.GUI.SingleInstance
+{
+    public class SingleInstanceApplicationManager
+    {
+        private readonly ISingleInstanceApplication application;
+
+        public SingleInstanceApplicationManager(ISingleInstanceApplication application)
+        {
+            this.application = application;
+        }
+
+        public void Run(string[] args)
+        {
+            // todo:
+            application.InvokeMainInstance(args);
+        }
+    }
+}
