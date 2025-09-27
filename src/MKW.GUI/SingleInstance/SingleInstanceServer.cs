a1efbf1219d9c681f651a80fa996c4a1da4aacb7
Timofei Zhakov <tima@chemodax.net>
2025-09-27 13:15:45 +0000

    GUI: SingleInstanceApplicationManager: Notify the main instance when a second one is lunching.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@5259 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.GUI/SingleInstance/SingleInstanceServer.cs
@@ -0,0 +1,56 @@
+﻿using MKW.Common;
+using System.IO.Pipes;
+
+namespace MKW.GUI.SingleInstance
+{
+    internal class SingleInstanceServer : IDisposable
+    {
+        private readonly NamedPipeServerStream pipe;
+        private readonly ISingleInstanceApplication application;
+
+        public SingleInstanceServer(ISingleInstanceApplication application)
+        {
+            this.application = application;
+            pipe = new NamedPipeServerStream(SingleInstanceConstants.SingleInstancePipeName);
+        }
+
+        public async Task Run(string[] args, CancellationToken cancellationToken)
+        {
+            try
+            {
+                await pipe.WaitForConnectionAsync(cancellationToken);
+
+                while (true)
+                {
+                    cancellationToken.ThrowIfCancellationRequested();
+
+                    ReadOnlyMemory<byte> data = await ReadPacket(cancellationToken);
+
+                    application.InvokeExternalInstance([]);
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                return;
+            }
+        }
+
+        private async Task<ReadOnlyMemory<byte>> ReadPacket(CancellationToken cancellationToken)
+        {
+            Memory<byte> lengthBuffer = new byte[4];
+            await pipe.ReadAsync(lengthBuffer, cancellationToken);
+
+            int length = BitConverter.ToInt32(lengthBuffer.ToArray(), 0);
+            Memory<byte> dataBuffer = new byte[lengthBuffer.Length];
+
+            await pipe.ReadAsync(dataBuffer, cancellationToken);
+
+            return dataBuffer;
+        }
+
+        public void Dispose()
+        {
+            pipe.Dispose();
+        }
+    }
+}
