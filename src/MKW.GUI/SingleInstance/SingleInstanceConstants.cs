01768cb2888abe3dc8e975b2731616b8648bbd5d
Ivan.Z <53048645+chemodax@users.noreply.github.com>
2025-09-27 19:49:28 +0000

    GUI: Remove single instance mutex for now.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@5297 0083e6b4-6090-234a-836e-88fcc467669c

--- src/MKW.GUI/SingleInstance/SingleInstanceConstants.cs
+++ src/MKW.GUI/SingleInstance/SingleInstanceConstants.cs
@@ -4,8 +4,6 @@
     {
         public const string ApplicationMagic = "MKW.{A2AF56DE-DDB8-4FC6-86BA-666ACC77EC98}";
 
-        public const string SingleInstanceMutexName = $"{ApplicationMagic}.SingleInstanceMutex";
-
         public static class DataMessageId
         {
             public const nint RunRequest = 1;
