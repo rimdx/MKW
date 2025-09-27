acf1fc1c3b943b6bec82585c8e89d5b45df021b2
Timofei Zhakov <tima@chemodax.net>
2025-09-27 12:24:47 +0000

    GUI: SingleInstanceApplicationManager: enter mutex.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@5257 0083e6b4-6090-234a-836e-88fcc467669c

--- src/MKW.GUI/SingleInstance/SingleInstanceApplicationManager.cs
+++ src/MKW.GUI/SingleInstance/SingleInstanceApplicationManager.cs
@@ -1,7 +1,12 @@
-﻿namespace MKW.GUI.SingleInstance
+﻿using System.IO.Pipes;
+
+namespace MKW.GUI.SingleInstance
 {
     public class SingleInstanceApplicationManager
     {
+        private static readonly string SingleInstanceMutexName = "{D22C3E6A-1B2F-4B80-9F54-A601E5CBEC37}";
+        private static readonly string SingleInstancePipeName = "{09FCE022-F082-45F0-9E3C-92556F6C6079}";
+
         private readonly ISingleInstanceApplication application;
 
         public SingleInstanceApplicationManager(ISingleInstanceApplication application)
@@ -11,8 +16,19 @@
 
         public void Run(string[] args)
         {
-            // todo:
-            application.InvokeMainInstance(args);
+            using (Mutex singleInstanceMutex = new Mutex(true,
+                                                         SingleInstanceMutexName,
+                                                         out bool mainInstance))
+            {
+                if (mainInstance)
+                {
+                    application.InvokeMainInstance(args);
+                }
+                else
+                {
+                    // signal main app
+                }
+            }
         }
     }
 }
