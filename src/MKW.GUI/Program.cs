538f8a2ba19deb63c84c4f3480cf8a120cd64d28
Timofei Zhakov <tima@chemodax.net>
2025-09-27 12:05:53 +0000

    GUI: Override application entrypoint.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@5255 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.GUI/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace MKW.GUI
+{
+    public static class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            App app = new App();
+            app.Run();
+        }
+    }
+}
