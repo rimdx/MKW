649f18dc6ec563b997b7691283ea8bfc3cb5d3f7
Timofei Zhakov <tima@chemodax.net>
2025-09-27 13:21:59 +0000

    Factor out PacketRPC.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@5260 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.Common/PacketRPC.cs
@@ -0,0 +1,42 @@
+﻿namespace MKW.Common
+{
+    public class PacketRPC : IDisposable
+    {
+        private readonly Stream proxy;
+
+        public PacketRPC(Stream proxy)
+        {
+            this.proxy = proxy;
+        }
+
+        public async Task<ReadOnlyMemory<byte>> ReadPacket(CancellationToken cancellationToken)
+        {
+            Memory<byte> lengthBuffer = new byte[4];
+            await proxy.ReadAsync(lengthBuffer, cancellationToken);
+
+            int length = BitConverter.ToInt32(lengthBuffer.ToArray(), 0);
+            Memory<byte> dataBuffer = new byte[lengthBuffer.Length];
+
+            await proxy.ReadAsync(dataBuffer, cancellationToken);
+
+            return dataBuffer;
+        }
+
+        public async Task WritePacketAsync(ReadOnlyMemory<byte> buffer, CancellationToken cancellationToken)
+        {
+            ReadOnlyMemory<byte> length = BitConverter.GetBytes(buffer.Length);
+
+            Memory<byte> packet = new byte[buffer.Length + length.Length];
+
+            length.Span.CopyTo(packet.Span.Slice(0));
+            buffer.Span.CopyTo(packet.Span.Slice(length.Length));
+
+            await proxy.WriteAsync(packet, cancellationToken);
+        }
+
+        public void Dispose()
+        {
+            proxy.Dispose();
+        }
+    }
+}
