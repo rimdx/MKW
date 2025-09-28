4425d66208feebe7912cf2a6a86c8647567d5c3e
Timofei Zhakov <tima@chemodax.net>
2025-09-28 15:50:05 +0000

    GUI: LoginProfileView: Select everything in the password input when achieving focus.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@5358 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.GUI/Behaviors/PasswordBoxBehavior.cs
@@ -0,0 +1,63 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace MKW.GUI.Behaviors
+{
+    public class PasswordBoxBehavior
+    {
+        public const string SelectAllTextOnFocusPropertyPropertyName = "SelectAllTextOnFocus";
+
+        public static readonly DependencyProperty SelectAllTextOnFocusProperty =
+            DependencyProperty.RegisterAttached(
+                "SelectAllTextOnFocusPropertyPropertyName",
+                typeof(bool),
+                typeof(PasswordBoxBehavior),
+                new UIPropertyMetadata(false, OnSelectAllTextOnFocusChanged));
+
+        public static void SetSelectAllTextOnFocus(UIElement element, bool value)
+        {
+            element.SetValue(SelectAllTextOnFocusProperty, value);
+        }
+
+        public static bool GetSelectAllTextOnFocus(UIElement element)
+        {
+            return (bool)element.GetValue(SelectAllTextOnFocusProperty);
+        }
+
+        private static void OnSelectAllTextOnFocusChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is PasswordBox input && e.NewValue is bool newValue)
+            {
+                if (newValue)
+                {
+                    input.GotFocus += PasswordBox_GotFocus;
+                    input.PreviewMouseDown += PasswordBox_PreviewMouseDown;
+                }
+                else
+                {
+                    input.GotFocus -= PasswordBox_GotFocus;
+                    input.PreviewMouseDown -= PasswordBox_PreviewMouseDown;
+                }
+            }
+        }
+
+        private static void PasswordBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            PasswordBox? input = e.OriginalSource as PasswordBox;
+            input?.SelectAll();
+        }
+
+        private static void PasswordBox_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is PasswordBox input)
+            {
+                if (!input.IsKeyboardFocusWithin)
+                {
+                    e.Handled = true;
+                    input.Focus();
+                }
+            }
+        }
+    }
+}
