f947fab5100c7e6d763a08dd209b5729dbfd1a2a
Timofei Zhakov <tima@chemodax.net>
2025-09-29 19:39:19 +0000

    GUI: Add initial SelectorBox for users selector.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@5372 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.GUI/Controls/SelectorBoxItemAutomationPeer.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Automation.Peers;
+using System.Windows.Automation.Provider;
+
+namespace MKW.GUI.Controls
+{
+    internal sealed class SelectorBoxItemAutomationPeer
+        : SelectorItemAutomationPeer
+        , IVirtualizedItemProvider
+        , ISelectionItemProvider
+    {
+        public SelectorBoxItemAutomationPeer(object item, SelectorBoxAutomationPeer selectorBoxAutomationPeer)
+            : base(item, selectorBoxAutomationPeer)
+        {
+        }
+
+        protected override AutomationControlType GetAutomationControlTypeCore()
+        {
+            return AutomationControlType.Custom;
+        }
+
+        protected override string GetClassNameCore()
+        {
+            return nameof(SelectorBoxItem);
+        }
+    }
+}
