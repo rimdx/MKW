6fbeaa1c039eb8324f031122a842c2904abaca6f
Timofei Zhakov <tima@chemodax.net>
2025-10-02 17:47:05 +0000

    Add ASCIIStream that wraps a stream into a TextWriter, encoding everything in ASCII encoding.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@5388 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.Common/ASCIIStream.cs
@@ -0,0 +1,56 @@
+﻿using System.Text;
+
+namespace MKW.Common
+{
+    public sealed class ASCIIStream : Stream
+    {
+        private readonly TextWriter writer;
+
+        public ASCIIStream(TextWriter writer)
+        {
+            this.writer = writer;
+        }
+
+        public override bool CanRead => false;
+        public override bool CanSeek => false;
+        public override bool CanWrite => true;
+
+        public override long Length => throw new NotSupportedException();
+        public override long Position
+        {
+            get => throw new NotSupportedException();
+            set => throw new NotSupportedException();
+        }
+
+        public override void Flush()
+        {
+            writer.Flush();
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override void SetLength(long value)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            string str = Encoding.ASCII.GetString(buffer, offset, count);
+            writer.Write(str);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            // TODO: close writer
+        }
+    }
+}
