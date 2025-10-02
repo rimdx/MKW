d3e1af69aa7e60ad648cb0669a7b1da85dad4ce9
Timofei Zhakov <tima@chemodax.net>
2025-10-02 16:00:42 +0000

    Add Asn1Version.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@5383 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.Core.Serialization/Asn1Version.cs
@@ -0,0 +1,35 @@
+﻿using Org.BouncyCastle.Asn1;
+
+namespace MKW.Core.Serialization
+{
+    internal sealed class Asn1Version
+        : Asn1Encodable
+    {
+        private readonly int expectedVersion;
+
+        public Asn1Version(int expectedVersion)
+        {
+            this.expectedVersion = expectedVersion;
+        }
+
+        public void ConsumeVersion(Asn1Encodable obj)
+        {
+            DerInteger integer = DerInteger.GetInstance(obj);
+            int version = integer.IntValueExact;
+
+            if (version != expectedVersion)
+            {
+                throw new Asn1VersionMismatch(expectedVersion, version);
+            }
+            else
+            {
+                /* we're fine */
+            }
+        }
+
+        public override Asn1Object ToAsn1Object()
+        {
+            return new DerInteger(expectedVersion);
+        }
+    }
+}
---
+++ src/MKW.Core.Serialization/Asn1VersionMismatch.cs
@@ -0,0 +1,7 @@
+﻿namespace MKW.Core.Serialization
+{
+    internal sealed class Asn1VersionMismatch(int expected, int actual)
+        : Exception($"Asn1 version mismatch: expected {expected} but was {actual}.")
+    {
+    }
+}
