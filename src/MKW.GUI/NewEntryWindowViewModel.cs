5da6dee0d071f8b9db9ce003d75547334d54a9b9
Timofei Zhakov <tima@chemodax.net>
2025-10-02 19:06:56 +0000

    Make EntryPayload.Notes nullable.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@5401 0083e6b4-6090-234a-836e-88fcc467669c

--- src/MKW.GUI/NewEntryWindowViewModel.cs
+++ src/MKW.GUI/NewEntryWindowViewModel.cs
@@ -16,7 +16,7 @@ namespace MKW.GUI
 
         public string Notes
         {
-            get => payload.Notes;
+            get => payload.Notes ?? string.Empty;
             set => payload.Notes = value;
         }
 
