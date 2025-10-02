30b48082dd95ac496e3049ebbf83d258b38131d2
Timofei Zhakov <tima@chemodax.net>
2025-10-02 19:26:55 +0000

    GUI: Factor out PayloadEditor.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@5405 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.GUI/EntryEditor/PayloadEditor.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace MKW.GUI.EntryEditor
+{
+    public partial class PayloadEditor : UserControl
+    {
+        public PayloadEditor()
+        {
+            InitializeComponent();
+        }
+    }
+}
