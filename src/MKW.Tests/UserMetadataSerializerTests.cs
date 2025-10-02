b116b68e1e81deadeda16acc84ec02acd8d243b9
Timofei Zhakov <tima@chemodax.net>
2025-10-02 18:09:31 +0000

    UserMetadataSerializerTests: Add regression test for UserMetadataSerializer.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@5391 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.Tests/UserMetadataSerializerTests.cs
@@ -0,0 +1,35 @@
+﻿using MKW.Core;
+using MKW.Core.Client;
+using MKW.Core.Serialization;
+using MKW.Core.Storage;
+using MKW.Testing.Client;
+using NUnit.Framework.Legacy;
+
+namespace MKW.Tests
+{
+    [TestFixture]
+    public class UserMetadataSerializerTests
+    {
+        [Test]
+        public void SimpleTest()
+        {
+            using ClientSandBox sbox = new ClientSandBox();
+            using IDatabase db = sbox.OpenDatabase();
+            using ClientSession client = sbox.OpenSession(db);
+
+            UserMetadata metadata = new UserMetadata
+            {
+                UserId = "user-id",
+                DisplayName = "display name",
+            };
+
+            ReadOnlyMemory<byte> serialized = UserMetadataSerializer.Serialize(metadata);
+
+            Console.WriteLine(Convert.ToBase64String(serialized.ToArray()));
+
+            UserMetadata deserialized = UserMetadataSerializer.Deserialize(serialized.Span);
+
+            ClassicAssert.AreEqual(metadata, deserialized);
+        }
+    }
+}
