1d7176736d5099c88eab30177c2a76d80e6917fc
Timofei Zhakov <tima@chemodax.net>
2025-10-02 19:15:01 +0000

    EntryPayload: Declare several standard fields.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@5404 0083e6b4-6090-234a-836e-88fcc467669c

--- src/MKW.Core/EntryPayload.cs
+++ src/MKW.Core/EntryPayload.cs
@@ -2,6 +2,10 @@
 {
     public sealed record class EntryPayload
     {
+        public string? Title { get; set; }
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+        public string? Url { get; set; }
         public string? Notes { get; set; }
     }
 }
