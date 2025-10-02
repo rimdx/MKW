b0c849adce3f116c306cb01073267527c36e8e38
Timofei Zhakov <tima@chemodax.net>
2025-10-02 18:16:49 +0000

    Add some error handling to the serializers.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@5394 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.Core.Serialization/Exceptions/InvalidUserMetadataException.cs
@@ -0,0 +1,7 @@
+﻿namespace MKW.Core.Serialization.Exceptions
+{
+    public sealed class InvalidUserMetadataException(Exception innerException)
+        : Exception("User metadata is invalid.", innerException)
+    {
+    }
+}
