fd76d2282507eae99f311ff09245ad9b86816b7f
Timofei Zhakov <tima@chemodax.net>
2025-10-02 19:11:17 +0000

    GUI: Factor out EntryPayloadEditorViewModel.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@5402 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.GUI/EntryPayloadEditorViewModel.cs
@@ -0,0 +1,25 @@
+﻿using MKW.Core;
+
+namespace MKW.GUI
+{
+    public class EntryPayloadEditorViewModel : ViewModelBase
+    {
+        private readonly EntryPayload payload;
+
+        public EntryPayloadEditorViewModel(EntryPayload payload)
+        {
+            this.payload = payload;
+        }
+
+        public string Notes
+        {
+            get => payload.Notes ?? string.Empty;
+            set => payload.Notes = value;
+        }
+
+        public EntryPayload GetPayload()
+        {
+            return payload;
+        }
+    }
+}
