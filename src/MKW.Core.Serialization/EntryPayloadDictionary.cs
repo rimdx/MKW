5e728cead2c201dc31f7b941023188a6882aa5a6
Timofei Zhakov <tima@chemodax.net>
2025-10-04 16:24:23 +0000

    MKW.Core.Serialization: Actually serialize fields inside EntryPayload structure.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@5415 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.Core.Serialization/EntryPayloadDictionary.cs
@@ -0,0 +1,53 @@
+﻿using MKW.Core.Serialization.Exceptions;
+using Org.BouncyCastle.Asn1;
+
+namespace MKW.Core.Serialization
+{
+    internal sealed class EntryPayloadDictionary
+        : Asn1Encodable
+    {
+        private readonly IReadOnlyCollection<KeyValuePair<EntryPayloadKey, string>> items;
+
+        public EntryPayloadDictionary(IReadOnlyCollection<KeyValuePair<EntryPayloadKey, string>> items)
+        {
+            this.items = items;
+        }
+
+        public EntryPayloadDictionary(Asn1Set set)
+        {
+            Dictionary<EntryPayloadKey, string> items = [];
+
+            foreach (Asn1Encodable? item in set)
+            {
+                Asn1Sequence sequence = Asn1Sequence.GetInstance(item);
+                EntryPayloadKeyValuePair serialized = new EntryPayloadKeyValuePair(sequence);
+
+                if (items.ContainsKey(serialized.Key))
+                {
+                    throw new InvalidEntryPayloadDuplicatedKeyException(serialized.Key);
+                }
+
+                items.Add(serialized.Key, serialized.Value);
+            }
+
+            this.items = items;
+        }
+
+        public override Asn1Object ToAsn1Object()
+        {
+            Asn1EncodableVector vector = new Asn1EncodableVector(items.Count);
+
+            foreach (KeyValuePair<EntryPayloadKey, string> item in items)
+            {
+                vector.Add(new EntryPayloadKeyValuePair(item.Key, item.Value));
+            }
+
+            return new DerSet(vector);
+        }
+
+        public IReadOnlyCollection<KeyValuePair<EntryPayloadKey, string>> GetValue()
+        {
+            return items;
+        }
+    }
+}
---
+++ src/MKW.Core.Serialization/Exceptions/InvalidEntryPayloadDuplicatedKeyException.cs
@@ -0,0 +1,7 @@
+﻿namespace MKW.Core.Serialization.Exceptions
+{
+    internal sealed class InvalidEntryPayloadDuplicatedKeyException(EntryPayloadKey key)
+        : InvalidEntryPayload($"Duplicated key '{key}'.")
+    {
+    }
+}
