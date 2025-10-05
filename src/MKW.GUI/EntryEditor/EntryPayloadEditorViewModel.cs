35616e847e6ed2bc1976a56d32957bfd2f34e40c
Timofei Zhakov <tima@chemodax.net>
2025-10-05 13:14:48 +0000

    GUI: Access entry properties using index expressions instead of maintaining dozens of properties.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@5427 0083e6b4-6090-234a-836e-88fcc467669c

--- src/MKW.GUI/EntryEditor/EntryPayloadEditorViewModel.cs
+++ src/MKW.GUI/EntryEditor/EntryPayloadEditorViewModel.cs
@@ -9,43 +9,17 @@ namespace MKW.GUI.EntryEditor
 
         public EntryId Id { get; }
 
+        public EntryPayloadEditorPropertiesViewModel Properties { get; }
+
         public EntryPayloadEditorViewModel(EntryId id, EntryPayload payload)
         {
             Id = id;
             this.payload = payload;
 
+            Properties = new EntryPayloadEditorPropertiesViewModel(payload);
+
             CustomProperties = [];
             RefreshCustomProperties();
-        }
-
-        public string Title
-        {
-            get => payload.GetPropertyOrEmpty(EntryPayloadCommonProperties.Title);
-            set => payload.SetProperty(EntryPayloadCommonProperties.Title, value);
-        }
-
-        public string Username
-        {
-            get => payload.GetPropertyOrEmpty(EntryPayloadCommonProperties.Username);
-            set => payload.SetProperty(EntryPayloadCommonProperties.Username, value);
-        }
-
-        public string Password
-        {
-            get => payload.GetPropertyOrEmpty(EntryPayloadCommonProperties.Password);
-            set => payload.SetProperty(EntryPayloadCommonProperties.Password, value);
-        }
-
-        public string Url
-        {
-            get => payload.GetPropertyOrEmpty(EntryPayloadCommonProperties.Url);
-            set => payload.SetProperty(EntryPayloadCommonProperties.Url, value);
-        }
-
-        public string Notes
-        {
-            get => payload.GetPropertyOrEmpty(EntryPayloadCommonProperties.Notes);
-            set => payload.SetProperty(EntryPayloadCommonProperties.Notes, value);
         }
 
         public EntryPayload GetPayload()
---
+++ src/MKW.GUI/EntryEditor/EntryPayloadValueViewModel.cs
@@ -0,0 +1,24 @@
+﻿using MKW.Core;
+
+namespace MKW.GUI.EntryEditor
+{
+    public class EntryPayloadValueViewModel : ViewModelBase
+    {
+        private readonly EntryPayload payload;
+        private readonly EntryPayloadKey key;
+
+        public EntryPayloadValueViewModel(EntryPayload payload, EntryPayloadKey key)
+        {
+            this.payload = payload;
+            this.key = key;
+        }
+
+        public string DisplayValue => Value;
+
+        public string Value
+        {
+            get => payload.GetPropertyOrEmpty(key);
+            set => payload.SetProperty(key, value);
+        }
+    }
+}
