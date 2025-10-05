a6660b3e3088d579cd41a60509b4e722f0de2877
Timofei Zhakov <tima@chemodax.net>
2025-10-05 15:36:24 +0000

    Make EntryPayloadKey format more strict.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@5438 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.Core/EntryPayloadParser.cs
@@ -0,0 +1,44 @@
+﻿using MKW.Core.Exceptions;
+
+namespace MKW.Core
+{
+    internal static class EntryPayloadParser
+    {
+        public const char NamespaceSeparator = ':';
+
+        private const string AllowedCharacters =
+            "0123456789" +
+            "abcdefghijklmnopqrstuvwxyz" +
+            "_";
+
+        public static void ValidateKeyComponent(string component)
+        {
+            if (component == string.Empty)
+            {
+                throw new InvalidEntryPayloadKey("empty component found.");
+            }
+
+            foreach (char c in component)
+            {
+                if (!AllowedCharacters.Contains(c))
+                {
+                    throw new InvalidEntryPayloadKey($"only Latin letters, numbers, and underscores are allowed.");
+                }
+            }
+        }
+
+        public static string ParseKey(string key)
+        {
+            string lowered = key.ToLower();
+
+            string[] components = lowered.Split(NamespaceSeparator);
+
+            foreach (string component in components)
+            {
+                ValidateKeyComponent(component);
+            }
+
+            return string.Join(NamespaceSeparator.ToString(), components);
+        }
+    }
+}
---
+++ src/MKW.Tests/EntryPayloadTests.cs
@@ -0,0 +1,26 @@
+﻿using MKW.Core;
+using MKW.Core.Exceptions;
+using NUnit.Framework.Legacy;
+
+namespace MKW.Tests
+{
+    public class EntryPayloadTests
+    {
+        [Test]
+        public void EntryPayloadKeyParseTests()
+        {
+            EntryPayloadKey key = new EntryPayloadKey("a:b:c");
+            ClassicAssert.AreEqual("a:b:c", key.ToString());
+
+            EntryPayloadKey branch = key.Branch("x");
+
+            Assert.Throws<InvalidEntryPayloadKey>(() => new EntryPayloadKey("a:b:c:"));
+            Assert.Throws<InvalidEntryPayloadKey>(() => new EntryPayloadKey(""));
+            Assert.Throws<InvalidEntryPayloadKey>(() => new EntryPayloadKey("ъъ"));
+            Assert.Throws<InvalidEntryPayloadKey>(() => branch.Branch(""));
+            Assert.Throws<InvalidEntryPayloadKey>(() => branch.Branch("ъъ"));
+            Assert.Throws<InvalidEntryPayloadKey>(() => branch.Branch("y:z"));
+            Assert.Throws<InvalidEntryPayloadKey>(() => branch.Branch(":"));
+        }
+    }
+}
