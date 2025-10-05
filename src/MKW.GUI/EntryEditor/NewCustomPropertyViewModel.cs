c05af8a81c6fb24aac09effde5045ef68319be0d
Timofei Zhakov <tima@chemodax.net>
2025-10-05 19:09:43 +0000

    GUI: Display recent property names in a dropdown when editing or creating new custom property.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@5450 0083e6b4-6090-234a-836e-88fcc467669c

--- src/MKW.GUI/EntryEditor/NewCustomPropertyViewModel.cs
+++ src/MKW.GUI/EntryEditor/NewCustomPropertyViewModel.cs
@@ -1,12 +1,17 @@
-﻿namespace MKW.GUI.EntryEditor
+﻿using MKW.GUI.Model;
+
+namespace MKW.GUI.EntryEditor
 {
     public class NewCustomPropertyViewModel : ViewModelBase
     {
         private readonly EntryPayloadEditorPropertiesViewModel properties;
+        public CommonEntryPropertiesModel CommonPropertiesModel { get; }
 
-        public NewCustomPropertyViewModel(EntryPayloadEditorPropertiesViewModel properties)
+        public NewCustomPropertyViewModel(EntryPayloadEditorPropertiesViewModel properties,
+                                          CommonEntryPropertiesModel commonPropertiesModel)
         {
             this.properties = properties;
+            CommonPropertiesModel = commonPropertiesModel;
 
             content = string.Empty;
             name= string.Empty;
