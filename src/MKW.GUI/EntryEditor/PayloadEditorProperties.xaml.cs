289855e960f9f23d29024556aab1335236930e48
Timofei Zhakov <tima@chemodax.net>
2025-10-05 11:44:04 +0000

    GUI: EntryEditor: Add 'Properties' page allowing user to declare custom properties for each entry.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@5425 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.GUI/EntryEditor/PayloadEditorProperties.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace MKW.GUI.EntryEditor
+{
+    public partial class PayloadEditorProperties : UserControl
+    {
+        public PayloadEditorProperties()
+        {
+            InitializeComponent();
+        }
+
+        private void PropertiesListView_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                throw new NotImplementedException();
+            }
+            catch (Exception ex)
+            {
+                ErrorReporter.HandleException(Window.GetWindow(this), ex);
+            }
+        }
+
+        private void AddCustomProperty_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            try
+            {
+                throw new NotImplementedException();
+            }
+            catch (Exception ex)
+            {
+                ErrorReporter.HandleException(Window.GetWindow(this), ex);
+            }
+        }
+
+        private void EditCustomProperty_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            try
+            {
+                throw new NotImplementedException();
+            }
+            catch (Exception ex)
+            {
+                ErrorReporter.HandleException(Window.GetWindow(this), ex);
+            }
+        }
+
+        private void DeleteCustomProperty_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            try
+            {
+                throw new NotImplementedException();
+            }
+            catch (Exception ex)
+            {
+                ErrorReporter.HandleException(Window.GetWindow(this), ex);
+            }
+        }
+    }
+}
