2fe1ae72afa0687604791854eb8829d7b714929e
Timofei Zhakov <tima@chemodax.net>
2025-10-05 10:57:07 +0000

    GUI: EntryEditor: Add multiple tabs support and highlight their layout.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@5423 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.GUI/EntryEditor/PayloadEditorGeneral.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace MKW.GUI.EntryEditor
+{
+    public partial class PayloadEditorGeneral : UserControl
+    {
+        public PayloadEditorGeneral()
+        {
+            InitializeComponent();
+        }
+    }
+}
