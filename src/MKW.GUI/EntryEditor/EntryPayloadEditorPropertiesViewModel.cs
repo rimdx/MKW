a289efe8cbf17f39ac041b78cf52fc697570ef5d
Timofei Zhakov <tima@chemodax.net>
2025-10-05 13:14:48 +0000

    GUI: Access entry properties using index expressions instead of maintaining dozens of properties.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@5427 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.GUI/EntryEditor/EntryPayloadEditorPropertiesViewModel.cs
@@ -0,0 +1,39 @@
+﻿using MKW.Core;
+
+namespace MKW.GUI.EntryEditor
+{
+    public class EntryPayloadEditorPropertiesViewModel : ViewModelBase
+    {
+        private readonly Dictionary<EntryPayloadKey, EntryPayloadValueViewModel> values;
+
+        private readonly EntryPayload payload;
+
+        public EntryPayloadEditorPropertiesViewModel(EntryPayload payload)
+        {
+            this.payload = payload;
+
+            values = [];
+            foreach (KeyValuePair<EntryPayloadKey, string> item in payload)
+            {
+                values[item.Key] = new EntryPayloadValueViewModel(payload, item.Key);
+            }
+        }
+
+        public EntryPayloadValueViewModel this[string keyStr]
+        {
+            get
+            {
+                EntryPayloadKey key = new EntryPayloadKey(keyStr);
+
+                if (values.TryGetValue(key, out EntryPayloadValueViewModel value))
+                {
+                    return value;
+                }
+
+                EntryPayloadValueViewModel result = new EntryPayloadValueViewModel(payload, key);
+                values.Add(key, result);
+                return result;
+            }
+        }
+    }
+}
