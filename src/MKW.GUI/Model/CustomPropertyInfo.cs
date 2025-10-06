080395ce1faa77a7d643e8a732ffd6828712a7d0
Timofei Zhakov <tima@chemodax.net>
2025-10-06 16:04:54 +0000

    GUI: Add CustomPropertyInfo structure to store some metadata about custom property.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@5453 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.GUI/Model/CustomPropertyInfo.cs
@@ -0,0 +1,26 @@
+﻿using MKW.Core;
+
+namespace MKW.GUI.Model
+{
+    public sealed class CustomPropertyInfo : ViewModelBase
+    {
+        public EntryPayloadKey Key { get; }
+        public string Name { get; }
+
+        public CustomPropertyInfo(string name)
+        {
+            Name = name;
+            Key = CommonEntryPropertiesModel.CustomPropertyNamespace.Branch(name);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is CustomPropertyInfo info && Key == info.Key;
+        }
+
+        public override int GetHashCode()
+        {
+            return Key.GetHashCode();
+        }
+    }
+}
