4eedee33502955cf9599064722569a9ceb96688d
Timofei Zhakov <tima@chemodax.net>
2025-10-09 15:56:20 +0000

    GUI.BackupModel: Load all entries into itself as BackupModelEntry.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@5472 0083e6b4-6090-234a-836e-88fcc467669c

--- src/MKW.GUI/Model/BackupModel.cs
+++ src/MKW.GUI/Model/BackupModel.cs
@@ -1,4 +1,5 @@
 ﻿using MKW.Core;
+using System.Collections.ObjectModel;
 
 namespace MKW.GUI.Model
 {
@@ -7,18 +8,27 @@ namespace MKW.GUI.Model
         private readonly IUserSession user;
         private readonly KeePassXmlBackup backup;
 
+        public ObservableCollection<BackupModelEntry> Entries { get; }
+
         public BackupModel(IUserSession user, KeePassXmlBackup backup)
         {
             this.user = user;
             this.backup = backup;
+
+            Entries = [];
+
+            foreach (EntryPayload payload in backup.EnumerateEntries())
+            {
+                Entries.Add(new BackupModelEntry(payload));
+            }
         }
 
         public void Import()
         {
-            foreach (EntryPayload payload in backup.EnumerateEntries())
+            foreach (BackupModelEntry entry in Entries)
             {
                 using IEntrySession entrySession = user.CreateEntry();
-                entrySession.UpdatePayload(payload);
+                entrySession.UpdatePayload(entry.Payload);
             }
         }
     }
