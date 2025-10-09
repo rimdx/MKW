3e6f77f4ae0392876a12d519dfef1eef29c9326c
Timofei Zhakov <tima@chemodax.net>
2025-10-09 16:09:40 +0000

    GUI.EntryEditor: Factor out abstract CustomPropertyEditorViewModelBase.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@5475 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.GUI/EntryEditor/CustomPropertyEditorViewModelBase.cs
@@ -0,0 +1,27 @@
+﻿namespace MKW.GUI.EntryEditor
+{
+    public abstract class CustomPropertyEditorViewModelBase : ViewModelBase
+    {
+        public CustomPropertyEditorViewModelBase(string name, string content)
+        {
+            this.name = name;
+            this.content = content;
+        }
+
+        private string name;
+        public string Name
+        {
+            get => name;
+            set => SetProperty(ref name, value);
+        }
+
+        private string content;
+        public string Content
+        {
+            get => content;
+            set => SetProperty(ref content, value);
+        }
+
+        public abstract void OnOK();
+    }
+}
