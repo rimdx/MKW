7c719643d3d1112ff0177d8fbef76dac3aef73ba
Timofei Zhakov <tima@chemodax.net>
2025-10-09 15:56:20 +0000

    GUI.BackupModel: Load all entries into itself as BackupModelEntry.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@5472 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.GUI/Model/BackupModelEntry.cs
@@ -0,0 +1,22 @@
+﻿using MKW.Core;
+
+namespace MKW.GUI.Model
+{
+    public sealed class BackupModelEntry : ViewModelBase
+    {
+        private bool isSelected;
+        public bool IsSelected
+        {
+            get => isSelected;
+            set => SetProperty(ref isSelected, value);
+        }
+
+        public EntryPayload Payload { get; }
+
+        public BackupModelEntry(EntryPayload payload)
+        {
+            isSelected = true;
+            Payload = payload;
+        }
+    }
+}
