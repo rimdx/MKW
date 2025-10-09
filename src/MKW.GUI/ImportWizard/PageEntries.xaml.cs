b3f099b3f0f5ae97d4be63e4a37d235cfc7bd464
Timofei Zhakov <tima@chemodax.net>
2025-10-09 16:22:58 +0000

    GUI.ImportWizard: Add blank page for picking entries to import.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@5477 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.GUI/ImportWizard/PageEntries.xaml.cs
@@ -0,0 +1,17 @@
+﻿using MKW.GUI.Wizard;
+
+namespace MKW.GUI.ImportWizard
+{
+    public partial class PageEntries : WizardPage
+    {
+        private readonly ImportWizardViewModel viewModel;
+
+        public PageEntries(ImportWizardViewModel viewModel)
+            : base("Pick Entries")
+        {
+            this.viewModel = viewModel;
+            DataContext = viewModel;
+            InitializeComponent();
+        }
+    }
+}
