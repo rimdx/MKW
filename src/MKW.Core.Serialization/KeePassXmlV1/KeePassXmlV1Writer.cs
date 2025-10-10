850c0c2696869934f665cb6d065c58f3f4c081bd
Timofei Zhakov <tima@chemodax.net>
2025-10-10 17:02:03 +0000

    Add KeePassXmlV1Writer.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@5508 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.Core.Serialization/KeePassXmlV1/KeePassXmlV1Writer.cs
@@ -0,0 +1,40 @@
+﻿using System.Xml;
+
+namespace MKW.Core.Serialization.KeePassXmlV1
+{
+    public sealed class KeePassXmlV1Writer : IDisposable
+    {
+        private readonly XmlWriter writer;
+
+        public KeePassXmlV1Writer(Stream stream)
+        {
+            writer = XmlWriter.Create(stream, new XmlWriterSettings
+            {
+                Indent = true,
+            });
+
+            writer.WriteStartDocument();
+            writer.WriteStartElement("pwlist");
+        }
+
+        public void WriteEntry(BackupEntry entry)
+        {
+            writer.WriteStartElement("pwentry");
+
+            foreach (KeyValuePair<string, string> field in entry.Fields)
+            {
+                writer.WriteStartElement(field.Key);
+                writer.WriteString(field.Value);
+                writer.WriteEndElement();
+            }
+
+            writer.WriteEndElement();
+        }
+
+        public void Dispose()
+        {
+            writer.WriteEndDocument();
+            writer.Dispose();
+        }
+    }
+}
