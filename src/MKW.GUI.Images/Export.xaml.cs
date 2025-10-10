4b83e7b52aa9bcd1aca6c2124af8b39257b88128
Timofei Zhakov <tima@chemodax.net>
2025-10-10 20:30:36 +0000

    GUI.Images: Add 'Import' and 'Export' icons.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@5533 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.GUI.Images/Export.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace MKW.GUI.Images
+{
+    public partial class Export : Viewbox
+    {
+        public Export()
+        {
+            InitializeComponent();
+        }
+    }
+}
