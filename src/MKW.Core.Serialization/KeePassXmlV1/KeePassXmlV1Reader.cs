ff064704a5ab1600de4aacf502d87924245b474c
Timofei Zhakov <tima@chemodax.net>
2025-10-10 16:44:30 +0000

    Add reader for KeePassXmlV1.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@5504 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.Core.Serialization/KeePassXmlV1/KeePassXmlV1Reader.cs
@@ -0,0 +1,125 @@
+﻿using System.Xml;
+
+namespace MKW.Core.Serialization.KeePassXmlV1
+{
+    public sealed class KeePassXmlV1Reader : IDisposable
+    {
+        private readonly XmlReader reader;
+
+        public KeePassXmlV1Reader(Stream stream)
+        {
+            reader = XmlReader.Create(stream);
+        }
+
+        public BackupEntry? ReadEntry()
+        {
+            while (reader.Read())
+            {
+                if (reader.NodeType == XmlNodeType.Element)
+                {
+                    if (reader.Name == "pwentry")
+                    {
+                        Dictionary<string, string> fields = [];
+
+                        using (XmlReader entryReader = reader.ReadSubtree())
+                        {
+                            foreach (KeyValuePair<string, string> pair in ReadEntryElement(entryReader))
+                            {
+                                fields.Add(pair.Key, pair.Value);
+                            }
+                        }
+
+                        return new BackupEntry(fields);
+                    }
+                    else if (reader.Name == "pwlist")
+                    {
+                        // ingore root
+                    }
+                    else
+                    {
+                        throw new Exception($"Unexpected tag: {reader.Name}");
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        private static IEnumerable<KeyValuePair<string, string>> ReadEntryElement(XmlReader entryReader)
+        {
+            entryReader.MoveToContent(); // <pwentry>
+
+            while (entryReader.Read())
+            {
+                if (entryReader.NodeType == XmlNodeType.Element)
+                {
+                    using XmlReader fieldReader = entryReader.ReadSubtree();
+
+                    yield return ReadEntryField(fieldReader);
+                }
+                else if (entryReader.NodeType == XmlNodeType.EndElement)
+                {
+                    yield break;
+                }
+            }
+        }
+
+        private static KeyValuePair<string, string> ReadEntryField(XmlReader fieldReader)
+        {
+            string? key = null;
+            string? content = null;
+
+            while (fieldReader.Read())
+            {
+                if (fieldReader.NodeType == XmlNodeType.Text)
+                {
+                    content = fieldReader.ReadContentAsString();
+                }
+                if (fieldReader.NodeType == XmlNodeType.Element)
+                {
+                    if (key == null)
+                    {
+                        key = fieldReader.Name;
+                    }
+                    else
+                    {
+                        throw new Exception($"Unexpected tag: {fieldReader.Name}.");
+                    }
+                }
+                else if (fieldReader.NodeType == XmlNodeType.EndElement)
+                {
+                    break;
+                }
+            }
+
+            if (key == null)
+            {
+                throw new Exception("Key is missing.");
+            }
+
+            return new KeyValuePair<string, string>(key, content ?? string.Empty);
+        }
+
+        public IEnumerable<BackupEntry> GetEntriesEnumerator()
+        {
+            while (true)
+            {
+                BackupEntry? entry = ReadEntry();
+
+                if (entry != null)
+                {
+                    yield return entry;
+                }
+                else
+                {
+                    yield break;
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            reader.Dispose();
+        }
+    }
+}
