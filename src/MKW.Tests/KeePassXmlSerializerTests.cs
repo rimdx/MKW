463886f4e2c0688db520b9908a2035f169c6ec78
Timofei Zhakov <tima@chemodax.net>
2025-10-10 15:30:54 +0000

    MKW.Tests: Upload kee_pass_export_v1 and organise files.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@5503 0083e6b4-6090-234a-836e-88fcc467669c

--- src/MKW.Tests/KeePassXmlSerializerTests.cs
+++ src/MKW.Tests/KeePassXmlSerializerTests.cs
@@ -9,7 +9,7 @@ namespace MKW.Tests
         [Test]
         public void SimpleTest()
         {
-            FileStream stream = new FileStream("key_pass_export.xml", FileMode.Open, FileAccess.Read);
+            FileStream stream = new FileStream("key_pass_export_v2.xml", FileMode.Open, FileAccess.Read);
             KeePassXmlReader reader = new KeePassXmlReader(stream);
 
             BackupEntry[] entries = reader.EnumerateEntries().ToArray();
