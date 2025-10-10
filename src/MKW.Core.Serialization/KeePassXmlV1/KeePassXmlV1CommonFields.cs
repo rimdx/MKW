827b16b75552a2bd2e67fcbfe078cf23852472f7
Timofei Zhakov <tima@chemodax.net>
2025-10-10 17:11:26 +0000

    GUI: Support KeePassXmlV1.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@5510 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.Core.Serialization/KeePassXmlV1/KeePassXmlV1CommonFields.cs
@@ -0,0 +1,11 @@
+﻿namespace MKW.Core.Serialization.KeePassXmlV1
+{
+    public static class KeePassXmlV1CommonFields
+    {
+        public const string Title = "title";
+        public const string Username = "username";
+        public const string Password = "password";
+        public const string Url = "url";
+        public const string Notes = "notes";
+    }
+}
---
+++ src/MKW.GUI/Model/KeePassXmlV1Backup.cs
@@ -0,0 +1,49 @@
+﻿using MKW.Core;
+using MKW.Core.Serialization;
+using MKW.Core.Serialization.KeePassXmlV1;
+using System.IO;
+
+namespace MKW.GUI.Model
+{
+    public class KeePassXmlV1Backup : IBackup, IDisposable
+    {
+        private readonly Stream stream;
+
+        public KeePassXmlV1Backup(Stream stream)
+        {
+            this.stream = stream;
+        }
+
+        private static EntryPayloadKey ConvertPropertyName(string propname) => propname switch
+        {
+            KeePassXmlV1CommonFields.Title => CommonEntryPropertiesModel.Title.Key,
+            KeePassXmlV1CommonFields.Username => CommonEntryPropertiesModel.Username.Key,
+            KeePassXmlV1CommonFields.Password => CommonEntryPropertiesModel.Password.Key,
+            KeePassXmlV1CommonFields.Url => CommonEntryPropertiesModel.Url.Key,
+            KeePassXmlV1CommonFields.Notes => CommonEntryPropertiesModel.Notes.Key,
+            _ => CommonEntryPropertiesModel.CustomPropertyNamespace.Branch(propname),
+        };
+
+        public IEnumerable<EntryPayload> EnumerateEntries()
+        {
+            using KeePassXmlV1Reader reader = new KeePassXmlV1Reader(stream);
+
+            foreach (BackupEntry backupEntry in reader.GetEntriesEnumerator())
+            {
+                EntryPayload payload = new EntryPayload();
+
+                foreach (KeyValuePair<string, string> property in backupEntry.Fields)
+                {
+                    payload.SetProperty(ConvertPropertyName(property.Key), property.Value);
+                }
+
+                yield return payload;
+            }
+        }
+
+        public void Dispose()
+        {
+            stream.Dispose();
+        }
+    }
+}
