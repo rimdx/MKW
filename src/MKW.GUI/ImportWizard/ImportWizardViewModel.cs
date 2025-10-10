21479cb058da688faef7c5c46cca9251cb727286
Timofei Zhakov <tima@chemodax.net>
2025-10-10 20:31:21 +0000

    GUI: Import and Export wizards: Setup icons.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@5534 0083e6b4-6090-234a-836e-88fcc467669c

--- src/MKW.GUI/ImportWizard/ImportWizardViewModel.cs
+++ src/MKW.GUI/ImportWizard/ImportWizardViewModel.cs
@@ -17,7 +17,7 @@ namespace MKW.GUI.ImportWizard
         private BackupImportModel? backupModel;
 
         public ImportWizardViewModel(DatabaseUnlockedModel database)
-            : base("Import Data", ImageMoniker.None)
+            : base("Import Data", ImageMoniker.Image)
         {
             this.database = database;
 
