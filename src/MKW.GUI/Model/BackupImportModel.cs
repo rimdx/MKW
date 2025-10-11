60fcc1625a2d5e402f1ea996346b11ef6a8ec35f
Timofei Zhakov <tima@chemodax.net>
2025-10-11 08:00:18 +0000

    GUI.Model.BackupImportModel: Untick duplicated entries.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@5541 0083e6b4-6090-234a-836e-88fcc467669c

--- src/MKW.GUI/Model/BackupImportModel.cs
+++ src/MKW.GUI/Model/BackupImportModel.cs
@@ -10,13 +10,46 @@ namespace MKW.GUI.Model
 
         public BackupImportModel(DatabaseUnlockedModel database, IBackupReader backup)
         {
-            foreach (EntryPayload payload in backup.EnumerateEntries())
-            {
-                Entries.Add(new BackupModelEntry(payload));
-            }
-
             this.database = database;
             this.backup = backup;
+
+            foreach (EntryPayload payload in backup.EnumerateEntries())
+            {
+                BackupModelEntry entry = new BackupModelEntry(payload);
+
+                if (FindSimilar(payload) != null)
+                {
+                    entry.IsSelected = false;
+                }
+
+                Entries.Add(entry);
+            }
+        }
+
+        private EntryEditorModel? FindSimilar(EntryPayload payload)
+        {
+            string title1 = payload.GetPropertyOrEmpty(CommonEntryPropertiesModel.Title.Key);
+            string username1 = payload.GetPropertyOrEmpty(CommonEntryPropertiesModel.Username.Key);
+
+            if (title1 == string.Empty && username1 == string.Empty)
+            {
+                return null;
+            }
+
+            foreach (EntryEditorModel entry in database.Entries)
+            {
+                EntryPayload entryPl = entry.GetPayload();
+
+                string title2 = entryPl.GetPropertyOrEmpty(CommonEntryPropertiesModel.Title.Key);
+                string username2 = entryPl.GetPropertyOrEmpty(CommonEntryPropertiesModel.Username.Key);
+
+                if (title1 == title2 && username1 == username2)
+                {
+                    return entry;
+                }
+            }
+
+            return null;
         }
 
         public void Import()
