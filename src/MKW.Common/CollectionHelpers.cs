16c0dfe8988db9e1a1cbf25e1dadf15e16c20c63
Timofei Zhakov <tima@chemodax.net>
2025-10-12 15:06:35 +0000

    Implement Merge() for two collections.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@5594 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.Common/CollectionHelpers.cs
@@ -0,0 +1,62 @@
+﻿namespace MKW.Common
+{
+    public static class CollectionHelpers
+    {
+        public static IEnumerable<LeftRightPair<T>> Merge<T>(IEnumerable<T> left,
+                                                             IEnumerable<T> right,
+                                                             IComparer<T> comparer)
+            where T : class
+        {
+            List<T> leftSorted = [.. left];
+            List<T> rightSorted = [.. right];
+
+            leftSorted.Sort(comparer);
+            rightSorted.Sort(comparer);
+
+            using IEnumerator<T> leftEnum = leftSorted.GetEnumerator();
+            using IEnumerator<T> rightEnum = rightSorted.GetEnumerator();
+
+            bool leftHasValue = leftEnum.MoveNext();
+            bool rightHasValue = rightEnum.MoveNext();
+
+            while (leftHasValue && rightHasValue)
+            {
+                int diff = comparer.Compare(leftEnum.Current, rightEnum.Current);
+
+                if (diff == 0)
+                {
+                    yield return new LeftRightPair<T>(leftEnum.Current, rightEnum.Current);
+
+                    leftHasValue = leftEnum.MoveNext();
+                    rightHasValue = rightEnum.MoveNext();
+                }
+                else if (diff < 0)
+                {
+                    yield return new LeftRightPair<T>(leftEnum.Current, null);
+                    leftHasValue = leftEnum.MoveNext();
+                }
+                else if (diff > 0)
+                {
+                    yield return new LeftRightPair<T>(null, rightEnum.Current);
+                    rightHasValue = rightEnum.MoveNext();
+                }
+                else
+                {
+                    // never reached
+                }
+            }
+
+            while (leftHasValue)
+            {
+                yield return new LeftRightPair<T>(leftEnum.Current, null);
+                leftHasValue = leftEnum.MoveNext();
+            }
+
+            while (rightHasValue)
+            {
+                yield return new LeftRightPair<T>(null, rightEnum.Current);
+                rightHasValue = rightEnum.MoveNext();
+            }
+        }
+    }
+}
---
+++ src/MKW.Common/LeftRightPair.cs
@@ -0,0 +1,7 @@
+﻿namespace MKW.Common
+{
+    public record class LeftRightPair<T>(
+        T? Left,
+        T? Right
+    ) where T : class;
+}
