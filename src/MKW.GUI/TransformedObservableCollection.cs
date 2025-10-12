4f8b79106c2e8a1442cb8277771e9fe43eb9ea8a
Ivan.Z <53048645+chemodax@users.noreply.github.com>
2025-10-12 17:11:53 +0000

    GUI: Add TransformedObservableCollection.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@5600 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.GUI/TransformedObservableCollection.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.ObjectModel;
+
+namespace MKW.GUI
+{
+    public abstract class TransformedObservableCollection<TViewModel, TItem> : ObservableCollection<TViewModel>
+    {
+        protected abstract TViewModel CreateViewModel(TItem item);
+        protected abstract void UpdateViewModel(TViewModel viewModel, TItem item);
+        protected abstract object GetViewModelKey(TViewModel viewModel);
+        protected abstract object GetItemKey(TItem item);
+
+        public void SetItems(IEnumerable<TItem> newItems)
+        {
+            Dictionary<object, TItem> itemsDictionary = [];
+            foreach (TItem item in newItems)
+            {
+                itemsDictionary.Add(GetItemKey(item), item);
+            }
+
+            using (BlockReentrancy())
+            {
+                for (int i = 0; i < Count;)
+                {
+                    TViewModel viewModel = this[i];
+                    object key = GetViewModelKey(viewModel);
+                    if (itemsDictionary.TryGetValue(key, out TItem item))
+                    {
+                        UpdateViewModel(viewModel, item);
+                        itemsDictionary.Remove(key);
+                        i++;
+                    }
+                    else
+                    {
+                        RemoveItem(i);
+                    }
+                }
+
+                foreach (TItem item in itemsDictionary.Values)
+                {
+                    InsertItem(Count, CreateViewModel(item));
+                }
+            }
+        }
+    }
+}
