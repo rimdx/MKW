10ccdb809b0bd0a1bf7fddc80f797b6b9d9c3182
Ivan.Z <53048645+chemodax@users.noreply.github.com>
2025-10-12 15:43:21 +0000

    GUI: Move DocumentTable.Document class to separate file.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@5599 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.GUI/Model/DocumentTable.Document.cs
@@ -0,0 +1,79 @@
+﻿namespace MKW.GUI.Model
+{
+    public sealed partial class DocumentTable
+    {
+        private sealed class Document : IDisposable
+        {
+            public DatabaseModel Database { get; }
+            private int lockCount;
+            private bool disposed;
+            private readonly DocumentTable documentTable;
+
+            private Document(DocumentTable documentTable, DatabaseModel database)
+            {
+                this.documentTable = documentTable;
+                Database = database;
+                lockCount = 0;
+            }
+
+            public static Document Make(DocumentTable documentTable, DatabaseModel database)
+            {
+                return new Document(documentTable, database);
+            }
+
+            public IDocumentLock ObtainLock()
+            {
+                lockCount++;
+
+                return new DocumentLock(this);
+            }
+
+            public void Dispose()
+            {
+                if (!disposed)
+                {
+                    Database.Dispose();
+                    documentTable.documents.Remove(this);
+
+                    disposed = true;
+                }
+            }
+
+            private void ReleaseLock()
+            {
+                lockCount--;
+                if (lockCount == 0)
+                {
+                    Dispose();
+                }
+            }
+
+            private sealed class DocumentLock : IDocumentLock
+            {
+                private readonly Document document;
+                private bool disposed;
+
+                public DocumentLock(Document document)
+                {
+                    this.document = document;
+                }
+
+                public DatabaseModel Database => document.Database;
+
+                public IDocumentLock Clone()
+                {
+                    return document.ObtainLock();
+                }
+
+                public void Dispose()
+                {
+                    if (!disposed)
+                    {
+                        document.ReleaseLock();
+                        disposed = true;
+                    }
+                }
+            }
+        }
+    }
+}
