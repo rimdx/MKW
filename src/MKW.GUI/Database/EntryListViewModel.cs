f77810423e29ae2d34f2686e3f03ecfcacbe68c4
Ivan.Z <53048645+chemodax@users.noreply.github.com>
2025-10-12 17:12:43 +0000

    GUI: Implement DatabaseEntryCollectionViewModel via TransformedObservableCollection.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@5601 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.GUI/Database/EntryListViewModel.cs
@@ -0,0 +1,23 @@
+﻿using MKW.Core;
+using MKW.GUI.Model;
+
+namespace MKW.GUI.Database
+{
+    public class EntryListViewModel : ViewModelBase
+    {
+        private EntryEditorModel entryEditorModel;
+
+        public EntryListViewModel(EntryEditorModel entryEditorModel)
+        {
+            this.entryEditorModel = entryEditorModel;
+        }
+
+        public EntryEditorModel EntryEditorModel
+        {
+            get => entryEditorModel;
+            set => SetProperty(ref entryEditorModel, value);
+        }
+
+        public EntryId Id => EntryEditorModel.Id;
+    }
+}
