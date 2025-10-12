14ebd4608f3ed5961e7bfcbc6ef222c23b63776a
Ivan.Z <53048645+chemodax@users.noreply.github.com>
2025-10-12 13:24:03 +0000

    DocumentTable: Maintain document list.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@5588 0083e6b4-6090-234a-836e-88fcc467669c

--- src/MKW.GUI/Model/DocumentTable.cs
+++ src/MKW.GUI/Model/DocumentTable.cs
@@ -1,35 +1,82 @@
 ﻿using MKW.Cryptography;
 using MKW.Cryptography.Loader;
+using System.IO;
 
 namespace MKW.GUI.Model
 {
     public sealed class DocumentTable
     {
         private readonly ICryptographyProvider cryptographyProvider;
+        private readonly List<Document> documents;
 
         public DocumentTable()
         {
             cryptographyProvider = CryptographyLoader.GetProvider();
+            documents = [];
         }
 
         public IDocumentLock CreateDatabase(string databasePath, string password)
         {
-             return new DocumentLock(DatabaseModel.Create(cryptographyProvider, databasePath, password));
+            Document? doc = GetDocumentByPath(databasePath);
+            if (doc == null)
+            {
+                doc = new Document(DatabaseModel.Create(cryptographyProvider, databasePath, password));
+            }
+
+            return new DocumentLock(doc);
         }
 
         public IDocumentLock OpenTable(string filename)
         {
-            return new DocumentLock(DatabaseModel.Open(cryptographyProvider, filename));
+            Document? doc = GetDocumentByPath(filename);
+            if (doc == null)
+            {
+                doc = new Document(DatabaseModel.Open(cryptographyProvider, filename));
+            }
+
+            return new DocumentLock(doc);
+        }
+
+        private Document? GetDocumentByPath(string path)
+        {
+            string fullPath = Path.GetFullPath(path);
+
+            foreach (Document doc in documents)
+            {
+                if (string.Compare(Path.GetFullPath(doc.Database.Path), fullPath, StringComparison.OrdinalIgnoreCase) == 0)
+                {
+                    return doc;
+                }
+            }
+
+            return null;
+        }
+
+        private class Document : IDisposable
+        {
+            public DatabaseModel Database { get; }
+
+            public Document(DatabaseModel database)
+            {
+                Database = database;
+            }
+
+            public void Dispose()
+            {
+                Database.Dispose();
+            }
         }
 
         private class DocumentLock : IDocumentLock
         {
-            public DatabaseModel Database { get; }
+            private readonly Document document;
 
-            public DocumentLock(DatabaseModel databaseModel)
+            public DocumentLock(Document document)
             {
-                Database = databaseModel;
+                this.document = document;
             }
+
+            public DatabaseModel Database => document.Database;
 
             public void Dispose()
             {
