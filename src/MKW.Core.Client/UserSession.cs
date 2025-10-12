ebd2c596c29cd77846070649ad4a07928a28f9b7
Timofei Zhakov <tima@chemodax.net>
2025-10-12 08:31:56 +0000

    Drop IEntryController.UpdateEntry().
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@5571 0083e6b4-6090-234a-836e-88fcc467669c

--- src/MKW.Core.Client/UserSession.cs
+++ src/MKW.Core.Client/UserSession.cs
@@ -73,11 +73,6 @@ namespace MKW.Core.Client
             return entryController.DeleteEntry(id);
         }
 
-        public EntryInfo UpdateEntry(EntryId id, EntryPayload? payload)
-        {
-            return entryController.UpdateEntry(id, payload);
-        }
-
         public IEnumerable<IEntrySession> EnumerateEntries()
         {
             foreach (IEntrySession entry in entryController.EnumerateEntries())
