ecff4cead6e10343bbcaf47a52e9ea7d53aded7a
Ivan.Z <53048645+chemodax@users.noreply.github.com>
2025-10-12 15:37:43 +0000

    GUI: DocumentTable: Create document data by external document data factory.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@5597 0083e6b4-6090-234a-836e-88fcc467669c

--- src/MKW.GUI/AppModel.cs
+++ src/MKW.GUI/AppModel.cs
@@ -1,24 +1,28 @@
-﻿using MKW.GUI.Model;
+﻿using MKW.Cryptography;
+using MKW.Cryptography.Loader;
+using MKW.GUI.Model;
 
 namespace MKW.GUI
 {
     public class AppModel
     {
         private readonly DocumentTable documentTable;
+        private readonly ICryptographyProvider cryptographyProvider;
 
         public AppModel()
         {
+            cryptographyProvider = CryptographyLoader.GetProvider();
             documentTable = new DocumentTable();
         }
 
         public IDocumentLock CreateDatabase(string databasePath, string password)
         {
-            return documentTable.CreateDatabase(databasePath, password);
+            return documentTable.OpenDocument(databasePath, () => DatabaseModel.Create(cryptographyProvider, databasePath, password));
         }
 
         public IDocumentLock OpenDatabase(string filename)
         {
-            return documentTable.OpenDatabase(filename);
+            return documentTable.OpenDocument(filename, () => DatabaseModel.Open(cryptographyProvider, filename));
         }
     }
 }
