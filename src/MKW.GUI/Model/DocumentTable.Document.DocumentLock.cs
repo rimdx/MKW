d4f3adf43b72379443dcdb68f3a68aa1196e34ee
Timofei Zhakov <tima@chemodax.net>
2025-10-13 16:16:37 +0000

    Move DocumentTable.Document.DocumentLock to a separate file.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@5618 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.GUI/Model/DocumentTable.Document.DocumentLock.cs
@@ -0,0 +1,35 @@
+﻿namespace MKW.GUI.Model
+{
+    public sealed partial class DocumentTable
+    {
+        private sealed partial class Document
+        {
+            private sealed class DocumentLock : IDocumentLock
+            {
+                private readonly Document document;
+                private bool disposed;
+
+                public DocumentLock(Document document)
+                {
+                    this.document = document;
+                }
+
+                public DatabaseModel Database => document.Database;
+
+                public IDocumentLock Clone()
+                {
+                    return document.ObtainLock();
+                }
+
+                public void Dispose()
+                {
+                    if (!disposed)
+                    {
+                        document.ReleaseLock();
+                        disposed = true;
+                    }
+                }
+            }
+        }
+    }
+}
