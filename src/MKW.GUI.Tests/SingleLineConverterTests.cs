f4d83940bdd47d8fe923337c4c13a86fe8ea61af
Timofei Zhakov <tima@chemodax.net>
2025-10-13 16:15:29 +0000

    GUI: Test SingleLineConverter.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@5617 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.GUI.Tests/SingleLineConverterTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework.Legacy;
+
+namespace MKW.GUI.Tests
+{
+    public class SingleLineConverterTests
+    {
+        [Test]
+        [TestCase("abc", "abc")]
+        [TestCase("abc 123", "abc 123")]
+        [TestCase("abc   123", "abc 123")]
+        [TestCase("abc  \n\n 123", "abc 123")]
+        [TestCase("abc  \n\r\n 123", "abc 123")]
+        public void SimpleTest(string value, string expected)
+        {
+            ClassicAssert.AreEqual(expected, SingleLineConverter.CompactText(value));
+        }
+    }
+}
