888ef158f6d2c025c5f65304d2f0df9cd1aa3047
Timofei Zhakov <tima@chemodax.net>
2025-10-14 18:37:16 +0000

    Crypto: Factor out SymmetricCipher.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@5636 0083e6b4-6090-234a-836e-88fcc467669c

--- src/MKW.Cryptography.BouncyCastle/AesGcmSymmetricTransformer.cs
+++ src/MKW.Cryptography.BouncyCastle/AesGcmSymmetricTransformer.cs
@@ -1,8 +1,5 @@
-﻿using MKW.Common;
-using MKW.Cryptography.Exceptions;
-using Org.BouncyCastle.Crypto;
+﻿using Org.BouncyCastle.Crypto;
 using Org.BouncyCastle.Crypto.Engines;
-using Org.BouncyCastle.Crypto.IO;
 using Org.BouncyCastle.Crypto.Modes;
 using Org.BouncyCastle.Crypto.Parameters;
 
@@ -11,69 +8,31 @@ namespace MKW.Cryptography.BouncyCastle
     internal sealed class AesGcmSymmetricTransformer : ISymmetricTransformer, IDisposable
     {
         private readonly SymmetricKey key;
-
-        private readonly IBlockCipher blockCipher;
-        private readonly IAeadBlockCipher blockCipherMode;
-
-        private readonly IBufferedCipher cipher;
-
-        private readonly ICipherParameters parameters;
+        private readonly SymmetricCipher cipher;
 
         public AesGcmSymmetricTransformer(SymmetricKey key)
         {
             this.key = key;
 
-            blockCipher = new AesEngine();
-            blockCipherMode = new GcmBlockCipher(blockCipher);
+            AesEngine blockCipher = new AesEngine();
+            GcmBlockCipher blockCipherMode = new GcmBlockCipher(blockCipher);
 
-            cipher = new BufferedAeadBlockCipher(blockCipherMode);
+            BufferedAeadBlockCipher cipher = new BufferedAeadBlockCipher(blockCipherMode);
 
             KeyParameter aesKey = new KeyParameter(key.KeyBytes.ToArray());
-            parameters = new AeadParameters(aesKey, key.IVBytes.Length * 8, key.IVBytes.ToArray());
+            AeadParameters parameters = new AeadParameters(aesKey, key.IVBytes.Length * 8, key.IVBytes.ToArray());
+
+            this.cipher = new SymmetricCipher(cipher, parameters);
         }
 
         public Memory<byte> Decrypt(ReadOnlySpan<byte> data)
         {
-            try
-            {
-                cipher.Init(false, parameters);
-
-                using MemoryStream output = new MemoryStream();
-
-                using (CipherStream cipherStream = new CipherStream(new StreamDisown(output),
-                                                                    null, cipher))
-                {
-                    cipherStream.Write(data);
-                }
-
-                return output.ToArray();
-            }
-            catch (CryptoException ex)
-            {
-                throw new SymmetricOperationFailedException(ex);
-            }
+            return cipher.Decrypt(data);
         }
 
         public Memory<byte> Encrypt(ReadOnlySpan<byte> data)
         {
-            try
-            {
-                cipher.Init(true, parameters);
-
-                using MemoryStream output = new MemoryStream();
-
-                using (CipherStream cipherStream = new CipherStream(new StreamDisown(output),
-                                                                    null, cipher))
-                {
-                    cipherStream.Write(data);
-                }
-
-                return output.ToArray();
-            }
-            catch (CryptoException ex)
-            {
-                throw new SymmetricOperationFailedException(ex);
-            }
+            return cipher.Encrypt(data);
         }
 
         public Memory<byte> ExportIV()
---
+++ src/MKW.Cryptography.BouncyCastle/SymmetricCipher.cs
@@ -0,0 +1,63 @@
+﻿using MKW.Common;
+using MKW.Cryptography.Exceptions;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.IO;
+
+namespace MKW.Cryptography.BouncyCastle
+{
+    internal sealed class SymmetricCipher
+    {
+        private readonly IBufferedCipher cipher;
+        private readonly ICipherParameters parameters;
+
+        public SymmetricCipher(IBufferedCipher cipher, ICipherParameters parameters)
+        {
+            this.cipher = cipher;
+            this.parameters = parameters;
+        }
+
+        public Memory<byte> Decrypt(ReadOnlySpan<byte> data)
+        {
+            try
+            {
+                cipher.Init(false, parameters);
+
+                using MemoryStream output = new MemoryStream();
+
+                using (CipherStream cipherStream = new CipherStream(new StreamDisown(output),
+                                                                    null, cipher))
+                {
+                    cipherStream.Write(data);
+                }
+
+                return output.ToArray();
+            }
+            catch (CryptoException ex)
+            {
+                throw new SymmetricOperationFailedException(ex);
+            }
+        }
+
+        public Memory<byte> Encrypt(ReadOnlySpan<byte> data)
+        {
+            try
+            {
+                cipher.Init(true, parameters);
+
+                using MemoryStream output = new MemoryStream();
+
+                using (CipherStream cipherStream = new CipherStream(new StreamDisown(output),
+                                                                    null, cipher))
+                {
+                    cipherStream.Write(data);
+                }
+
+                return output.ToArray();
+            }
+            catch (CryptoException ex)
+            {
+                throw new SymmetricOperationFailedException(ex);
+            }
+        }
+    }
+}
