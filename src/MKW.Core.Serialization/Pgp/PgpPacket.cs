8d89630af6911bf0a885442bd027f22c919ea57f
Timofei Zhakov <tima@chemodax.net>
2025-10-15 17:24:39 +0000

    Make PgpPacket.Tag an abstract property instead of initializing it through a constructor.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@5645 0083e6b4-6090-234a-836e-88fcc467669c

--- src/MKW.Core.Serialization/Pgp/PgpPacket.cs
+++ src/MKW.Core.Serialization/Pgp/PgpPacket.cs
@@ -4,11 +4,6 @@ namespace MKW.Core.Serialization.Pgp
 {
     public abstract class PgpPacket : PgpObject
     {
-        public PacketTag Tag { get; }
-
-        public PgpPacket(PacketTag packetTag)
-        {
-            Tag = packetTag;
-        }
+        public abstract PacketTag Tag { get; }
     }
 }
