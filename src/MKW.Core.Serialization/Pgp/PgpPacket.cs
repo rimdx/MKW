d8bed1e1af26fc8012c2359f60e866ffdc888a73
Timofei Zhakov <tima@chemodax.net>
2025-10-15 17:18:39 +0000

    Add PgpPacket entity.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@5642 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.Core.Serialization/Pgp/PgpPacket.cs
@@ -0,0 +1,14 @@
+﻿using Org.BouncyCastle.Bcpg;
+
+namespace MKW.Core.Serialization.Pgp
+{
+    public abstract class PgpPacket : PgpObject
+    {
+        public PacketTag Tag { get; }
+
+        public PgpPacket(PacketTag packetTag)
+        {
+            Tag = packetTag;
+        }
+    }
+}
