3969af72391a9f272c61950b19c6bff5b520f318
Timofei Zhakov <tima@chemodax.net>
2025-10-15 17:27:33 +0000

    PgpPacket's aren't actually PgpObject's.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@5647 0083e6b4-6090-234a-836e-88fcc467669c

--- src/MKW.Core.Serialization/Pgp/PgpPacket.cs
+++ src/MKW.Core.Serialization/Pgp/PgpPacket.cs
@@ -2,8 +2,10 @@
 
 namespace MKW.Core.Serialization.Pgp
 {
-    public abstract class PgpPacket : PgpObject
+    public abstract class PgpPacket
     {
         public abstract PacketTag Tag { get; }
+
+        public abstract void Encode(PgpOutputStream stream);
     }
 }
