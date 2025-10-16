138e5bcdaa895180dadb975eb3d415a545fcd18b
Timofei Zhakov <tima@chemodax.net>
2025-10-16 07:32:11 +0000

    Implement parser for S2K structure.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@5653 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.Core.Serialization/Pgp/Packets/StringToKeyTag.cs
@@ -0,0 +1,9 @@
+﻿namespace MKW.Core.Serialization.Pgp.Packets
+{
+    public enum StringToKeyTag : byte
+    {
+        Simple = 0,
+        Salted = 1,
+        IteratedSalted = 3,
+    }
+}
---
+++ src/MKW.Core.Serialization/Pgp/Packets/SymEncryptedSessionKeyV4.cs
@@ -0,0 +1,41 @@
+﻿using MKW.Common;
+using MKW.Core.Serialization.Pgp.Primitives;
+using Org.BouncyCastle.Bcpg;
+
+namespace MKW.Core.Serialization.Pgp.Packets
+{
+    // The body of this packet consists of:
+    // 
+    // - A one-octet version number.  The only currently defined version
+    //   is 4.
+    // 
+    // - A one-octet number describing the symmetric algorithm used.
+    // 
+    // - A string-to-key (S2K) specifier, length as defined above.
+    // 
+    // - Optionally, the encrypted session key itself, which is decrypted
+    //   with the string-to-key object.
+    public sealed class SymEncryptedSessionKeyV4 : PgpPacket
+    {
+        private static readonly PgpVersion version = new PgpVersion(4);
+
+        public override PacketTag Tag => PacketTag.SymmetricKeyEncryptedSessionKey;
+
+        public SymmetricKeyAlgorithmTag SymmetricAlgorithmTag { get; }
+        public StringToKey StringToKey { get; }
+
+        public SymEncryptedSessionKeyV4(PgpInputStream stream)
+        {
+            version.ConsumeVersion(stream);
+            SymmetricAlgorithmTag = (SymmetricKeyAlgorithmTag)stream.RequireByte();
+            StringToKey = new StringToKey(stream);
+        }
+
+        public override void Encode(PgpOutputStream stream)
+        {
+            version.Encode(stream);
+            stream.WriteByte((byte)SymmetricAlgorithmTag);
+            StringToKey.Encode(stream);
+        }
+    }
+}
