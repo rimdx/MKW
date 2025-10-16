3d565dd308c6b54ecaf1f7e3a28f61f52cb131b3
Timofei Zhakov <tima@chemodax.net>
2025-10-16 15:40:47 +0000

    EntrySerializer: Implement deserialization.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@5661 0083e6b4-6090-234a-836e-88fcc467669c

--- src/MKW.Storage.MKGP/EntrySerializer.cs
+++ src/MKW.Storage.MKGP/EntrySerializer.cs
@@ -1,5 +1,7 @@
-﻿using MKW.Core.Serialization.Pgp;
+﻿using MKW.Core;
+using MKW.Core.Serialization.Pgp;
 using MKW.Core.Serialization.Pgp.Packets;
+using Org.BouncyCastle.Bcpg;
 using System.Buffers;
 
 namespace MKW.Storage.MKGP
@@ -8,13 +10,67 @@ namespace MKW.Storage.MKGP
     {
         public static DatabaseEntry Deserialize(ArrayBufferReader reader)
         {
-            throw new NotImplementedException();
+            Dictionary<UserId, ReadOnlyMemory<byte>> users = [];
+            ReadOnlyMemory<byte>? encryptedData = null;
+
+            while (reader.RemainingBytes > 0)
+            {
+                PgpPacket packet = PgpPacketSerializer.ReadPacket(reader);
+                ArrayBufferReader subreader = new ArrayBufferReader(packet.EncodedBody);
+
+                if (packet.Tag == PacketTag.PublicKeyEncryptedSession)
+                {
+                    PublicKeyEncryptedSessionKeyV3 sessionKey = PublicKeyEncryptedSessionKeyV3Serializer.Deserialize(subreader);
+
+                    Guid id = new Guid([
+                        ..new byte[8],
+                        ..sessionKey.KeyId.Span,
+                    ]);
+
+                    users.Add(UserId.FromGuid(id), sessionKey.Data);
+                }
+                else if (packet.Tag == PacketTag.SymmetricEncryptedIntegrityProtected)
+                {
+                    SymEncryptedProtectedDataV1 data = SymEncryptedProtectedDataV1Serializer.Deserialize(subreader);
+
+                    encryptedData = data.Data;
+                }
+                else
+                {
+                    throw new NotSupportedException();
+                }
+            }
+
+            if (encryptedData == null)
+            {
+                throw new Exception("Missing data packet.");
+            }
+
+            return new DatabaseEntry
+            {
+                Id = EntryId.Create(),
+                Data = encryptedData.Value,
+                Salt = null,
+                Keys = users,
+            };
         }
 
         public static void Serialize(IBufferWriter<byte> writer,
                                      DatabaseEntry obj)
         {
             //AsymmetricKeyParameter pubkey = PublicKeyFactory.CreateKey(entry.Data);
+
+            foreach (KeyValuePair<UserId, ReadOnlyMemory<byte>> user in obj.Keys)
+            {
+                PublicKeyEncryptedSessionKeyV3 sessionKey = new PublicKeyEncryptedSessionKeyV3
+                {
+                    KeyId = user.Key.GetGuid().ToByteArray().AsMemory().Slice(0, 8),
+                    Tag = PublicKeyAlgorithmTag.RsaGeneral,
+                    Data = user.Value,
+                };
+
+                PublicKeyEncryptedSessionKeyV3Serializer.SerializePacket(writer, sessionKey, true);
+            }
 
             SymEncryptedProtectedDataV1 packet = new SymEncryptedProtectedDataV1
             {
--- src/MKW.Storage.Tests/MKGPLowLevelTests.cs
+++ src/MKW.Storage.Tests/MKGPLowLevelTests.cs
@@ -1,4 +1,5 @@
 ﻿using MKW.Core;
+using MKW.Core.Serialization.Pgp;
 using MKW.Cryptography;
 using MKW.Storage.MKGP;
 using Org.BouncyCastle.Bcpg.OpenPgp;
@@ -29,25 +30,23 @@ namespace MKW.Storage.Tests
         [Test]
         public void SimpleEntryDecode()
         {
-            using MemoryStream stream = new MemoryStream(
-                Convert.FromBase64String(
-                    "hQIMA87HrhcaeKn3AQ//TtsqaTQPGbNTPPuH0832+xVobQ8HYMSJeo3jXRh8Umjo49ZE85tmG93x" +
-                    "jnGrGzIygEQqUIF543hbxT1G4mXRU1gLmx0v4d0B5pH/3azKecB6PiRo4CBLRGJLhSQbRA45/9+C" +
-                    "+bjzE+v4NZ7qrXAN/PsRR+rkK+SUKqQxI3fifXI61/xiY7OkdrMb6xSnAnBNhs/U1+Iqxl6+nwjl" +
-                    "Dok0UXVS6meeWPS7FwS5i2LQkzCEuPcEPZxiH80uxuXGYKjYQnDzydniypukSs1P2ZXSJW59oL9W" +
-                    "jCYjjnLRL3zuL8mMa9wn+iWF7qSE7Wk9q8L84FV47etUbQw+1m5iVWVWg6QRLOaUE6Q5ja4GIK1S" +
-                    "fx5MLGokd82QDzFjvfu33m4GDoa+eq1T3OIe1izLsvtR6guuTruq1h/5GfkMei3O7iADQ/ZvU5Ww" +
-                    "CHqFM8I6SpCbsgoOgF2YOCMBaDKx0w6vyTzIZ/EFk3zMLi6aZvi1AxcrxzHxcgTcYjbuwAMB8gRb" +
-                    "mzb87rRt7l0PsXOn10m/iQYSi3HrHKulRRhNQVFOWAW51tXR2RcmtqBMwh1D1HeX5ox88LlNUQO3" +
-                    "Hw6+KqAAiayEcBX8kVAKu88JSeLTmDi22JxucqQ9iJAruzeNtGHcKYnn9X0K36UdiEQA+ipsgi4h" +
-                    "7gG2gFwfczFUgS9Mx37SdwEo2P/dRNKYAlOCKMFO71/y9LGCqS4Ep5t2HuB2SqZi6GAliadRxi9V" +
-                    "+6mamRhDv9PcDRc/NH2V6M4ip/m7eqpTwxGVrSotn4fI/ouVGgcNO447UpbHaiav7D1CETP5vykm" +
-                    "JoPWR/eBc2sju6nuENtcYFc+icy7"
-            ));
+            byte[] data = Convert.FromBase64String(
+                "hQIMA87HrhcaeKn3AQ//TtsqaTQPGbNTPPuH0832+xVobQ8HYMSJeo3jXRh8Umjo49ZE85tmG93x" +
+                "jnGrGzIygEQqUIF543hbxT1G4mXRU1gLmx0v4d0B5pH/3azKecB6PiRo4CBLRGJLhSQbRA45/9+C" +
+                "+bjzE+v4NZ7qrXAN/PsRR+rkK+SUKqQxI3fifXI61/xiY7OkdrMb6xSnAnBNhs/U1+Iqxl6+nwjl" +
+                "Dok0UXVS6meeWPS7FwS5i2LQkzCEuPcEPZxiH80uxuXGYKjYQnDzydniypukSs1P2ZXSJW59oL9W" +
+                "jCYjjnLRL3zuL8mMa9wn+iWF7qSE7Wk9q8L84FV47etUbQw+1m5iVWVWg6QRLOaUE6Q5ja4GIK1S" +
+                "fx5MLGokd82QDzFjvfu33m4GDoa+eq1T3OIe1izLsvtR6guuTruq1h/5GfkMei3O7iADQ/ZvU5Ww" +
+                "CHqFM8I6SpCbsgoOgF2YOCMBaDKx0w6vyTzIZ/EFk3zMLi6aZvi1AxcrxzHxcgTcYjbuwAMB8gRb" +
+                "mzb87rRt7l0PsXOn10m/iQYSi3HrHKulRRhNQVFOWAW51tXR2RcmtqBMwh1D1HeX5ox88LlNUQO3" +
+                "Hw6+KqAAiayEcBX8kVAKu88JSeLTmDi22JxucqQ9iJAruzeNtGHcKYnn9X0K36UdiEQA+ipsgi4h" +
+                "7gG2gFwfczFUgS9Mx37SdwEo2P/dRNKYAlOCKMFO71/y9LGCqS4Ep5t2HuB2SqZi6GAliadRxi9V" +
+                "+6mamRhDv9PcDRc/NH2V6M4ip/m7eqpTwxGVrSotn4fI/ouVGgcNO447UpbHaiav7D1CETP5vykm" +
+                "JoPWR/eBc2sju6nuENtcYFc+icy7");
 
-            PgpObjectFactory bcpg = new PgpObjectFactory(stream);
+            ArrayBufferReader reader = new ArrayBufferReader(data);
 
-            PgpEncryptedDataList obj = (PgpEncryptedDataList)bcpg.NextPgpObject();
+            DatabaseEntry entry = EntrySerializer.Deserialize(reader);
         }
     }
 }
