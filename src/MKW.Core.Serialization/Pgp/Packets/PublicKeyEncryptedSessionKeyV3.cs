069b90768c0aee38c8404dccac4c930e0e2b41f8
Timofei Zhakov <tima@chemodax.net>
2025-10-16 14:35:41 +0000

    Add serializer for `Public-Key Encrypted Session Key Packets (Tag 1)` packet.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@5655 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.Core.Serialization/Pgp/Packets/PublicKeyEncryptedSessionKeyV3.cs
@@ -0,0 +1,11 @@
+﻿using Org.BouncyCastle.Bcpg;
+
+namespace MKW.Core.Serialization.Pgp.Packets
+{
+    public sealed record class PublicKeyEncryptedSessionKeyV3
+    {
+        public required ReadOnlyMemory<byte> KeyId { get; init; }
+        public required PublicKeyAlgorithmTag Tag { get; init; }
+        public required ReadOnlyMemory<byte> Data { get; init; }
+    }
+}
