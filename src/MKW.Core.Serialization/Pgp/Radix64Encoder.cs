cfa1d620463e0d853771b2af10891461dab5c652
Timofei Zhakov <tima@chemodax.net>
2025-10-17 07:43:56 +0000

    Add simple Radix64 encoder.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@5674 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.Core.Serialization/Pgp/Radix64Encoder.cs
@@ -0,0 +1,73 @@
+﻿using Org.BouncyCastle.Bcpg;
+using System.Buffers.Binary;
+using System.Collections;
+using System.Security.Cryptography;
+
+namespace MKW.Core.Serialization.Pgp
+{
+    public sealed class Radix64Encoder : ICryptoTransform
+    {
+        public bool CanReuseTransform => false;
+        public bool CanTransformMultipleBlocks => false;
+
+        public int InputBlockSize => 3;
+        public int OutputBlockSize => 4;
+
+        private readonly Crc24 crc;
+        private readonly BitArray bits;
+
+        public Radix64Encoder()
+        {
+            crc = new Crc24();
+            bits = new BitArray(24);
+        }
+
+        public int TransformBlock(byte[] inputBuffer,
+                                  int inputOffset,
+                                  int inputCount,
+                                  byte[] outputBuffer,
+                                  int outputOffset)
+        {
+            ReadOnlySpan<byte> inputSpan = new ReadOnlySpan<byte>(inputBuffer, inputOffset, inputCount);
+            Span<byte> outputSpan = new Span<byte>(outputBuffer, outputOffset, OutputBlockSize);
+
+            crc.Update3(outputBuffer, outputOffset);
+
+            Radix64BitConvert.BufferToBits(inputSpan, bits);
+            Radix64BitConvert.EncodeChunk(bits, inputSpan.Length * 8, outputSpan);
+
+            return outputBuffer.Length;
+        }
+
+        public byte[] TransformFinalBlock(byte[] inputBuffer,
+                                          int inputOffset,
+                                          int inputCount)
+        {
+            ReadOnlySpan<byte> inputSpan = new ReadOnlySpan<byte>(inputBuffer, inputOffset, inputCount);
+
+            byte[] outputBuffer = new byte[OutputBlockSize * 2 + 1];
+            Span<byte> outputSpan = new Span<byte>(outputBuffer);
+
+            Radix64BitConvert.BufferToBits(inputSpan, bits);
+            int count = Radix64BitConvert.EncodeChunk(bits, inputSpan.Length * 8, outputSpan);
+
+            Radix64BitConvert.WritePadding(outputSpan.Slice(count));
+
+            foreach (byte b in inputSpan)
+            {
+                crc.Update(b);
+            }
+
+            Span<byte> crcBytes = stackalloc byte[4];
+            BinaryPrimitives.WriteUInt32BigEndian(crcBytes, (uint)crc.Value);
+            Radix64BitConvert.BufferToBits(crcBytes.Slice(1, 3), bits);
+            Radix64BitConvert.EncodeChunk(bits, inputSpan.Length * 8, outputSpan.Slice(OutputBlockSize + 1));
+
+            return outputBuffer;
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
