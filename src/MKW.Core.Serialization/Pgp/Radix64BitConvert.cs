70822d9502593784edd226d2b51f129bd82e697d
Timofei Zhakov <tima@chemodax.net>
2025-10-17 17:06:13 +0000

    Factor out Radix64Charset.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@5679 0083e6b4-6090-234a-836e-88fcc467669c

--- src/MKW.Core.Serialization/Pgp/Radix64BitConvert.cs
+++ src/MKW.Core.Serialization/Pgp/Radix64BitConvert.cs
@@ -10,52 +10,6 @@ namespace MKW.Core.Serialization.Pgp
         private const int numberWidth = 6;
         private const int byteWidth = 8;
 
-        private const byte padding = (byte)'=';
-
-        private static readonly byte[] encodingTable =
-        [
-            (byte)'A', (byte)'B', (byte)'C', (byte)'D', (byte)'E', (byte)'F', (byte)'G',
-            (byte)'H', (byte)'I', (byte)'J', (byte)'K', (byte)'L', (byte)'M', (byte)'N',
-            (byte)'O', (byte)'P', (byte)'Q', (byte)'R', (byte)'S', (byte)'T', (byte)'U',
-            (byte)'V', (byte)'W', (byte)'X', (byte)'Y', (byte)'Z',
-            (byte)'a', (byte)'b', (byte)'c', (byte)'d', (byte)'e', (byte)'f', (byte)'g',
-            (byte)'h', (byte)'i', (byte)'j', (byte)'k', (byte)'l', (byte)'m', (byte)'n',
-            (byte)'o', (byte)'p', (byte)'q', (byte)'r', (byte)'s', (byte)'t', (byte)'u',
-            (byte)'v',
-            (byte)'w', (byte)'x', (byte)'y', (byte)'z',
-            (byte)'0', (byte)'1', (byte)'2', (byte)'3', (byte)'4', (byte)'5', (byte)'6',
-            (byte)'7', (byte)'8', (byte)'9',
-            (byte)'+', (byte)'/'
-        ];
-
-        private readonly static ReadOnlyMemory<byte> decodingTable = CreateDecodingTable();
-
-        private static ReadOnlyMemory<byte> CreateDecodingTable()
-        {
-            byte[] result = new byte[128];
-            result.AsSpan().Fill(0xFF);
-
-            for (int i = 'A'; i <= 'Z'; i++)
-            {
-                result[i] = (byte)(i - 'A');
-            }
-
-            for (int i = 'a'; i <= 'z'; i++)
-            {
-                result[i] = (byte)(i - 'a' + 26);
-            }
-
-            for (int i = '0'; i <= '9'; i++)
-            {
-                result[i] = (byte)(i - '0' + 52);
-            }
-
-            result['+'] = 62;
-            result['/'] = 63;
-
-            return result;
-        }
-
         public static void BufferFromBits(BitArray input, Span<byte> output)
         {
             for (int i = 0; i < input.Length / 8; i++)
@@ -79,10 +33,10 @@ namespace MKW.Core.Serialization.Pgp
         // encoded.count = 4
         public static void EncodeFullBlock(ReadOnlySpan<byte> bytes, Span<byte> encoded)
         {
-            encoded[0] = encodingTable[0x3f & (bytes[0] >> 2)];
-            encoded[1] = encodingTable[0x3f & (bytes[0] << 4) | (bytes[1] >> 4)];
-            encoded[2] = encodingTable[0x3f & (bytes[1] << 2) | (bytes[2] >> 6)];
-            encoded[3] = encodingTable[0x3f & (bytes[2])];
+            encoded[0] = Radix64Charset.EncodingTable[0x3f & (bytes[0] >> 2)];
+            encoded[1] = Radix64Charset.EncodingTable[0x3f & (bytes[0] << 4) | (bytes[1] >> 4)];
+            encoded[2] = Radix64Charset.EncodingTable[0x3f & (bytes[1] << 2) | (bytes[2] >> 6)];
+            encoded[3] = Radix64Charset.EncodingTable[0x3f & (bytes[2])];
         }
 
         // bytes.count ~ (0, 3)
@@ -95,17 +49,17 @@ namespace MKW.Core.Serialization.Pgp
             }
             else if (bytes.Length == 1)
             {
-                encoded[0] = encodingTable[0x3f & (bytes[0] >> 2)];
-                encoded[1] = encodingTable[0x3f & (bytes[0] << 4)];
-                encoded[2] = padding;
-                encoded[3] = padding;
+                encoded[0] = Radix64Charset.EncodingTable[0x3f & (bytes[0] >> 2)];
+                encoded[1] = Radix64Charset.EncodingTable[0x3f & (bytes[0] << 4)];
+                encoded[2] = Radix64Charset.Padding;
+                encoded[3] = Radix64Charset.Padding;
             }
             else if (bytes.Length == 2)
             {
-                encoded[0] = encodingTable[0x3f & (bytes[0] >> 2)];
-                encoded[1] = encodingTable[0x3f & (bytes[0] << 4) | (bytes[1] >> 4)];
-                encoded[2] = encodingTable[0x3f & (bytes[1] << 2)];
-                encoded[3] = padding;
+                encoded[0] = Radix64Charset.EncodingTable[0x3f & (bytes[0] >> 2)];
+                encoded[1] = Radix64Charset.EncodingTable[0x3f & (bytes[0] << 4) | (bytes[1] >> 4)];
+                encoded[2] = Radix64Charset.EncodingTable[0x3f & (bytes[1] << 2)];
+                encoded[3] = Radix64Charset.Padding;
             }
             else if (encoded.Length == 3)
             {
@@ -119,7 +73,7 @@ namespace MKW.Core.Serialization.Pgp
 
             for (; i < inputSpan.Length; i++)
             {
-                byte value = decodingTable.Span[inputSpan[i]];
+                byte value = Radix64Charset.DecodingTable.Span[inputSpan[i]];
 
                 for (int j = 0; j < numberWidth; j++)
                 {
