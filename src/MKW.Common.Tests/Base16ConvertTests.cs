e774715d00e14dad652e1c76fe9b4cd426896aea
Timofei Zhakov <tima@chemodax.net>
2025-10-18 10:23:56 +0000

    Implement Base16Convert.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@5716 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.Common.Tests/Base16ConvertTests.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Timofei Zhakov. All Rights Reserved
+// Licensed under the Apache License, Version 2.0.
+
+using NUnit.Framework.Legacy;
+
+namespace MKW.Common.Tests
+{
+    public class Base16ConvertTests
+    {
+        [Test]
+        public void SimpleTest()
+        {
+            byte[] buf = [1, 2, 3];
+            string str = Base16Convert.GetString(buf);
+
+            Console.WriteLine(str);
+
+            byte[] buf2 = Base16Convert.GetBytes(str);
+            CollectionAssert.AreEqual(buf, buf2);
+        }
+
+        [Test]
+        public void RejectStringWithOddLength()
+        {
+            Assert.Throws<ArgumentException>(() => Base16Convert.GetBytes("BEFFA"));
+        }
+    }
+}
