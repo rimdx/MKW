01769f473876e95518c80d802f537a2c63999891
Timofei Zhakov <tima@chemodax.net>
2025-10-18 07:20:11 +0000

    Move reading from a span into Crc24 checksum into Crc24Extensions.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@5695 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.Core.Serialization/Pgp/Crc24Extensions.cs
@@ -0,0 +1,26 @@
+﻿using Org.BouncyCastle.Bcpg;
+
+namespace MKW.Core.Serialization.Pgp
+{
+    public static class Crc24Extensions
+    {
+        public static void Update(this Crc24 crc, ReadOnlySpan<byte> data)
+        {
+            byte[] buf = new byte[3];
+
+            int i = 0;
+            for (; i + 3 < data.Length; i += 3)
+            {
+                buf[0] = data[i + 0];
+                buf[1] = data[i + 1];
+                buf[2] = data[i + 2];
+                crc.Update3(buf, 0);
+            }
+
+            for (; i < data.Length; i++)
+            {
+                crc.Update(data[i]);
+            }
+        }
+    }
+}
