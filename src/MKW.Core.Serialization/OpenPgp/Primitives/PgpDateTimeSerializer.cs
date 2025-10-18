ef6e71332684623d478dc66ebc08364276a12319
Timofei Zhakov <tima@chemodax.net>
2025-10-18 14:57:38 +0000

    Implement PgpDateTimeSerializer.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@5722 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.Core.Serialization/OpenPgp/Primitives/PgpDateTimeSerializer.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) Timofei Zhakov. All Rights Reserved
+// Licensed under the Apache License, Version 2.0.
+
+using MKW.Common;
+using System.Buffers;
+using System.Buffers.Binary;
+
+namespace MKW.Core.Serialization.OpenPgp.Primitives
+{
+    public static class PgpDateTimeSerializer
+    {
+        private static readonly DateTime origin = new DateTime(
+            1970 /* year */,
+            1 /* month */,
+            1 /* day */,
+            0 /* hour */,
+            0 /* minute */,
+            0 /* second */,
+            0 /* millisecond */,
+            DateTimeKind.Utc
+        );
+
+        public static void Serialize(IBufferWriter<byte> writer,
+                                     DateTime dateTime)
+        {
+            TimeSpan difference = dateTime - origin;
+            uint unixTime = ((uint)difference.TotalSeconds) & 0xFFFF_FFFF;
+
+            BinaryPrimitives.WriteUInt32BigEndian(writer.GetSpan(4), unixTime);
+        }
+
+        public static DateTime Deserialize(ArrayBufferReader reader)
+        {
+            int unixTime = BinaryPrimitives.ReadInt32BigEndian(reader.ReadBytes(4).Span);
+            return origin.AddSeconds(unixTime);
+        }
+    }
+}
