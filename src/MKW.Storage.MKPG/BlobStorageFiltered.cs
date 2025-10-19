a621106bf636bf653d6819bd0dfcc2ceaf6909e3
Timofei Zhakov <tima@chemodax.net>
2025-10-19 07:24:13 +0000

    Add BlobStorageFiltered.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@5749 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.Storage.MKPG/BlobStorageFiltered.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (c) Timofei Zhakov. All Rights Reserved
+// Licensed under the Apache License, Version 2.0.
+
+namespace MKW.Storage.MKPG
+{
+    internal sealed class BlobStorageFiltered : IBlobStorage
+    {
+        private readonly IBlobStorage proxy;
+        private readonly string type;
+
+        public BlobStorageFiltered(IBlobStorage proxy, string type)
+        {
+            this.proxy = proxy;
+            this.type = type;
+        }
+
+        public void Create(BlobEntry entry)
+        {
+            if (entry.Type != type)
+            {
+                throw new Exception("Attempted to created entry with wrong type.");
+            }
+
+            proxy.Create(entry);
+        }
+
+        public bool Delete(BlobId id)
+        {
+            // TODO: fix collision between different types with the same ids.
+            return proxy.Delete(id);
+        }
+
+        public IEnumerable<BlobEntry> Enumerate()
+        {
+            foreach (BlobEntry entry in proxy.Enumerate())
+            {
+                if (entry.Type == type)
+                {
+                    yield return entry;
+                }
+            }
+        }
+
+        public bool Exists(BlobId id)
+        {
+            // TODO: fix collision between different types with the same ids.
+            return proxy.Exists(id);
+        }
+
+        public BlobEntry Open(BlobId id)
+        {
+            // TODO: fix collision between different types with the same ids.
+            return proxy.Open(id);
+        }
+    }
+}
