192ade9baefbbdf6205e118ea78649e60a64b171
Timofei Zhakov <tima@chemodax.net>
2025-10-19 07:13:15 +0000

    Add MKPGConstants.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@5746 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.Storage.MKPG/MKPGConstants.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Timofei Zhakov. All Rights Reserved
+// Licensed under the Apache License, Version 2.0.
+
+namespace MKW.Storage.MKPG
+{
+    internal static class MKPGConstants
+    {
+        public static class ArmourHeaderNames
+        {
+            public const string Id = "MKWID";
+        }
+
+        public static class ArmourTypeHeaders
+        {
+            public const string Entry = "MKW ENTRY";
+            public const string User = "MKW USER";
+        }
+    }
+}
