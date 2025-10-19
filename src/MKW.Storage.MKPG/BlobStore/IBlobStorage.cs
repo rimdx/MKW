3cb69a4a5704c8edcaf60c20ed2639b881e00172
Timofei Zhakov <tima@chemodax.net>
2025-10-19 15:34:33 +0000

    Move IBlobStorage into BlobStore namespace.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@5798 0083e6b4-6090-234a-836e-88fcc467669c

--- src/MKW.Storage.MKPG/BlobStore/IBlobStorage.cs
+++ src/MKW.Storage.MKPG/BlobStore/IBlobStorage.cs
@@ -1,9 +1,7 @@
 ﻿// Copyright (c) Timofei Zhakov. All Rights Reserved
 // Licensed under the Apache License, Version 2.0.
 
-using MKW.Storage.MKPG.BlobStore;
-
-namespace MKW.Storage.MKPG
+namespace MKW.Storage.MKPG.BlobStore
 {
     internal interface IBlobStorage
     {
