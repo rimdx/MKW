7bed8f5cc5ed0a1a18059d17d250ff38d6203a19
Timofei Zhakov <tima@chemodax.net>
2025-10-19 07:32:40 +0000

    MKPGDatabaseTests: Add some tests on users and entries/users in the same database.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@5752 0083e6b4-6090-234a-836e-88fcc467669c

--- src/MKW.Storage.Tests/MKPGDatabaseTests.cs
+++ src/MKW.Storage.Tests/MKPGDatabaseTests.cs
@@ -2,6 +2,8 @@
 // Licensed under the Apache License, Version 2.0.
 
 using MKW.Core;
+using MKW.Cryptography;
+using MKW.Cryptography.Loader;
 using MKW.Storage.MKPG;
 using NUnit.Framework.Legacy;
 
@@ -43,6 +45,89 @@ namespace MKW.Storage.Tests
             DatabaseEntry[] entries = database.EnumerateEntries().ToArray();
 
             ClassicAssert.AreEqual(4, entries.Length);
+        }
+
+        [Test]
+        public void SimpleUserTest()
+        {
+            ICryptographyProvider crypto = BouncyCastleLoader.GetProvider();
+            IRandomGenerator random = crypto.CreateRandomGenerator();
+
+            AsymmetricPrivateKey keypair = crypto.CreateAsymmetricKey(CommonCryptographyAlgorithms.Rsa2048);
+            ReadOnlyMemory<byte> pubkey = crypto.EncodePkcsPublicKey(keypair.GetPublicKey());
+            ReadOnlyMemory<byte> fakeseckey = random.NextBytes(432);
+
+            DatabaseUser user = new DatabaseUser
+            {
+                Id = UserId.Create(),
+                Salt = random.NextBytes(8),
+                PrivateKey = new SecretPayload(fakeseckey),
+                PublicKey = new SignedPayload(pubkey, new byte[32]),
+                AdminSignature = null,
+                Metadata = null,
+            };
+
+            using MKPGDatabase database = new MKPGDatabase();
+
+            database.CreateUser(user.Id, user);
+            DatabaseUser decoded = database.OpenUser(user.Id);
+
+            CollectionAssert.AreEqual(user.Salt.ToArray(), decoded.Salt.ToArray());
+            CollectionAssert.AreEqual(user.PrivateKey.EncryptedPayload.ToArray(), decoded.PrivateKey.EncryptedPayload.ToArray());
+            CollectionAssert.AreEqual(user.PublicKey.Payload.ToArray(), decoded.PublicKey.Payload.ToArray());
+            CollectionAssert.AreEqual(user.PublicKey.Signature.ToArray(), decoded.PublicKey.Signature.ToArray());
+
+            database.CreateUser(UserId.Create(), user);
+            database.CreateUser(UserId.Create(), user);
+
+            DatabaseUser[] users = database.EnumerateUsers().ToArray();
+            ClassicAssert.AreEqual(3, users.Length);
+        }
+
+        [Test]
+        public void UsersAndEntriesSameFile()
+        {
+            ICryptographyProvider crypto = BouncyCastleLoader.GetProvider();
+            IRandomGenerator random = crypto.CreateRandomGenerator();
+
+            DatabaseEntry entry = new DatabaseEntry
+            {
+                Id = EntryId.Create(),
+                Data = random.NextBytes(42),
+                Salt = random.NextBytes(42),
+                Keys = new Dictionary<UserId, ReadOnlyMemory<byte>>(),
+            };
+
+            AsymmetricPrivateKey keypair = crypto.CreateAsymmetricKey(CommonCryptographyAlgorithms.Rsa2048);
+            ReadOnlyMemory<byte> pubkey = crypto.EncodePkcsPublicKey(keypair.GetPublicKey());
+            ReadOnlyMemory<byte> fakeseckey = random.NextBytes(432);
+
+            DatabaseUser user = new DatabaseUser
+            {
+                Id = UserId.Create(),
+                Salt = random.NextBytes(8),
+                PrivateKey = new SecretPayload(fakeseckey),
+                PublicKey = new SignedPayload(pubkey, new byte[32]),
+                AdminSignature = null,
+                Metadata = null,
+            };
+
+            using MKPGDatabase database = new MKPGDatabase();
+
+            database.CreateEntry(EntryId.Create(), entry);
+            database.CreateUser(UserId.Create(), user);
+
+            ClassicAssert.AreEqual(1, database.EnumerateEntries().Count());
+            ClassicAssert.AreEqual(1, database.EnumerateUsers().Count());
+
+            database.CreateEntry(EntryId.Create(), entry);
+            database.CreateEntry(EntryId.Create(), entry);
+            database.CreateEntry(EntryId.Create(), entry);
+            database.CreateUser(UserId.Create(), user);
+            database.CreateUser(UserId.Create(), user);
+
+            ClassicAssert.AreEqual(4, database.EnumerateEntries().Count());
+            ClassicAssert.AreEqual(3, database.EnumerateUsers().Count());
         }
     }
 }
