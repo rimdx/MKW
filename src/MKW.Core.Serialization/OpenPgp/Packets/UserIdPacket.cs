67123ef4254ce0c323698048930b27e375e3b17f
Timofei Zhakov <tima@chemodax.net>
2025-10-19 14:56:37 +0000

    Draft implementation for UserIdPacket.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@5789 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.Core.Serialization/OpenPgp/Packets/UserIdPacket.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Timofei Zhakov. All Rights Reserved
+// Licensed under the Apache License, Version 2.0.
+
+using System.Text;
+
+namespace MKW.Core.Serialization.OpenPgp.Packets
+{
+    public sealed record class UserIdPacket
+    {
+        public ReadOnlyMemory<byte> Content { get; }
+
+        public UserIdPacket(ReadOnlyMemory<byte> content)
+        {
+            Content = content;
+        }
+
+        public UserIdPacket(string content)
+        {
+            Content = Encoding.UTF8.GetBytes(content);
+        }
+
+        public string GetString()
+        {
+            return Encoding.UTF8.GetString(Content.ToArray());
+        }
+    }
+}
---
+++ src/MKW.Core.Serialization/OpenPgp/Packets/UserIdPacketSerializer.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Timofei Zhakov. All Rights Reserved
+// Licensed under the Apache License, Version 2.0.
+
+using MKW.Common;
+using Org.BouncyCastle.Bcpg;
+using System.Buffers;
+
+namespace MKW.Core.Serialization.OpenPgp.Packets
+{
+    public static class UserIdPacketSerializer
+    {
+        public const PacketTag Tag = PacketTag.UserId;
+
+        public static void Serialize(IBufferWriter<byte> writer,
+                                     UserIdPacket obj)
+        {
+            writer.Write(obj.Content.Span);
+        }
+
+        public static UserIdPacket Deserialize(IBufferReader<byte> reader)
+        {
+            return new UserIdPacket(reader.ReadAll());
+        }
+    }
+}
