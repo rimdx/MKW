0a1df8c08514c9e23a6990c5b568493a934b2b17
Timofei Zhakov <tima@chemodax.net>
2025-10-19 13:16:36 +0000

    Implement RfcUserId.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@5781 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.Core.Serialization/RfcUserIdMalformedException.cs
@@ -0,0 +1,7 @@
+﻿// Copyright (c) Timofei Zhakov. All Rights Reserved
+// Licensed under the Apache License, Version 2.0.
+
+namespace MKW.Core.Serialization
+{
+    public class RfcUserIdMalformedException() : Exception("Malformed User ID.");
+}
