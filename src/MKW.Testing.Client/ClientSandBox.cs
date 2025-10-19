cec2b604b3322e35d9c51c589bc974e9f5e325b9
Timofei Zhakov <tima@chemodax.net>
2025-10-19 15:17:02 +0000

    ClientSandBox: Add capability to test new MKPG database format.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@5793 0083e6b4-6090-234a-836e-88fcc467669c

--- src/MKW.Testing.Client/ClientSandBox.cs
+++ src/MKW.Testing.Client/ClientSandBox.cs
@@ -7,12 +7,15 @@ using MKW.Cryptography;
 using MKW.Cryptography.Loader;
 using MKW.Storage;
 using MKW.Storage.JSON;
+using MKW.Storage.MKPG;
 using MKW.Testing.Common;
 
 namespace MKW.Testing.Client
 {
     public class ClientSandBox : SandBoxBase
     {
+        private const bool USE_MKPGDatabase = false;
+
         public string AdminSecret => "adminsecret123";
 
         public ICryptographyProvider Crypto = CryptographyLoader.GetProvider();
@@ -27,7 +30,7 @@ namespace MKW.Testing.Client
                     UserId = "admin@contoso.com"
                 };
 
-                using JSONDatabaseSession db = JSONDatabaseSession.Create(DatabasePath);
+                using IDatabase db = CreateDatabase();
                 using ClientSession client = ClientSession.Create(db, Crypto, AdminSecret, metadata);
             }
         }
@@ -39,9 +42,28 @@ namespace MKW.Testing.Client
             return result;
         }
 
+        public IDatabase CreateDatabase()
+        {
+            if (USE_MKPGDatabase)
+            {
+                return MKPGDatabase.Create(DatabasePath);
+            }
+            else
+            {
+                return JSONDatabaseSession.Create(DatabasePath);
+            }
+        }
+
         public IDatabase OpenDatabase()
         {
-            return JSONDatabaseSession.Open(DatabasePath);
+            if (USE_MKPGDatabase)
+            {
+                return MKPGDatabase.Open(DatabasePath);
+            }
+            else
+            {
+                return JSONDatabaseSession.Open(DatabasePath);
+            }
         }
 
         public ClientSession OpenSession(IDatabase db)
