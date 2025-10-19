8a93977749e294de6c91fc0dd4f62eed375d6138
Timofei Zhakov <tima@chemodax.net>
2025-10-19 13:40:42 +0000

    Add some compatibility testing.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@5784 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.Storage.Tests/CompatTests.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Timofei Zhakov. All Rights Reserved
+// Licensed under the Apache License, Version 2.0.
+
+using MKW.Core;
+using MKW.Core.Client;
+using MKW.Cryptography.Loader;
+using MKW.Storage.JSON;
+using NUnit.Framework.Legacy;
+
+namespace MKW.Storage.Tests
+{
+    public class CompatTests
+    {
+        [Test]
+        [TestCase("test_json_database_v2.mkw")]
+        [TestCase("test_json_database_v2_formatted.mkw")]
+        public void LoadLockedJsonDatabase(string path)
+        {
+            using JSONDatabaseSession database = JSONDatabaseSession.Open(path);
+
+            ClassicAssert.AreEqual(2, database.EnumerateEntries().Count());
+            ClassicAssert.AreEqual(2, database.EnumerateUsers().Count());
+
+            using ClientSession client = ClientSession.Open(database, BouncyCastleLoader.GetProvider());
+
+            using IAdminSession admin = client.OpenAdmin("123");
+
+            ClassicAssert.NotNull(admin.EnumerateEntries().ToArray()[0].OpenPayload());
+        }
+    }
+}
