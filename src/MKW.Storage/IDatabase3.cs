47f60b9c0e96d79c38cdd0fdb460188e6a4b703e
Timofei Zhakov <tima@chemodax.net>
2025-10-22 19:59:46 +0000

    Draft IDatabase3 API which supports transactions.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@5831 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.Storage/IDatabase3.cs
@@ -0,0 +1,53 @@
+﻿// Copyright (c) Timofei Zhakov. All Rights Reserved
+// Licensed under the Apache License, Version 2.0.
+
+using MKW.Core;
+
+namespace MKW.Storage
+{
+    public interface IDatabase3
+    {
+        public interface ISerializer
+        {
+            ReadOnlyMemory<byte> SerializeProtectedData(DatabaseUserProtectedData obj);
+        }
+
+        public interface ISnapshot
+            : ISerializer
+            , IDisposable
+        {
+            DatabaseEntry OpenEntry();
+            bool HasUser(UserId id);
+            IEnumerable<DatabaseUser> EnumerateUsers();
+
+            DatabaseEntry OpenEntry(EntryId id);
+            bool HasEntry(EntryId id);
+            IEnumerable<DatabaseEntry> EnumerateEntries();
+        }
+
+        public interface ITransaction
+            : ISnapshot
+            , ISerializer
+            , IDisposable
+        {
+            void CreateUser(UserId id, DatabaseUser user);
+            void UpdateUser(UserId id, DatabaseUser user);
+            bool DeleteUser(UserId id);
+
+            void CreateEntry(EntryId id, DatabaseEntry entry);
+            void UpdateEntry(EntryId id, DatabaseEntry entry);
+            bool DeleteEntry(EntryId id);
+
+            void Commit();
+        }
+
+        ITransaction BeginTransaction();
+
+        ISnapshot CreateSnapshot();
+
+        DatabaseConfiguration GetConfiguration();
+
+        Task<bool> WaitForDatabaseChangesAsync(CancellationToken cancellationToken);
+        void ReloadDatabaseFile();
+    }
+}
