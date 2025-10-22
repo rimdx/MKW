1982fb687883fd3b7471cc98847c195ca800cc40
Timofei Zhakov <tima@chemodax.net>
2025-10-22 06:39:54 +0000

    BlobEntry: Remove abstract modifier.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@5819 0083e6b4-6090-234a-836e-88fcc467669c

--- src/MKW.Storage/BlobEntry.cs
+++ src/MKW.Storage/BlobEntry.cs
@@ -5,7 +5,7 @@ using MKW.Common;
 
 namespace MKW.Storage
 {
-    public abstract record class BlobEntry
+    public record class BlobEntry
     {
         public required BlobId Id { get; init; }
         public required ReadOnlyMemory<byte> Data { get; init; }
