4faaccef2d83ae1b378abdc41b728cd3c85ee3e3
Timofei Zhakov <tima@chemodax.net>
2025-10-23 19:53:56 +0000

    Move IDatabaseSerializer away from IDatabase3.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@5838 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.Storage/IDatabaseSerializer.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Timofei Zhakov. All Rights Reserved
+// Licensed under the Apache License, Version 2.0.
+
+namespace MKW.Storage
+{
+    public interface IDatabaseSerializer
+    {
+        BlobSecretEntry SerializeEntry(DatabaseEntry entry);
+        DatabaseEntry DeserializeEntry(BlobSecretEntry blob);
+
+        BlobUser SerializeUser(DatabaseUser user);
+        DatabaseUser DeserializeUser(BlobUser blob);
+
+        ReadOnlyMemory<byte> SerializeProtectedData(DatabaseUserProtectedData obj);
+    }
+}
