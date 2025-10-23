5db599398b3f0fe9e4dbd31f26cc9429e086d8e7
Timofei Zhakov <tima@chemodax.net>
2025-10-23 14:07:27 +0000

    DatabaseBlobStorageMemory: Add first implementation of transactionist IDatabaseBlobStore.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@5834 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.Storage.MKPG/BlobStore/DatabaseBlobStorageMemory.Snapshot.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Timofei Zhakov. All Rights Reserved
+// Licensed under the Apache License, Version 2.0.
+
+namespace MKW.Storage.MKPG.BlobStore
+{
+    internal sealed partial class DatabaseBlobStorageMemory
+    {
+        private sealed class Snapshot : IDatabaseBlobStore.ISnapshot
+        {
+            private readonly IReadOnlyCollection<Blob> blobs;
+
+            public Snapshot(IReadOnlyCollection<Blob> blobs)
+            {
+                this.blobs = [.. blobs];
+            }
+
+            public IEnumerable<Blob> Enumerate()
+            {
+                return blobs;
+            }
+
+            public void Dispose()
+            {
+            }
+        }
+    }
+}
---
+++ src/MKW.Storage.MKPG/BlobStore/DatabaseBlobStorageMemory.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Timofei Zhakov. All Rights Reserved
+// Licensed under the Apache License, Version 2.0.
+
+namespace MKW.Storage.MKPG.BlobStore
+{
+    internal sealed partial class DatabaseBlobStorageMemory : IDatabaseBlobStore
+    {
+        private IReadOnlyList<Blob> blobs;
+
+        public DatabaseBlobStorageMemory()
+        {
+            blobs = [];
+        }
+
+        public IDatabaseBlobStore.ITransaction BeginTransaction()
+        {
+            return new Transaction(this, blobs);
+        }
+
+        public IDatabaseBlobStore.ISnapshot CreateSnapshot()
+        {
+            return new Snapshot(blobs);
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
