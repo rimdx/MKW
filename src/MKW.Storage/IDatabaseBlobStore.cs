c2f6c2327ee5316b595ba6fab101089af300edae
Timofei Zhakov <tima@chemodax.net>
2025-10-23 07:42:02 +0000

    Add IDatabaseBlobStore abstraction.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@5833 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.Storage/IDatabaseBlobStore.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Timofei Zhakov. All Rights Reserved
+// Licensed under the Apache License, Version 2.0.
+
+namespace MKW.Storage
+{
+    public interface IDatabaseBlobStore : IDisposable
+    {
+        public interface ISnapshot
+            : IDisposable
+        {
+            IEnumerable<Blob> Enumerate();
+        }
+
+        public interface ITransaction
+            : ISnapshot
+            , IDisposable
+        {
+            void Create(Blob blob);
+            void Update(Blob blob);
+            bool Delete(BlobId blobId);
+
+            void Commit();
+        }
+
+        ITransaction BeginTransaction();
+        ISnapshot CreateSnapshot();
+    }
+}
