7bc0ac12ccc6ba196417b8bd4e76c756eb01517e
Timofei Zhakov <tima@chemodax.net>
2025-10-23 16:24:00 +0000

    Implement DatabaseBlobStorageSingleFile.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@5835 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.Storage.MKPG/BlobStore/DatabaseBlobStorageSingleFile.Transaction.cs
@@ -0,0 +1,102 @@
+﻿// Copyright (c) Timofei Zhakov. All Rights Reserved
+// Licensed under the Apache License, Version 2.0.
+
+using MKW.Storage.Exceptions;
+using MKW.Storage.MKPG.FileSystem;
+
+namespace MKW.Storage.MKPG.BlobStore
+{
+    internal sealed partial class DatabaseBlobStorageSingleFile
+    {
+        private sealed class Transaction : SnapshotBase, IDatabaseBlobStore.ITransaction
+        {
+            private readonly Dictionary<BlobId, PgpBlobEntry> editedEntries;
+            private readonly IFileEditorFactory.ITransaction transaction;
+            private bool disposed = false;
+
+            public Transaction(IFileEditorFactory.ITransaction transaction)
+            {
+                using StreamReader reader = new StreamReader(transaction.Reader);
+
+                editedEntries = [];
+                foreach (PgpBlobEntry blob in BlobStorageSerializer.ReadBlobs(reader))
+                {
+                    editedEntries.Add(blob.Id, blob);
+                }
+
+                this.transaction = transaction;
+            }
+
+            protected override IEnumerable<PgpBlobEntry> Blobs => editedEntries.Values;
+
+            private static string GetType(Blob blob)
+            {
+                if (blob is BlobUser)
+                {
+                    return MKPGConstants.ArmourTypeHeaders.User;
+                }
+                else if (blob is BlobSecretEntry)
+                {
+                    return MKPGConstants.ArmourTypeHeaders.Entry;
+                }
+                else
+                {
+                    throw new InvalidCastException();
+                }
+            }
+
+            public void Create(Blob blob)
+            {
+                if (editedEntries.ContainsKey(blob.Id))
+                {
+                    throw new EntryAlreadyExistsException();
+                }
+
+                editedEntries[blob.Id] = new PgpBlobEntry
+                {
+                    Id = blob.Id,
+                    Data = blob.Data,
+                    Type = GetType(blob),
+                };
+            }
+
+            public void Update(Blob blob)
+            {
+                editedEntries[blob.Id] = new PgpBlobEntry
+                {
+                    Id = blob.Id,
+                    Data = blob.Data,
+                    Type = GetType(blob),
+                };
+            }
+
+            public bool Delete(BlobId blobId)
+            {
+                return editedEntries.Remove(blobId);
+            }
+
+            public void Commit()
+            {
+                if (disposed)
+                {
+                    throw new ObjectDisposedException(nameof(Transaction));
+                }
+
+                using (StreamWriter writer = new StreamWriter(transaction.Writer))
+                {
+                    BlobStorageSerializer.WriteBlobs(writer, editedEntries.Values);
+                }
+
+                transaction.Commit();
+
+                disposed = true;
+            }
+
+            public override void Dispose()
+            {
+                transaction.Dispose();
+                disposed = true;
+            }
+        }
+    }
+}
---
+++ src/MKW.Storage.MKPG/BlobStore/DatabaseBlobStorageSingleFile.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Timofei Zhakov. All Rights Reserved
+// Licensed under the Apache License, Version 2.0.
+
+using MKW.Storage.MKPG.FileSystem;
+
+namespace MKW.Storage.MKPG.BlobStore
+{
+    internal sealed partial class DatabaseBlobStorageSingleFile : IDatabaseBlobStore
+    {
+        private readonly IFileEditorFactory editor;
+
+        public DatabaseBlobStorageSingleFile(IFileEditorFactory editor)
+        {
+            this.editor = editor;
+        }
+
+        public IDatabaseBlobStore.ITransaction BeginTransaction()
+        {
+            return new Transaction(editor.CreateTransaction());
+        }
+
+        public IDatabaseBlobStore.ISnapshot CreateSnapshot()
+        {
+            return new Snapshot(editor.CreateReader());
+        }
+
+        public void Dispose()
+        {
+            editor.Dispose();
+        }
+    }
+}
