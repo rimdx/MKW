ddac286de57c1517c7cc45627cddf254b827fdfa
Timofei Zhakov <tima@chemodax.net>
2025-10-23 14:07:27 +0000

    DatabaseBlobStorageMemory: Add first implementation of transactionist IDatabaseBlobStore.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@5834 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.Storage.MKPG/BlobStore/DatabaseBlobStorageMemory.Transaction.cs
@@ -0,0 +1,63 @@
+﻿// Copyright (c) Timofei Zhakov. All Rights Reserved
+// Licensed under the Apache License, Version 2.0.
+
+using MKW.Storage.Exceptions;
+
+namespace MKW.Storage.MKPG.BlobStore
+{
+    internal sealed partial class DatabaseBlobStorageMemory
+    {
+        private sealed class Transaction : IDatabaseBlobStore.ITransaction
+        {
+            private readonly DatabaseBlobStorageMemory database;
+            private readonly Dictionary<BlobId, Blob> blobs;
+
+            public Transaction(DatabaseBlobStorageMemory database,
+                               IReadOnlyCollection<Blob> blobs)
+            {
+                this.database = database;
+
+                this.blobs = [];
+                foreach (Blob blob in blobs)
+                {
+                    this.blobs[blob.Id] = blob;
+                }
+            }
+
+            public void Create(Blob blob)
+            {
+                if (blobs.ContainsKey(blob.Id))
+                {
+                    throw new EntryAlreadyExistsException();
+                }
+
+                blobs[blob.Id] = blob;
+            }
+
+            public bool Delete(BlobId blobId)
+            {
+                return blobs.Remove(blobId);
+            }
+
+            public IEnumerable<Blob> Enumerate()
+            {
+                return blobs.Values;
+            }
+
+            public void Update(Blob blob)
+            {
+                blobs[blob.Id] = blob;
+            }
+
+            public void Commit()
+            {
+                database.blobs = [.. blobs.Values];
+            }
+
+            public void Dispose()
+            {
+                /* no-op */
+            }
+        }
+    }
+}
---
+++ src/MKW.Storage.Tests/BlobStorageTests2.cs
@@ -0,0 +1,103 @@
+﻿// Copyright (c) Timofei Zhakov. All Rights Reserved
+// Licensed under the Apache License, Version 2.0.
+
+using MKW.Core;
+using MKW.Storage.Exceptions;
+using MKW.Storage.MKPG.BlobStore;
+using NUnit.Framework.Legacy;
+
+namespace MKW.Storage.Tests
+{
+    [TestFixture(BackendType.Memory)]
+    public class BlobStorageTests2(BlobStorageTests2.BackendType type)
+    {
+        public enum BackendType
+        {
+            Memory,
+        }
+
+        private IDatabaseBlobStore store = default!;
+
+        [SetUp]
+        public void Setup()
+        {
+            if (type == BackendType.Memory)
+            {
+                store = new DatabaseBlobStorageMemory();
+            }
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            store.Dispose();
+        }
+
+        [Test]
+        public void SimpleTest()
+        {
+            Random random = new Random(42);
+
+            BlobId id1 = BlobId.From(UserId.Create());
+            BlobId id2 = BlobId.From(UserId.Create());
+
+            byte[] data1 = new byte[1024];
+            random.NextBytes(data1);
+
+            byte[] data2 = new byte[512];
+            random.NextBytes(data2);
+
+            using (IDatabaseBlobStore.ITransaction transaction = store.BeginTransaction())
+            {
+                transaction.Create(new BlobUser
+                {
+                    Id = id1,
+                    Data = data1
+                });
+
+                transaction.Create(new BlobUser
+                {
+                    Id = id2,
+                    Data = data2
+                });
+
+                transaction.Commit();
+            }
+
+            using (IDatabaseBlobStore.ISnapshot snapshot = store.CreateSnapshot())
+            {
+                Blob[] entries = [.. snapshot.Enumerate()];
+
+                ClassicAssert.AreEqual(2, entries.Length);
+                CollectionAssert.AreEqual(data1, entries[0].Data.ToArray());
+                CollectionAssert.AreEqual(data2, entries[1].Data.ToArray());
+            }
+
+            using (IDatabaseBlobStore.ITransaction transaction = store.BeginTransaction())
+            {
+                Assert.Throws<EntryAlreadyExistsException>(
+                    () =>
+                    {
+                        transaction.Create(new BlobUser
+                        {
+                            Id = id1,
+                            Data = data1
+                        });
+                    });
+            }
+
+            using (IDatabaseBlobStore.ITransaction transaction = store.BeginTransaction())
+            {
+                ClassicAssert.AreEqual(false, transaction.Delete(BlobId.From(UserId.Create())));
+                ClassicAssert.AreEqual(true, transaction.Delete(id2));
+                ClassicAssert.AreEqual(1, transaction.Enumerate().Count());
+                transaction.Commit();
+            }
+
+            using (IDatabaseBlobStore.ISnapshot snapshot = store.CreateSnapshot())
+            {
+                ClassicAssert.AreEqual(1, snapshot.Enumerate().Count());
+            }
+        }
+    }
+}
