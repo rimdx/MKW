0b0d12806eea95df518cc4f9b06a8f9878bb8583
Timofei Zhakov <tima@chemodax.net>
2025-10-24 10:23:05 +0000

    MKPGSerializer: Implement IDatabaseSerializer for this storage provider.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@5840 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.Storage.MKPG/MKPGSerializer.cs
@@ -0,0 +1,60 @@
+﻿// Copyright (c) Timofei Zhakov. All Rights Reserved
+// Licensed under the Apache License, Version 2.0.
+
+using MKW.Core;
+using System.Buffers;
+
+namespace MKW.Storage.MKPG
+{
+    public sealed class MKPGSerializer
+        : IDatabaseSerializer
+    {
+        public MKPGSerializer()
+        {
+        }
+
+        // Entry
+        public DatabaseEntry DeserializeEntry(BlobSecretEntry blob)
+        {
+            return EntrySerializer.Deserialize(blob.CreateReader(),
+                                               EntryId.FromBytes(blob.Id.GetBytes().Span));
+        }
+
+        public BlobSecretEntry SerializeEntry(DatabaseEntry entry)
+        {
+            ArrayBufferWriter<byte> writer = new ArrayBufferWriter<byte>();
+            EntrySerializer.Serialize(writer, entry);
+
+            return new BlobSecretEntry
+            {
+                Id = BlobId.From(entry.Id),
+                Data = writer.WrittenMemory,
+            };
+        }
+
+        // User
+        public DatabaseUser DeserializeUser(BlobUser blob)
+        {
+            return UserSerializer.Deserialize(blob.CreateReader(),
+                                              UserId.FromBytes(blob.Id.GetBytes()));
+        }
+
+        public BlobUser SerializeUser(DatabaseUser user)
+        {
+            ArrayBufferWriter<byte> writer = new ArrayBufferWriter<byte>();
+            UserSerializer.Serialize(writer, user);
+
+            return new BlobUser
+            {
+                Id = BlobId.From(user.Id),
+                Data = writer.WrittenMemory,
+            };
+        }
+
+        // Payloads
+        public ReadOnlyMemory<byte> SerializeProtectedData(DatabaseUserProtectedData obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
