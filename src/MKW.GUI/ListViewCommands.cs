0cff4f4fa4dbaf3ec5f7d9b0ba826e5df18079e1
Ivan.Z <53048645+chemodax@users.noreply.github.com>
2025-10-25 14:52:23 +0000

    GUI: Add ListViewCommands.DefaultAction command and use it to handle double click in entries list view.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@5849 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.GUI/ListViewCommands.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Timofei Zhakov. All Rights Reserved
+// Licensed under the Apache License, Version 2.0.
+
+using System.Windows.Input;
+
+namespace MKW.GUI
+{
+    internal static class ListViewCommands
+    {
+        public static RoutedUICommand DefaultAction { get; } = new RoutedUICommand();
+    }
+}
