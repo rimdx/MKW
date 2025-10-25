0cfe7b8419b7677a874e9b237a43dbca6de39f8a
Ivan.Z <53048645+chemodax@users.noreply.github.com>
2025-10-25 19:56:39 +0000

    Move MDatabase.SnapshotBase.EnumerateBlobEntry<T> to extension method for IDatabaseBlobStore.ISnapshot.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@5856 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.Storage/IDatabaseBlobStoreExtensions.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Timofei Zhakov. All Rights Reserved
+// Licensed under the Apache License, Version 2.0.
+
+namespace MKW.Storage
+{
+    public static class IDatabaseBlobStoreExtensions
+    {
+        public static IEnumerable<T> EnumerateBlobEntry<T>(this IDatabaseBlobStore.ISnapshot snapshot) where T : Blob
+        {
+            foreach (Blob blob in snapshot.Enumerate())
+            {
+                if (blob is T entry)
+                {
+                    yield return entry;
+                }
+            }
+        }
+    }
+}
