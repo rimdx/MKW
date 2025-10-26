5efbb212316194f1698ea6d384a7d7c4bd3255b3
Timofei Zhakov <tima@chemodax.net>
2025-10-26 13:56:06 +0000

    IDatabaseBlobStore: Move nested interfaces into separate files.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@5871 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.Storage/IDatabaseBlobStore.ITransaction.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Timofei Zhakov. All Rights Reserved
+// Licensed under the Apache License, Version 2.0.
+
+namespace MKW.Storage
+{
+    public partial interface IDatabaseBlobStore
+    {
+        public interface ITransaction
+            : ISnapshot
+            , IDisposable
+        {
+            void Create(Blob blob);
+            void Update(Blob blob);
+            bool Delete(BlobId blobId);
+
+            void Commit();
+        }
+    }
+}
--- src/MKW.Storage/IDatabaseBlobStore.cs
+++ src/MKW.Storage/IDatabaseBlobStore.cs
@@ -3,25 +3,8 @@
 
 namespace MKW.Storage
 {
-    public interface IDatabaseBlobStore : IDisposable
+    public partial interface IDatabaseBlobStore : IDisposable
     {
-        public interface ISnapshot
-            : IDisposable
-        {
-            IEnumerable<Blob> Enumerate();
-        }
-
-        public interface ITransaction
-            : ISnapshot
-            , IDisposable
-        {
-            void Create(Blob blob);
-            void Update(Blob blob);
-            bool Delete(BlobId blobId);
-
-            void Commit();
-        }
-
         ITransaction BeginTransaction();
         ISnapshot CreateSnapshot();
     }
