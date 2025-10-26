ac7ed1655594d4f8f30269b21d716281ef2c5b4b
Timofei Zhakov <tima@chemodax.net>
2025-10-26 13:56:29 +0000

    IDatabase3: Move nested interfaces into separate files.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@5872 0083e6b4-6090-234a-836e-88fcc467669c

--- src/MKW.Storage/IDatabase3.cs
+++ src/MKW.Storage/IDatabase3.cs
@@ -1,41 +1,10 @@
 ﻿// Copyright (c) Timofei Zhakov. All Rights Reserved
 // Licensed under the Apache License, Version 2.0.
 
-using MKW.Core;
-
 namespace MKW.Storage
 {
-    public interface IDatabase3 : IDisposable
+    public partial interface IDatabase3 : IDisposable
     {
-        public interface ISnapshot
-            : IDatabaseSerializer
-            , IDisposable
-        {
-            DatabaseUser OpenUser(UserId userId);
-            bool HasUser(UserId id);
-            IEnumerable<DatabaseUser> EnumerateUsers();
-
-            DatabaseEntry OpenEntry(EntryId id);
-            bool HasEntry(EntryId id);
-            IEnumerable<DatabaseEntry> EnumerateEntries();
-        }
-
-        public interface ITransaction
-            : ISnapshot
-            , IDatabaseSerializer
-            , IDisposable
-        {
-            void CreateUser(DatabaseUser user);
-            void UpdateUser(DatabaseUser user);
-            bool DeleteUser(UserId id);
-
-            void CreateEntry(DatabaseEntry entry);
-            void UpdateEntry(DatabaseEntry entry);
-            bool DeleteEntry(EntryId id);
-
-            void Commit();
-        }
-
         ITransaction BeginTransaction();
 
         ISnapshot CreateSnapshot();
