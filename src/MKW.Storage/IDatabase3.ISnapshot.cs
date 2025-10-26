086dce288a22cc05d6c4fa132fb99326bbe2288f
Timofei Zhakov <tima@chemodax.net>
2025-10-26 13:56:29 +0000

    IDatabase3: Move nested interfaces into separate files.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@5872 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.Storage/IDatabase3.ISnapshot.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Timofei Zhakov. All Rights Reserved
+// Licensed under the Apache License, Version 2.0.
+
+using MKW.Core;
+
+namespace MKW.Storage
+{
+    public partial interface IDatabase3
+    {
+        public interface ISnapshot
+            : IDatabaseSerializer
+            , IDisposable
+        {
+            DatabaseUser OpenUser(UserId userId);
+            bool HasUser(UserId id);
+            IEnumerable<DatabaseUser> EnumerateUsers();
+
+            DatabaseEntry OpenEntry(EntryId id);
+            bool HasEntry(EntryId id);
+            IEnumerable<DatabaseEntry> EnumerateEntries();
+        }
+    }
+}
---
+++ src/MKW.Storage/IDatabase3.ITransaction.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Timofei Zhakov. All Rights Reserved
+// Licensed under the Apache License, Version 2.0.
+
+using MKW.Core;
+
+namespace MKW.Storage
+{
+    public partial interface IDatabase3
+    {
+        public interface ITransaction
+            : ISnapshot
+            , IDatabaseSerializer
+            , IDisposable
+        {
+            void CreateUser(DatabaseUser user);
+            void UpdateUser(DatabaseUser user);
+            bool DeleteUser(UserId id);
+
+            void CreateEntry(DatabaseEntry entry);
+            void UpdateEntry(DatabaseEntry entry);
+            bool DeleteEntry(EntryId id);
+
+            void Commit();
+        }
+    }
+}
