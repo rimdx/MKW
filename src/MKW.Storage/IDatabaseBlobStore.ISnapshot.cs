5938f2608dc2f779d519a71e68a68129f9c27af7
Timofei Zhakov <tima@chemodax.net>
2025-10-26 13:56:06 +0000

    IDatabaseBlobStore: Move nested interfaces into separate files.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@5871 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.Storage/IDatabaseBlobStore.ISnapshot.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Timofei Zhakov. All Rights Reserved
+// Licensed under the Apache License, Version 2.0.
+
+namespace MKW.Storage
+{
+    public partial interface IDatabaseBlobStore
+    {
+        public interface ISnapshot
+            : IDisposable
+        {
+            IEnumerable<Blob> Enumerate();
+        }
+    }
+}
