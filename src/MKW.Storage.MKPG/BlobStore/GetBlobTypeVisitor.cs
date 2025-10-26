659e5484d8fe57190c60bf4e12a33256bdf79b21
Timofei Zhakov <tima@chemodax.net>
2025-10-26 13:43:57 +0000

    Use a visitor to retrieve type of a blob.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@5866 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.Storage.MKPG/BlobStore/GetBlobTypeVisitor.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Timofei Zhakov. All Rights Reserved
+// Licensed under the Apache License, Version 2.0.
+
+namespace MKW.Storage.MKPG.BlobStore
+{
+    internal sealed class GetBlobTypeVisitor : Blob.IVisitor<string>
+    {
+        public string VisitSecretEntry(BlobSecretEntry secretEntry)
+        {
+            return MKPGConstants.ArmourTypeHeaders.Entry;
+        }
+
+        public string VisitUser(BlobUser user)
+        {
+            return MKPGConstants.ArmourTypeHeaders.User;
+        }
+    }
+}
