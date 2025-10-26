c8be701b0df88c796111e604517e101659833ba2
Timofei Zhakov <tima@chemodax.net>
2025-10-26 20:40:56 +0000

    Implement serialization for User ID signature subpacket.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@5888 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.Core.Serialization/OpenPgp/Packets/SignatureSubpacketIssuerKeyId.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Timofei Zhakov. All Rights Reserved
+// Licensed under the Apache License, Version 2.0.
+
+namespace MKW.Core.Serialization.OpenPgp.Packets
+{
+    public sealed record class SignatureSubpacketIssuerKeyId
+    {
+        // 8 octets
+        public required ReadOnlyMemory<byte> KeyId;
+    }
+}
---
+++ src/MKW.Core.Serialization/OpenPgp/Packets/SignatureSubpacketIssuerKeyIdSerializer.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Timofei Zhakov. All Rights Reserved
+// Licensed under the Apache License, Version 2.0.
+
+using MKW.Common;
+using Org.BouncyCastle.Bcpg;
+
+namespace MKW.Core.Serialization.OpenPgp.Packets
+{
+    // https://www.rfc-editor.org/rfc/rfc4880#section-5.2.3.5
+    //
+    // 5.2.3.5.  Issuer
+    // (8-octet Key ID)
+    // The OpenPGP Key ID of the key issuing the signature.
+    public static class SignatureSubpacketIssuerKeyIdSerializer
+    {
+        public const SignatureSubpacketTag Tag = SignatureSubpacketTag.IssuerKeyId;
+
+        public static SignatureSubpacketIssuerKeyId Deserialize(IBufferReader<byte> reader)
+        {
+            return new SignatureSubpacketIssuerKeyId
+            {
+                KeyId = reader.ReadBytes(8),
+            };
+        }
+    }
+}
