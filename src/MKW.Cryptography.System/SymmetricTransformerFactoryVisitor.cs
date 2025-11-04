c62546d09942db59b27ccef02197d247c524497b
Timofei Zhakov <tima@chemodax.net>
2025-11-04 10:41:23 +0000

    Use SymmetricKey.IVisitor to construct ISymmerticTransformer's in cryptography providers.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@5912 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.Cryptography.System/SymmetricTransformerFactoryVisitor.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Timofei Zhakov. All Rights Reserved
+// Licensed under the Apache License, Version 2.0.
+
+namespace MKW.Cryptography.System
+{
+    internal class SymmetricTransformerFactoryVisitor : SymmetricKey.IVisitor<ISymmetricTransformer>
+    {
+        public ISymmetricTransformer VisitAesGcm(SymmetricKeyAesGcm key)
+        {
+            return SymmetricTransformer.Open(key);
+        }
+
+        public ISymmetricTransformer VisitAesOpenPgpCfb(SymmetricKeyAesOpenPgpCfb key)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
