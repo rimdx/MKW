1501d7fb06646b9d35927393a2852ef5934de4b5
Timofei Zhakov <tima@chemodax.net>
2025-11-04 14:12:29 +0000

    Extend .NET framework version of BouncyCastle to support spans in IDigest.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@5916 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.Cryptography.BouncyCastle.NETFramework/IDigestExtensions.cs
@@ -0,0 +1,26 @@
+// Copyright (c) Timofei Zhakov. All Rights Reserved
+// Licensed under the Apache License, Version 2.0.
+
+using Org.BouncyCastle.Crypto;
+using System.Buffers;
+
+namespace MKW.Cryptography.BouncyCastle
+{
+    public static class IDigestExtensions
+    {
+        public static void BlockUpdate(this IDigest digest, ReadOnlySpan<byte> input)
+        {
+            byte[] sharedBuffer = ArrayPool<byte>.Shared.Rent(input.Length);
+
+            try
+            {
+                input.CopyTo(sharedBuffer);
+                digest.BlockUpdate(sharedBuffer, 0, input.Length);
+            }
+            finally
+            {
+                ArrayPool<byte>.Shared.Return(sharedBuffer);
+            }
+        }
+    }
+}
