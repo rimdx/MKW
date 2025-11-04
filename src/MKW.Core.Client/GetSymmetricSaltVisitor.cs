52e638972e29f5a5dd47e60cf5ac85eaa68c7f98
Timofei Zhakov <tima@chemodax.net>
2025-11-04 11:17:56 +0000

    Use visitors in client to retrieve both IV and Key of a symmetric key.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@5915 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.Core.Client/GetSymmetricSaltVisitor.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Timofei Zhakov. All Rights Reserved
+// Licensed under the Apache License, Version 2.0.
+
+using MKW.Cryptography;
+
+namespace MKW.Core.Client
+{
+    internal sealed class GetSymmetricSaltVisitor : SymmetricKey.IVisitor<ReadOnlyMemory<byte>>
+    {
+        public ReadOnlyMemory<byte> VisitAesGcm(SymmetricKeyAesGcm key)
+        {
+            return key.IVBytes;
+        }
+
+        public ReadOnlyMemory<byte> VisitAesOpenPgpCfb(SymmetricKeyAesOpenPgpCfb key)
+        {
+            return Array.Empty<byte>();
+        }
+    }
+}
