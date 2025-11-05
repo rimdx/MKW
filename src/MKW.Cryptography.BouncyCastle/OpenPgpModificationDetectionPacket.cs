c65f5012497da5559428194c2884fe549b508b88
Timofei Zhakov <tima@chemodax.net>
2025-11-05 14:15:03 +0000

    AesOpenPgpTransformer: Support modification detection.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@5919 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.Cryptography.BouncyCastle/OpenPgpModificationDetectionPacket.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Timofei Zhakov. All Rights Reserved
+// Licensed under the Apache License, Version 2.0.
+
+namespace MKW.Cryptography.BouncyCastle
+{
+    internal sealed record class OpenPgpModificationDetectionPacket
+    {
+        public required ReadOnlyMemory<byte> Salt { get; init; }
+        public required ReadOnlyMemory<byte> Plaintext { get; init; }
+        public required ReadOnlyMemory<byte> Checksum { get; init; }
+    }
+}
---
+++ src/MKW.Cryptography.BouncyCastle/OpenPgpModificationDetectionPacketCorruptedException.cs
@@ -0,0 +1,8 @@
+﻿// Copyright (c) Timofei Zhakov. All Rights Reserved
+// Licensed under the Apache License, Version 2.0.
+
+namespace MKW.Cryptography.BouncyCastle
+{
+    internal sealed class OpenPgpModificationDetectionPacketCorruptedException(string reason)
+        : Exception($"Modification detection packet corrupted: {reason}");
+}
