ab502a72703abf724f52ca9245280621d5719e78
Timofei Zhakov <tima@chemodax.net>
2025-11-08 09:18:47 +0000

    Implement RSA secret key serialization.
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@5926 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.Core.Serialization/OpenPgp/Packets/SecretKeyMaterialRSA.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Timofei Zhakov. All Rights Reserved
+// Licensed under the Apache License, Version 2.0.
+
+using Org.BouncyCastle.Math;
+
+namespace MKW.Core.Serialization.OpenPgp.Packets
+{
+    public sealed record class SecretKeyMaterialRSA
+    {
+        public required BigInteger PrivateExponent { get; init; }
+        public required BigInteger PrimeP { get; init; }
+        public required BigInteger PrimeQ { get; init; }
+        public required BigInteger Inverse { get; init; }
+    }
+}
---
+++ src/MKW.Core.Serialization/OpenPgp/Packets/SecretKeyMaterialRSASerializer.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) Timofei Zhakov. All Rights Reserved
+// Licensed under the Apache License, Version 2.0.
+
+using MKW.Common;
+using MKW.Core.Serialization.OpenPgp.Primitives;
+using Org.BouncyCastle.Math;
+
+namespace MKW.Core.Serialization.OpenPgp.Packets
+{
+    // Algorithm-Specific Fields for RSA secret keys:
+    // - multiprecision integer (MPI) of RSA secret exponent d.
+    // - MPI of RSA secret prime value p.
+    // - MPI of RSA secret prime value q (p < q).
+    // - MPI of u, the multiplicative inverse of p, mod q.
+    //
+    // Algorithm-Specific Fields for DSA secret keys:
+    // - MPI of DSA secret exponent x.
+    //
+    // Algorithm-Specific Fields for Elgamal secret keys:
+    // - MPI of Elgamal secret exponent x.
+    public static class SecretKeyMaterialRSASerializer
+    {
+        public static SecretKeyMaterialRSA Deserialize(IBufferReader<byte> reader)
+        {
+            BigInteger d = MPIntegerSerailizer.Deserialize(reader);
+
+            BigInteger p = MPIntegerSerailizer.Deserialize(reader);
+            BigInteger q = MPIntegerSerailizer.Deserialize(reader);
+
+            BigInteger u = MPIntegerSerailizer.Deserialize(reader);
+
+            return new SecretKeyMaterialRSA
+            {
+                PrivateExponent = d,
+                PrimeP = p,
+                PrimeQ = q,
+                Inverse = u,
+            };
+        }
+    }
+}
