1646d969665622e8c57ea541987af41fee30270e
Timofei Zhakov <tima@chemodax.net>
2025-11-08 20:07:21 +0000

    Implement serializer for session key payload.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@5931 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.Core.Serialization/OpenPgp/Packets/SessionKeyPayload.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Timofei Zhakov. All Rights Reserved
+// Licensed under the Apache License, Version 2.0.
+
+using Org.BouncyCastle.Bcpg;
+
+namespace MKW.Core.Serialization.OpenPgp.Packets
+{
+    public sealed record class SessionKeyPayload
+    {
+        public required SymmetricKeyAlgorithmTag SymmetricAlgorithm { get; init; }
+        public required ushort Checksum { get; init; }
+        public required ReadOnlyMemory<byte> KeyBytes { get; init; }
+    }
+}
