5f59efe05643eec13fed926efe95a47b6abe4b45
Ivan.Z <53048645+chemodax@users.noreply.github.com>
2025-11-09 14:10:49 +0000

    Add PgpPacketReader with method ReadAll().
    git-svn-id: https://svn.rinrab.com/rinrab/MKW/trunk@5961 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.Core.Serialization/OpenPgp/Packets/PgpPacketReader.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Timofei Zhakov. All Rights Reserved
+// Licensed under the Apache License, Version 2.0.
+
+using MKW.Common;
+
+namespace MKW.Core.Serialization.OpenPgp.Packets
+{
+    public static class PgpPacketReader
+    {
+        public static IEnumerable<PgpPacketBody> ReadAll(IBufferReader<byte> reader)
+        {
+            while (reader.RemainingBytes > 0)
+            {
+                PgpPacket packet = PgpPacketSerializer.ReadPacket(reader);
+
+                yield return PgpPacketBodySerializer.Deserialize(packet);
+            }
+        }
+    }
+}
