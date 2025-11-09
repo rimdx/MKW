6b880e236b536bfa1b6447ec428b04f0fcabd15f
Timofei Zhakov <tima@chemodax.net>
2025-11-09 14:07:01 +0000

    Implement a library that can encode or decode PGP data so we can investigate our algorithms and other infrastructure on real examples.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@5957 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/MKW.OpenPgp.Tests/PrivateKeyTests.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) Timofei Zhakov. All Rights Reserved
+// Licensed under the Apache License, Version 2.0.
+
+using MKW.Core.Serialization.OpenPgp;
+using MKW.Cryptography;
+using MKW.Cryptography.Loader;
+using MKW.Testing.Common;
+using NUnit.Framework.Legacy;
+using System.Text;
+
+namespace MKW.OpenPgp.Tests
+{
+    [TestFixture]
+    public class PrivateKeyTests
+    {
+        private static readonly ICryptographyProvider crypto = BouncyCastleLoader.GetProvider();
+
+        [Test]
+        public void SimpleTest()
+        {
+            PgpArmouredMessage? keyArmour = PgpArmouredMessageSerializer.Deserialize(new StreamReader(PgpTestKeys.TestPrivateKey));
+            ClassicAssert.NotNull(keyArmour);
+
+            PgpArmouredMessage? msgArmour = PgpArmouredMessageSerializer.Deserialize(new StreamReader(PgpTestKeys.PublicKeyEncryptedMessageAes128NoCompression));
+            ClassicAssert.NotNull(msgArmour);
+
+            PgpPrivateKey key = new PgpPrivateKey(crypto);
+            key.Import(keyArmour);
+
+            PgpMessage msg = PgpMessage.Open(msgArmour);
+
+            ReadOnlyMemory<byte> plaintext = key.DecryptMessage(msg);
+
+            Console.WriteLine(Encoding.UTF8.GetString(plaintext.ToArray()));
+        }
+    }
+}
---
+++ src/MKW.OpenPgp/SessionKeyProcessor.cs
@@ -0,0 +1,62 @@
+﻿// Copyright (c) Timofei Zhakov. All Rights Reserved
+// Licensed under the Apache License, Version 2.0.
+
+using MKW.Core.Serialization.OpenPgp.Packets;
+using MKW.Cryptography;
+using Org.BouncyCastle.Bcpg;
+
+namespace MKW.OpenPgp
+{
+    internal sealed class SessionKeyProcessor
+    {
+        private readonly ICryptographyProvider crypto;
+
+        public SessionKeyProcessor(ICryptographyProvider crypto)
+        {
+            this.crypto = crypto;
+        }
+
+        public SymmetricKey OpenKey(SessionKeyPayload payload)
+        {
+            ushort checksum = GetChecksum(payload.KeyBytes.Span);
+
+            if (checksum != payload.Checksum)
+            {
+                throw new SessionKeyChecksumMismatchException(checksum, payload.Checksum);
+            }
+
+            if (payload.SymmetricAlgorithm == SymmetricKeyAlgorithmTag.Aes256)
+            {
+                return crypto.OpenSymmetricKey(CommonCryptographyAlgorithms.Aes256OpenPgpCfb,
+                                               payload.KeyBytes,
+                                               null);
+            }
+            else if (payload.SymmetricAlgorithm == SymmetricKeyAlgorithmTag.Aes128)
+            {
+                return crypto.OpenSymmetricKey(CommonCryptographyAlgorithms.Aes128OpenPgpCfb,
+                                               payload.KeyBytes,
+                                               null);
+            }
+            else
+            {
+                throw new NotSupportedException();
+            }
+        }
+
+        // Then a two-octet checksum is appended, which is equal to the
+        // sum of the preceding session key octets, not including the algorithm
+        // identifier, modulo 65536.
+        private static ushort GetChecksum(ReadOnlySpan<byte> data)
+        {
+            ushort checksum = 0;
+
+            foreach (byte octet in data)
+            {
+                checksum += octet;
+            }
+
+            // #letsbeextrasafe
+            return (ushort)(checksum % 65536);
+        }
+    }
+}
