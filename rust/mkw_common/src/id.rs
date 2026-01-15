847e027fb24848f4d737bc3924962a4463907ee6
Timofei Zhakov <tima@chemodax.net>
2026-01-15 12:49:33 +0000

    * rust/mkw_common: New project to collect all general utilities in future.   Currently, only user ID and entry ID are implemented.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@6100 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ rust/mkw_common/src/id.rs
@@ -0,0 +1,51 @@
+extern crate rand;
+
+const USER_ID_SIZE: usize = 8;
+const ENTRY_ID_SIZE: usize = 8;
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct mkw_user_id_t {
+    data: [u8; USER_ID_SIZE],
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct mkw_entry_id_t {
+    data: [u8; ENTRY_ID_SIZE],
+}
+
+impl mkw_user_id_t {
+    pub fn create() -> Self {
+        let mut buf = [0u8; USER_ID_SIZE];
+        let mut rng = rand::rng();
+
+        rand::RngCore::fill_bytes(&mut rng, &mut buf[..]);
+
+        return Self {
+            data: buf,
+        };
+    }
+}
+
+impl mkw_entry_id_t {
+    pub fn create() -> Self {
+        let mut buf = [0u8; ENTRY_ID_SIZE];
+        let mut rng = rand::rng();
+
+        rand::RngCore::fill_bytes(&mut rng, &mut buf[..]);
+
+        return Self {
+            data: buf,
+        };
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::id::mkw_user_id_t;
+
+    #[test]
+    fn create_user_id_test() {
+        let id = mkw_user_id_t::create();
+        println!("{:?}", id);
+    }
+}
