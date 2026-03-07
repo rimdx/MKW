c5a0c8c557e47d4f98e99d7bff70a8f9925e45a4
Timofei Zhakov <tima@chemodax.net>
2026-03-07 20:38:34 +0000

    Implement hex decoder to write test vectors simply.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@6700 0083e6b4-6090-234a-836e-88fcc467669c

--- native/main.c
+++ native/main.c
@@ -308,6 +308,43 @@ test_seckeydata_round_trip(mkw_ctx_t *ctx, mkw_pool_t *pool)
     return MKW_ERROR_NONE;
 }
 
+static char *hex_enc = "0123456789abcdef";
+
+static int
+decrypt_one_hex(char ch)
+{
+    switch (ch) {
+        case '0': return 0;
+        case '1': return 1;
+        case '2': return 2;
+        case '3': return 3;
+        case '4': return 4;
+        case '5': return 5;
+        case '6': return 6;
+        case '7': return 7;
+        case '8': return 8;
+        case '9': return 9;
+        case 'a': return 10;
+        case 'b': return 11;
+        case 'c': return 12;
+        case 'd': return 13;
+        case 'e': return 14;
+        case 'f': return 15;
+        default: abort();
+    };
+}
+
+static uint8_t *
+hex(const char *str, mkw_pool_t *pool)
+{
+    assert(strlen(str) % 2 == 0);
+    uint8_t *result = mkw_pcalloc(pool, strlen(str) / 2);
+    for (; *str; str += 2, result++) {
+        *result = decrypt_one_hex(str[0]) * 16 + decrypt_one_hex(str[1]);
+    }
+    return result;
+}
+
 static mkw_error_t
 test_mdc_round_trip()
 {
