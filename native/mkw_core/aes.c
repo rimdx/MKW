e3a05b5ade23c2d576b401e176336a6552f96ce2
Timofei Zhakov <tima@chemodax.net>
2026-03-08 13:31:27 +0000

    Format aes.c to our standards.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@6718 0083e6b4-6090-234a-836e-88fcc467669c

--- native/mkw_core/aes.c
+++ native/mkw_core/aes.c
@@ -1,60 +1,49 @@
 /*
-
-This is an implementation of the AES algorithm, specifically ECB, CTR and CBC mode.
-Block size can be chosen in aes.h - available choices are AES128, AES192, AES256.
-
-The implementation is verified against the test vectors in:
-  National Institute of Standards and Technology Special Publication 800-38A 2001 ED
-
-ECB-AES128
-----------
-
-  plain-text:
-    6bc1bee22e409f96e93d7e117393172a
-    ae2d8a571e03ac9c9eb76fac45af8e51
-    30c81c46a35ce411e5fbc1191a0a52ef
-    f69f2445df4f9b17ad2b417be66c3710
-
-  key:
-    2b7e151628aed2a6abf7158809cf4f3c
-
-  resulting cipher
-    3ad77bb40d7a3660a89ecaf32466ef97 
-    f5d3d58503b9699de785895a96fdbaaf 
-    43b1cd7f598ece23881b00e3ed030688 
-    7b0c785e27e8ad3f8223207104725dd4 
-
-
-NOTE:   String length must be evenly divisible by 16byte (str_len % 16 == 0)
-        You should pad the end of the string with zeros if this is not the case.
-        For AES192/256 the key size is proportionally larger.
-
-*/
-
-
-/*****************************************************************************/
-/* Includes:                                                                 */
-/*****************************************************************************/
+ * This is an implementation of the AES algorithm, specifically ECB, CTR and
+ * CBC mode. Block size can be chosen in aes.h - available choices are AES128,
+ * AES192, AES256.
+ * 
+ * The implementation is verified against the test vectors in: National
+ * Institute of Standards and Technology Special Publication 800-38A 2001 ED
+ * 
+ * ECB-AES128
+ * ----------
+ * 
+ *   plain-text:
+ *     6bc1bee22e409f96e93d7e117393172a
+ *     ae2d8a571e03ac9c9eb76fac45af8e51
+ *     30c81c46a35ce411e5fbc1191a0a52ef
+ *     f69f2445df4f9b17ad2b417be66c3710
+ * 
+ *   key:
+ *     2b7e151628aed2a6abf7158809cf4f3c
+ * 
+ *   resulting cipher
+ *     3ad77bb40d7a3660a89ecaf32466ef97 
+ *     f5d3d58503b9699de785895a96fdbaaf 
+ *     43b1cd7f598ece23881b00e3ed030688 
+ *     7b0c785e27e8ad3f8223207104725dd4 
+ * 
+ * 
+ * NOTE:   String length must be evenly divisible by 16byte (str_len % 16 == 0)
+ *         You should pad the end of the string with zeros if this is not the case.
+ *         For AES192/256 the key size is proportionally larger.
+ * 
+ */
 
 #include "mkw_crypto.h"
 
-/*****************************************************************************/
-/* Defines:                                                                  */
-/*****************************************************************************/
-
-// The number of columns comprising a state in AES. This is a constant in AES. Value=4
+/* The number of columns comprising a state in AES. This is a constant in AES.
+ * Value=4 */
 #define Nb 4
 
-// The number of 32 bit words in a key.
+/* The number of 32 bit words in a key. */
 #define Nk 4
 
-// The number of rounds in AES Cipher.
+/* The number of rounds in AES Cipher. */
 #define Nr 10
 
-/*****************************************************************************/
-/* Private variables:                                                        */
-/*****************************************************************************/
-// state - array holding the intermediate results during decryption.
+/* state - array holding the intermediate results during decryption. */
 typedef uint8_t state_t[4][4];
 
 // The lookup-tables are marked const so they can be placed in read-only storage instead of RAM
@@ -97,123 +86,128 @@ static const uint8_t rsbox[256] = {
   0xa0, 0xe0, 0x3b, 0x4d, 0xae, 0x2a, 0xf5, 0xb0, 0xc8, 0xeb, 0xbb, 0x3c, 0x83, 0x53, 0x99, 0x61,
   0x17, 0x2b, 0x04, 0x7e, 0xba, 0x77, 0xd6, 0x26, 0xe1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0c, 0x7d };
 
-// The round constant word array, Rcon[i], contains the values given by 
-// x to the power (i-1) being powers of x (x is denoted as {02}) in the field GF(2^8)
+/*
+ * The round constant word array, Rcon[i], contains the values given by x to
+ * the power (i-1) being powers of x (x is denoted as {02}) in the field
+ * GF(2^8)
+ */
 static const uint8_t Rcon[11] = {
   0x8d, 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1b, 0x36 };
 
 /*
- * Jordan Goulder points out in PR #12 (https://github.com/kokke/tiny-AES-C/pull/12),
- * that you can remove most of the elements in the Rcon array, because they are unused.
+ * Jordan Goulder points out in PR #12
+ * (https://github.com/kokke/tiny-AES-C/pull/12), that you can remove most of
+ * the elements in the Rcon array, because they are unused.
  *
- * From Wikipedia's article on the Rijndael key schedule @ https://en.wikipedia.org/wiki/Rijndael_key_schedule#Rcon
- * 
- * "Only the first some of these constants are actually used – up to rcon[10] for AES-128 (as 11 round keys are needed), 
- *  up to rcon[8] for AES-192, up to rcon[7] for AES-256. rcon[0] is not used in AES algorithm."
+ * From Wikipedia's article on the Rijndael key schedule @
+ * https://en.wikipedia.org/wiki/Rijndael_key_schedule#Rcon
+ *
+ * "Only the first some of these constants are actually used – up to rcon[10]
+ * for AES-128 (as 11 round keys are needed), up to rcon[8] for AES-192, up to
+ * rcon[7] for AES-256. rcon[0] is not used in AES algorithm."
  */
 
-
-/*****************************************************************************/
-/* Private functions:                                                        */
-/*****************************************************************************/
-
-#define getSBoxValue(num) (sbox[(num)])
-
-// This function produces Nb(Nr+1) round keys. The round keys are used in each round to decrypt the states. 
-static void KeyExpansion(uint8_t* RoundKey, const uint8_t* Key)
+/*
+ * This function produces Nb(Nr+1) round keys. The round keys are used in each
+ * round to decrypt the states.
+ */
+static void KeyExpansion(uint8_t *RoundKey, const uint8_t *Key)
 {
-  unsigned i, j, k;
-  uint8_t tempa[4]; // Used for the column/row operations
-  
-  // The first round key is the key itself.
-  for (i = 0; i < Nk; ++i)
-  {
-    RoundKey[(i * 4) + 0] = Key[(i * 4) + 0];
-    RoundKey[(i * 4) + 1] = Key[(i * 4) + 1];
-    RoundKey[(i * 4) + 2] = Key[(i * 4) + 2];
-    RoundKey[(i * 4) + 3] = Key[(i * 4) + 3];
-  }
+    int i, j, k;
+    uint8_t tempa[4]; // Used for the column/row operations
 
-  // All other round keys are found from the previous round keys.
-  for (i = Nk; i < Nb * (Nr + 1); ++i)
-  {
-    {
-      k = (i - 1) * 4;
-      tempa[0]=RoundKey[k + 0];
-      tempa[1]=RoundKey[k + 1];
-      tempa[2]=RoundKey[k + 2];
-      tempa[3]=RoundKey[k + 3];
-
+    // The first round key is the key itself.
+    for (i = 0; i < Nk; ++i) {
+        RoundKey[(i * 4) + 0] = Key[(i * 4) + 0];
+        RoundKey[(i * 4) + 1] = Key[(i * 4) + 1];
+        RoundKey[(i * 4) + 2] = Key[(i * 4) + 2];
+        RoundKey[(i * 4) + 3] = Key[(i * 4) + 3];
     }
 
-    if (i % Nk == 0)
-    {
-      // This function shifts the 4 bytes in a word to the left once.
-      // [a0,a1,a2,a3] becomes [a1,a2,a3,a0]
+    // All other round keys are found from the previous round keys.
+    for (i = Nk; i < Nb * (Nr + 1); ++i) {
+        k = (i - 1) * 4;
+        tempa[0] = RoundKey[k + 0];
+        tempa[1] = RoundKey[k + 1];
+        tempa[2] = RoundKey[k + 2];
+        tempa[3] = RoundKey[k + 3];
 
-      // Function RotWord()
-      {
-        const uint8_t u8tmp = tempa[0];
-        tempa[0] = tempa[1];
-        tempa[1] = tempa[2];
-        tempa[2] = tempa[3];
-        tempa[3] = u8tmp;
-      }
+        if (i % Nk == 0) {
+            // This function shifts the 4 bytes in a word to the left once.
+            // [a0,a1,a2,a3] becomes [a1,a2,a3,a0]
 
-      // SubWord() is a function that takes a four-byte input word and 
-      // applies the S-box to each of the four bytes to produce an output word.
+            // Function RotWord()
+            {
+                const uint8_t u8tmp = tempa[0];
+                tempa[0] = tempa[1];
+                tempa[1] = tempa[2];
+                tempa[2] = tempa[3];
+                tempa[3] = u8tmp;
+            }
 
-      // Function Subword()
-      {
-        tempa[0] = getSBoxValue(tempa[0]);
-        tempa[1] = getSBoxValue(tempa[1]);
-        tempa[2] = getSBoxValue(tempa[2]);
-        tempa[3] = getSBoxValue(tempa[3]);
-      }
+            // SubWord() is a function that takes a four-byte input word and
+            // applies the S-box to each of the four bytes to produce an output
+            // word.
 
-      tempa[0] = tempa[0] ^ Rcon[i/Nk];
+            // Function Subword()
+            {
+                tempa[0] = sbox[tempa[0]];
+                tempa[1] = sbox[tempa[1]];
+                tempa[2] = sbox[tempa[2]];
+                tempa[3] = sbox[tempa[3]];
+            }
+
+            tempa[0] = tempa[0] ^ Rcon[i / Nk];
+        }
+
+        j = i * 4;
+        k = (i - Nk) * 4;
+        RoundKey[j + 0] = RoundKey[k + 0] ^ tempa[0];
+        RoundKey[j + 1] = RoundKey[k + 1] ^ tempa[1];
+        RoundKey[j + 2] = RoundKey[k + 2] ^ tempa[2];
+        RoundKey[j + 3] = RoundKey[k + 3] ^ tempa[3];
     }
-
-    j = i * 4; k=(i - Nk) * 4;
-    RoundKey[j + 0] = RoundKey[k + 0] ^ tempa[0];
-    RoundKey[j + 1] = RoundKey[k + 1] ^ tempa[1];
-    RoundKey[j + 2] = RoundKey[k + 2] ^ tempa[2];
-    RoundKey[j + 3] = RoundKey[k + 3] ^ tempa[3];
-  }
 }
 
-// This function adds the round key to state.
-// The round key is added to the state by an XOR function.
-static void AddRoundKey(uint8_t round, state_t* state, const uint8_t* RoundKey)
+/*
+ * This function adds the round key to state.
+ * The round key is added to the state by an XOR function.
+ */
+static void
+AddRoundKey(uint8_t round, state_t *state, const uint8_t *RoundKey)
 {
   uint8_t i,j;
-  for (i = 0; i < 4; ++i)
-  {
-    for (j = 0; j < 4; ++j)
-    {
-      (*state)[i][j] ^= RoundKey[(round * Nb * 4) + (i * Nb) + j];
-    }
+
+  for (i = 0; i < 4; ++i) {
+      for (j = 0; j < 4; ++j) {
+          (*state)[i][j] ^= RoundKey[(round * Nb * 4) + (i * Nb) + j];
+      }
   }
 }
 
-// The SubBytes Function Substitutes the values in the
-// state matrix with values in an S-box.
-static void SubBytes(state_t* state)
+/*
+ * The SubBytes Function Substitutes the values in the
+ * state matrix with values in an S-box.
+ */
+static void
+SubBytes(state_t *state)
 {
-  uint8_t i, j;
-  for (i = 0; i < 4; ++i)
-  {
-    for (j = 0; j < 4; ++j)
-    {
-      (*state)[j][i] = getSBoxValue((*state)[j][i]);
+    uint8_t i, j;
+
+    for (i = 0; i < 4; ++i) {
+        for (j = 0; j < 4; ++j) {
+            (*state)[j][i] = sbox[(*state)[j][i]];
+        }
     }
-  }
 }
 
-// The ShiftRows() function shifts the rows in the state to the left.
-// Each row is shifted with different offset.
-// Offset = Row number. So the first row is not shifted.
-static void ShiftRows(state_t* state)
+/*
+ * The ShiftRows() function shifts the rows in the state to the left.
+ * Each row is shifted with different offset.
+ * Offset = Row number. So the first row is not shifted.
+ */
+static void
+ShiftRows(state_t* state)
 {
   uint8_t temp;
 
@@ -246,7 +240,7 @@ static uint8_t xtime(uint8_t x)
   return ((x<<1) ^ (((x>>7) & 1) * 0x1b));
 }
 
-// MixColumns function mixes the columns of the state matrix
+/* MixColumns function mixes the columns of the state matrix */
 static void MixColumns(state_t* state)
 {
   uint8_t i;
@@ -269,11 +263,14 @@ static void MixColumns(state_t* state)
       ((y>>3 & 1) * xtime(xtime(xtime(x)))) ^         \
       ((y>>4 & 1) * xtime(xtime(xtime(xtime(x))))))   \
 
-#define getSBoxInvert(num) (rsbox[(num)])
-
-// MixColumns function mixes the columns of the state matrix.
-// The method used to multiply may be difficult to understand for the inexperienced.
-// Please use the references to gain more information.
+/*
+ * MixColumns function mixes the columns of the state matrix.
+ *
+ * The method used to multiply may be difficult to understand for the
+ * inexperienced.
+ *
+ * Please use the references to gain more information.
+ */
 static void InvMixColumns(state_t* state)
 {
   int i;
@@ -292,18 +289,19 @@ static void InvMixColumns(state_t* state)
   }
 }
 
-// The SubBytes Function Substitutes the values in the
-// state matrix with values in an S-box.
-static void InvSubBytes(state_t* state)
+/*
+ * The SubBytes Function Substitutes the values in the state matrix with values
+ * in an S-box.
+ */
+static void InvSubBytes(state_t *state)
 {
-  uint8_t i, j;
-  for (i = 0; i < 4; ++i)
-  {
-    for (j = 0; j < 4; ++j)
-    {
-      (*state)[j][i] = getSBoxInvert((*state)[j][i]);
+    uint8_t i, j;
+
+    for (i = 0; i < 4; ++i) {
+        for (j = 0; j < 4; ++j) {
+            (*state)[j][i] = rsbox[(*state)[j][i]];
+        }
     }
-  }
 }
 
 static void InvShiftRows(state_t* state)
@@ -339,8 +337,6 @@ void mkw_aes_init(mkw_aes_ctx_t *ctx,
 {
   KeyExpansion(ctx->RoundKey, key);
 }
-
-// Cipher is the main function that encrypts the PlainText.
 
 void mkw_aes_encrypt_block(mkw_aes_ctx_t *ctx,
                            uint8_t block[MKW_AES_BLOCK_SIZE])
