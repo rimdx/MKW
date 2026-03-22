d01c9ba41196efb3cfafe90d427bb7ffe8dde2ab
Timofei Zhakov <tima@chemodax.net>
2026-03-22 18:25:14 +0000

    Fix hex import.
    git-svn-id: file:///home/repos/rinrab/MKW/trunk@6770 0083e6b4-6090-234a-836e-88fcc467669c

--- native/mkw_core/bigint.c
+++ native/mkw_core/bigint.c
@@ -18,17 +18,43 @@ mkw_bigint_set(mkw_bigint_t *x, const mkw_bigint_t *n)
     return x;
 }
 
+static int
+decrypt_one_hex(char ch)
+{
+    switch (ch) {
+        case '0': return 0;
+        case '1': return 1;
+        case '2': return 2;
+        case '3': return 3;
+        case '4': return 4;
+        case '5': return 5;
+        case '6': return 6;
+        case '7': return 7;
+        case '8': return 8;
+        case '9': return 9;
+        case 'a': return 10;
+        case 'b': return 11;
+        case 'c': return 12;
+        case 'd': return 13;
+        case 'e': return 14;
+        case 'f': return 15;
+        default: abort();
+    };
+}
+
 mkw_bigint_t *
 mkw_bigint_set_hex(mkw_bigint_t *x, const char *data)
 {
     int i;
     int len = strlen(data);
+    int limb_nibbles = sizeof(mkw_limb_t) * 2;
     assert(len % 2 == 0);
     assert(len / 2 < sizeof(*x));
 
     memset(x->digits, 0, sizeof(x->digits));
-    for (; len >= 0; len--) {
-        x->digits[i] = data[i * 2 + 0];
+    for (i = 0; i < len; i++) {
+        int num = decrypt_one_hex(data[i]);
+        x->digits[i / limb_nibbles] |= num << (i % limb_nibbles * 4);
     }
     return x;
 }
